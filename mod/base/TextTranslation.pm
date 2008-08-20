4dc6d3090e24e4cad3299698c13a74a338a116a9
Basedow, Holm <holm.basedow@t-systems.com>
2008-08-20 13:15:51 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@3674 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/base/TextTranslation.pm
+++ mod/base/TextTranslation.pm
@@ -218,13 +218,326 @@ sub Text2Acronym
 {
    my $self=shift;
 
+   my $cursrc=Query->Param("src");
    print $self->HttpHeader("text/html");
    print $self->HtmlHeader(style=>'default.css',
                            title=>$self->T($self->Self()));
+#   my ($cursrc,$curdst,$cursrclang,$curdstlang);
+#   my $ua;
+#   my $html;
+#   eval('
+#use LWP::UserAgent;
+#use HTTP::Request::Common;
+#use HTTP::Cookies;
+#use HTML::Parser;
+#
+#$ua=new LWP::UserAgent(env_proxy=>1);
+#$ua->cookie_jar(HTTP::Cookies->new(file => "/tmp/.w5base.cookies.txt"));
+#$ua->timeout(60);
+#$ua->agent("Mozilla/5.0 (X11; U; Linux i686; de; rv:1.9.0.1) Gecko/2008070206 Firefox/3.0.1");
+#$html=new HTML::Parser();
+#');
+#   if ($@ ne ""){
+#      msg(ERROR,$@);
+#   }
+#   if (defined($ua)){
+#      my $proxy=$self->Config->Param("http_proxy");
+#      if ($proxy ne ""){
+#         msg(INFO,"set proxy to $proxy");
+#         $ua->proxy(['http', 'ftp'],$proxy);
+#      }
+#   }
+#
+#
+#
+#
+#   if ($cursrc ne "" && defined($ua)){
+#      my $ace="http://ac.epfl.ch";
+#      my $response=$ua->request(GET($ace));
+#print ("ace=$ace <br>");
+#print ("ua=$ua <br>");
+#print ("cursrc=$cursrc <br>");
+#printf ("code=%s <br>",$response->code);
+#      if ($response->code ne "200"){
+#         msg(ERROR,"fail to init '$ace' response code=".
+#                   $response->code);
+#         $ua=undef;
+#      }
+#      my $acetarget="http://ac.epfl.ch/cgi-bin/ac.cgi";
+#      my $response=$ua->request(POST($acetarget,
+#                       'Referer'=>'http://ac.epfl.ch/cgi-bin/ac.cgi',
+#                       'Content_Type'=>'application/x-www-form-urlencoded',
+#                       'Accept-Charset'=>'ISO-8859-15,utf-8;q=0.7,*;q=0.7',
+#                       'Accept'=>'text/xml,application/xml,application/xhtml'.
+#                                 '+xml,text/html;q=0.9,text/plain;q=0.8',
+#                       'Accept-Language'=>'de-de,de;q=0.8,en-us;q=0.5,en;q=0.3',
+#                       'Content'=>['F_UserId'=>'1',
+#                                   'F_FeedBack'=>'0',
+#                                   'F_Keywords'=>"$cursrc",
+#                                   'F_Submit'=>'Get New Acronyms',
+#                                   'F_SimpleSearch'=>'1',
+#                                   'F_Extra'=>'0',
+#                                   'F_Done'=>'0',
+#                                   'F_Advance'=>'0',
+#                                   'F_DisplayPage'=>'1',
+#                                   'F_FeedBack'=>'0']));
+#printf ("rccode=%s <br>",$response->status_line());
+#   }
 
-printf("fifi Text2Acronym");
+   my $handlermask=$self->getParsedTemplate("tmpl/base.Text2Acronym",{});
+   print $handlermask;
+   if ($cursrc ne ""){
+      my $letters=scanWord($cursrc);
+      my %acros;
+printf STDERR ("fifi nach scan word\n");
+      for (my $l=4;$l<7;$l++){
+          moveStart($letters,$l,\%acros);
+      }
+printf STDERR ("fifi nach reverse\n");
+      my $dbs=initDBs(['en']);
+printf STDERR ("fifi nach initDB\n");
+      seekWord(\%acros,$dbs);
+printf STDERR ("fifi nach seek\n");
+      closeDBs($dbs);
+      foreach my $key (keys(%acros)){
+         if ($acros{$key} == 5 and 
+             substr($key,0,1) eq lc(substr($cursrc,0,1))){
+             $acros{$key} =3;
+         }elsif($acros{$key} != 5){
+             delete($acros{$key});
+         } 
+      }
+      my @keys=sort({ $acros{$a} <=> $acros{$b} } keys(%acros));
+      foreach my $key (@keys){
+         my ($nword,$found,@ks);
+         for (my $ss=0;$ss<length($key);$ss++){
+             push(@ks,substr($key,$ss,1));
+         }
+         for (my $s=0;$s<length($cursrc);$s++){
+            $found=0;
+            my $keychr=lc(substr($cursrc,$s,1));
+            if ($keychr eq $ks[0]){
+               splice(@ks,0,1);
+               $found=1;
+            } 
+            if ($found==1){
+               $nword=$nword."<b>$keychr</b>";
+            }else{
+               $nword=$nword.$keychr;
+            }
+         }
+         print ("<tr><td></td><td align=center width=120>$key
+                 </td><td width=190 align=center>$nword</td><td></td></tr>");
+      }
+      printf ("<tr><td colspan=4>&nbsp;</td></tr><tr><td></td><td align=center".
+              " colspan=2><b>possibilities=%s</b><td></td></td></tr></table>",
+              my $co=keys(%acros));
+   }else{
+         print ("<tr><td></td><td colspan=2 align=center width=120>".
+                "please insert your keyword...</td><td></td></tr>");
+   }
    print $self->HtmlBottom(body=>1,form=>1);
 }
 
+sub scanWord
+{
+   my $name=shift;
+   my $word=1;
+   my $ipos=0;
+   my $char;
+   my @letters;
+   # scan given word 
+   for (my $s=0;$s<length($name);$s++){
+      my $tmpchr=substr($name,$s,1);
+      if ($tmpchr=~m/ /){
+         $word++;
+      }else{
+         my $rest=substr($name,$s+1,length($name)-$s);
+         $char={'char'=>$tmpchr,
+                'word'=>$word,
+                'rest'=>$rest,
+                'pos'=>$ipos};
+         push(@letters,$char);
+         $ipos++; 
+      }
+   }
+   return(\@letters);
+}
+
+sub moveStart
+{
+   my $letters=shift;
+   my $length=shift;
+   my $acros=shift;
+   # move start position one step forward
+   for (my $a=0;$a<$#{$letters}+2-$length;$a++){
+      if ($a == $#{$letters}+1-$length){
+         my $lastword;
+         for (my $y=0;$y<$length;$y++){ 
+            $lastword=$lastword.$letters->[$a+$y]->{'char'};
+         }
+         $acros->{lc($lastword)}=1;
+         next;
+      }
+      my ($rstr,$rpos)=chglastchar($letters,$length,$acros,"",$a);
+      for (my $l=2;$l<$length;$l++){
+         ($rstr)=chgchar($letters,$length,$acros,$rstr,$rpos,$l);
+      }   
+   }
+}
+
+sub chgchar
+{
+   # change characters after start position (first character in $str)
+   my $letters=shift;
+   my $length=shift;
+   my $acros=shift;
+   my $str=shift;
+   my $pos=shift;
+   my $level=shift;
+   my $newstr=substr($str,0,length($str)-$level);
+   my $posnew=$pos;
+   while (length($newstr) < $length){
+      if ($#{$letters} >= $posnew-$level+1){
+         $newstr=$newstr.$letters->[$posnew-$level+1]->{'char'};
+         $posnew++;
+      }else{
+         last;
+      }
+   }
+   if ($pos < $#{$letters}){
+      if ($level == 2){
+         chgchar($letters,$length,$acros,
+                 chglastchar($letters,$length,$acros,$newstr,$pos),
+                 $level);
+      }else{
+         chgchar($letters,$length,$acros,
+                 chgchar($letters,$length,$acros,$newstr,$pos,$level-1),
+                 $level); 
+      }
+   }elsif($pos == $#{$letters}){
+      if ($level == 2){
+         chglastchar($letters,$length,$acros,$newstr,$pos);
+      }else{
+         chgchar($letters,$length,$acros,$newstr,$pos,$level-1);
+      }
+   }
+   return($str,$pos+1);
+}
+
+sub chglastchar
+{
+   # change last character for all possibilities
+   my $letters=shift;
+   my $length=shift;
+   my $acros=shift;
+   my $str=shift;
+   my $pos=shift;
+   while (length($str) >= $length){
+      $str=substr($str,0,length($str)-1);
+   }
+   for (my $c=$pos;$c<=$#{$letters};$c++){
+      $str=$str.$letters->[$c]->{'char'};
+      if($length == length($str)){
+         $acros->{lc($str)}=1;
+         if ($#{$letters} != $letters->[$c]->{'pos'}){
+            chglastchar($letters,$length,$acros,substr($str,0,$length-1),$letters->[$c]->{'pos'}+1);
+            return($str,$letters->[$c]->{'pos'}+1);
+         }
+      }
+   }   
+   return($str,$pos+1);
+}
+
+sub initDBs
+{
+   my $db=shift;
+   my @rdb;
+   my $fnd="";
+   my $fnd2="";
+   my $fnd3="";
+   my $fnd4="";
+   foreach my $d (@$db){
+      my %db;
+      my $tell=0;
+      open(my $fh,'<'."/opt/w5base/lib/dict/$d") ||
+          printf STDERR ("ERROR: can't open dictionary $d\n");
+      $db{'tblname'}="$d";
+      while (my $w=<$fh>){
+         if ($fnd ne lc(substr($w,0,1))){
+            $fnd=lc(substr($w,0,1)); 
+            $db{'keypos'}->{$fnd}=$tell;
+         }
+         if ($fnd2 ne lc(substr($w,0,2))){
+            $fnd2=lc(substr($w,0,2)); 
+            $db{'keypos'}->{$fnd2}=$tell;
+         }
+         if ($fnd3 ne lc(substr($w,0,3))){
+            $fnd3=lc(substr($w,0,3)); 
+            $db{'keypos'}->{$fnd3}=$tell;
+         }
+         if ($fnd4 ne lc(substr($w,0,3))){
+            $fnd4=lc(substr($w,0,3)); 
+            $db{'keypos'}->{$fnd4}=$tell;
+         }
+         $tell=tell($fh);
+      }
+      seek($fh,0,0);
+      $db{'dbfp'}=$fh;
+      push(@rdb,\%db);
+   }
+   return(\@rdb); 
+}
+
+sub closeDBs
+{
+   my $db=shift;
+   foreach my $fh (@$db){
+      close($fh->{'dbfp'});
+   }
+}
+
+sub seekWord
+{
+   my $acros=shift;
+   my $dbs=shift;
+   foreach my $key (keys(%$acros)){
+      foreach my $db (@$dbs){
+         my $f;
+         $f=$db->{'dbfp'};
+         my ($ch,$l);
+         if (exists($db->{'keypos'}->{substr($key,0,4)})){
+            $ch=substr($key,0,4);
+            $l=4;
+            seek($f,$db->{'keypos'}->{$ch},0);
+         }elsif(exists($db->{'keypos'}->{substr($key,0,3)})){
+            $ch=substr($key,0,3);
+            $l=3;
+            seek($f,$db->{'keypos'}->{$ch},0);
+         }elsif(exists($db->{'keypos'}->{substr($key,0,2)})){
+            $ch=substr($key,0,2);
+            $l=2;
+            seek($f,$db->{'keypos'}->{$ch},0);
+         }elsif(exists($db->{'keypos'}->{substr($key,0,1)})){
+            $ch=substr($key,0,1);
+            $l=1;
+            seek($f,$db->{'keypos'}->{$ch},0);
+         }else{
+            last;
+         }
+         while(my $dict=<$f>){
+            chop($dict);
+            if (lc($dict) eq $key){
+               $acros->{$key}=5;
+               last;
+            } 
+            if (lc(substr($dict,0,$l)) ne $ch){
+               last;
+            }
+         }
+      }
+   }
+   return($acros);
+}
 
 1;
