727846271aff0576693baf2b496d1e35d5d87803
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2008-09-26 11:43:25 +0000

    remove old tinymce
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@3796 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- static/tinymce/jscripts/tiny_mce/plugins/clearbr/langs/de.js
+++
@@ -1,4 +0,0 @@
-tinyMCE.addToLang('',{
-clearbr_title : 'br-Clear',
-clearbr_desc : 'Positionierung neben dem Bild aufheben'
-});
--- static/tinymce/jscripts/tiny_mce/plugins/clearbr/langs/en.js
+++
@@ -1,4 +0,0 @@
-tinyMCE.addToLang('',{
-clearbr_title : 'br-Clear',
-clearbr_desc : 'Clear the position of text besides an image'
-});
