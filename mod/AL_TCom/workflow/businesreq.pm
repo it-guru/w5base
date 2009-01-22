7618571b7652b2025994d82f9628f1336f861bea
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2009-01-22 16:02:29 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@4085 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/AL_TCom/workflow/businesreq.pm
+++ mod/AL_TCom/workflow/businesreq.pm
@@ -101,7 +101,6 @@ sub isWriteValid
    my @grps=$self->SUPER::isWriteValid($rec);
    if (grep(/^init$/,@grps)){
       my $mandatorid=$rec->{mandatorid}; 
-      printf STDERR ("fifi mandator=%s\n",Dumper($mandatorid));
       $mandatorid=[$mandatorid] if (ref($mandatorid) ne "ARRAY");
       @$mandatorid=grep(!/^\s*$/,@$mandatorid);
       if ($#{$mandatorid}!=-1){
@@ -113,7 +112,6 @@ sub isWriteValid
          push(@grps,"tcomcod");
       }
    }
-#printf STDERR ("fifi grps=%s\n",Dumper(\@grps));
    return(@grps);
 }
 
