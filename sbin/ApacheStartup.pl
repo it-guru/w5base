d028912586846f097d3550a6f2508ba1ba79952c
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2009-03-12 13:30:35 +0000

    Apache Startup Handling
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@4269 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ sbin/ApacheStartup.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+BEGIN{
+   foreach my $path ("$W5V2::INSTDIR/mod","$W5V2::INSTDIR/lib"){
+      unshift(@INC,$path) if (!grep(/^$path$/,@INC));
+   }
+}
+use strict;
+use base::start;
+use base::MyW5Base;
+use base::workflow;
+use base::grp;
+use base::user;
+use base::userpic;
+use base::useraccount;
+use base::userlogon;
+use base::userbookmark;
+use base::lnkcontact;
+use base::lnkgrpuser;
+use base::workflowkey;
+use base::location;
+use base::cistatus;
+use base::XLSExpand;
+use base::load;
+use base::filemgmt;
+use base::w5stat;
+use base::menu;
+use base::qrule;
+use base::joblog;
+use base::mandator;
+use base::userdefault;
+use base::usermask;
+return(1);
