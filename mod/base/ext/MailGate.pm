8df96dbe950a7395de128d37b78b09da92291994
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2009-04-17 12:16:20 +0000

    add postmaster support to mailgateway
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@4379 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/base/ext/MailGate.pm
+++ mod/base/ext/MailGate.pm
@@ -38,7 +38,49 @@ sub Process
    my $rec=shift;
    my $answer=shift;
 
-   if ($rec->{mailmode} eq "adminrequest"){
+   if ($rec->{mailmode} eq "postmaster"){
+      if (my ($email)=$rec->{textdata}=~m/(\S+\@\S+)/m){
+         my $user=getModuleObject($app->Config,"base::user");
+         $email=lc($email);
+         $user->SetFilter({email=>\$email,cistatusid=>"<6"});
+         my ($urec,$msg)=$user->getOnlyFirst(qw(userid fullname));
+         if (defined($urec)){
+            msg(INFO,"delivery error on $urec->{fullname}");
+            my $wf=getModuleObject($app->Config,"base::workflow");
+            my $srcsys="MailgateDeliveryError";
+            $wf->SetFilter({srcid=>\$urec->{userid},srcsys=>\$srcsys});
+            my ($wrec,$msg)=$wf->getOnlyFirst(qw(ALL));
+            if (defined($wrec) && $wrec->{stateid}>=17){
+               $wf->ValidatedDeleteRecord($wrec);
+               $wrec=undef;
+            }
+            if (!defined($wrec)){
+               my $newrec={name=>"DataIssue: delivery error on $email",
+                            detaildescription=>$rec->{textdata},
+                            class=>"base::workflow::DataIssue",
+                            step=>"base::workflow::DataIssue::dataload",
+                            affectedobject=>"base::user",
+                            affectedobjectid=>$urec->{userid},
+                            altaffectedobjectname=>$urec->{fullname},
+                            directlnkmode=>"DataIssueMsg",
+                            eventend=>undef,
+                            eventstart=>NowStamp("en"),
+                            srcload=>NowStamp("en"),
+                            srcsys=>$srcsys,
+                            srcid=>$urec->{userid},
+                            DATAISSUEOPERATIONSRC=>"DataIssueMsg"};
+                my $bk=$wf->Store(undef,$newrec);
+            }
+         }
+      }
+
+      
+      msg(INFO,"ok delerror");
+      return(1);
+
+
+
+   }elsif ($rec->{mailmode} eq "adminrequest"){
       my $name=$rec->{name};
       my $desc=$rec->{textdata};
       my $wf=getModuleObject($app->Config,"base::workflow");
