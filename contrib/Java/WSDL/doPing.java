60ad98b650d19be4ed97a2560d889a235be5d808
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2009-05-14 12:41:47 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@4457 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- contrib/Java/WSDL/doPing.java
+++ contrib/Java/WSDL/doPing.java
@@ -13,8 +13,8 @@ public class doPing {
     // Now use the service to get a stub which implements the SDI.
     net.w5base.mod.base.user.Port W5Port = service.getW5BaseUser();
 
-    ((Stub) W5Port)._setProperty(Call.USERNAME_PROPERTY, "sued/vogler.hartmut");
-    ((Stub) W5Port)._setProperty(Call.PASSWORD_PROPERTY, "0Zehn410");
+    ((Stub) W5Port)._setProperty(Call.USERNAME_PROPERTY, "dummy/admin");
+    ((Stub) W5Port)._setProperty(Call.PASSWORD_PROPERTY, "acache");
 
 
     net.w5base.mod.base.user.DoPingOutput output=
