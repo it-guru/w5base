ab37b2270ba99cb66799e4997bbae8691568a21c
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2009-06-04 15:43:54 +0000

    add a qrule, to find invalid group references
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@4518 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ mod/itil/qrule/checkGroups.pm
@@ -0,0 +1,94 @@
+package itil::qrule::checkGroups;
+#######################################################################
+=pod
+
+=head3 PURPOSE
+
+This qulaity rule checks the CI-Status of the groups, references in
+businessteam, serviceteam and customer.
+
+=cut
+
+#######################################################################
+#######################################################################
+#
+#  W5Base Framework
+#  Copyright (C) 2007  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::QRule;
+@ISA=qw(kernel::QRule);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   return($self);
+}
+
+sub getPosibleTargets
+{
+   return(["itil::appl"]);
+}
+
+sub qcheckRecord
+{
+   my $self=shift;
+   my $dataobj=shift;
+   my $rec=shift;
+
+   my $wfrequest={};
+   my @qmsg;
+   my @dataissue;
+   my $errorlevel=0;
+
+   foreach my $fobj ($dataobj->getFieldObjsByView([qw(ALL)],
+                                           oldrec=>$rec)){
+      if ($fobj->Type() eq "Group"){
+         my $name=$fobj->Name();
+         my $localfield=$fobj->{vjoinon}->[0];
+         my $lfobj=$dataobj->getField($localfield);
+         my $ldata=$lfobj->RawValue($rec);
+         if ($ldata ne ""){
+            my $joinobj=$fobj->vjoinobj();
+            $joinobj->SetFilter({$fobj->{vjoinon}->[1]=>\$ldata});
+            my ($grec,$msg)=$joinobj->getOnlyFirst(qw(cistatusid fullname));
+            if (!defined($grec)){
+               push(@qmsg,"target group seems to be deleted: ".$name);
+            }
+            else{
+               if ($grec->{cistatusid}==6){
+                  push(@qmsg,"target group is marked as deleted: ".
+                             $name." ($grec->{fullname})");
+               }
+            }
+         }
+      }
+   }
+   if ($#qmsg==-1){
+      return(0,undef);
+   }
+   return(3,{qmsg=>\@qmsg,dataissue=>\@qmsg});
+}
+
+
+
+1;
