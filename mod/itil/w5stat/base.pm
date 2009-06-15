5fff5f00b15ed945a497945f30de897a9c839107
w5base <system@w5base.net>
2009-06-15 15:02:07 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@4566 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/itil/w5stat/base.pm
+++ mod/itil/w5stat/base.pm
@@ -507,8 +507,8 @@ sub processRecord
          }
          my $involvedresponseteam=$rec->{involvedresponseteam};
          my $involvedbusinessteam=$rec->{involvedbusinessteam};
-       #  $involvedresponseteam=[] if (!ref($involvedresponseteam));
-       #  $involvedbusinessteam=[] if (!ref($involvedbusinessteam));
+         $involvedresponseteam=[$involvedresponseteam] if (!ref($involvedresponseteam));
+         $involvedbusinessteam=[$involvedbusinessteam] if (!ref($involvedbusinessteam));
          my @groups=();
          push(@groups,@$involvedresponseteam);
          push(@groups,@$involvedbusinessteam);
