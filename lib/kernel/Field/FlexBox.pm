c390342c4eed7218bef1a83f2da39e923d0a454c
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2009-08-25 08:53:11 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@4708 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ lib/kernel/Field/FlexBox.pm
@@ -0,0 +1,110 @@
+package kernel::Field::FlexBox;
+#  W5Base Framework
+#  Copyright (C) 2006  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use Data::Dumper;
+@ISA    = qw(kernel::Field);
+
+
+sub new
+{
+   my $type=shift;
+   my $self=bless($type->SUPER::new(@_),$type);
+   return($self);
+}
+
+
+sub FormatedDetail
+{
+   my $self=shift;
+   my $current=shift;
+   my $mode=shift;
+   my $d=$self->RawValue($current);
+   my $name=$self->Name();
+
+   if (($mode eq "edit" || $mode eq "workflow")){
+      my $readonly=0;
+      if ($self->readonly($current)){
+         $readonly=1;
+      }
+      if ($self->frontreadonly($current)){
+         $readonly=1;
+      }
+      my $fromquery=Query->Param("Formated_$name");
+      if (defined($fromquery)){
+         $d=$fromquery;
+      }
+      my $input=$self->getSimpleInputField($d,$readonly);
+      my $targeturl=$self->{vjointo};
+      $targeturl=~s/::/\//;
+      $targeturl="../../$targeturl/Result";
+      my $disp=$self->{vjoindisp};
+      $input.=<<EOF;
+<script language=JavaScript>
+\$(document).ready(function(){
+   \$("#$name").autocomplete("$targeturl", {
+      max:50,
+      mustMatch: true,
+      minChars: 2,
+      extraParams: {
+          FormatAs: 'CsvV02',
+          CurrentView: '($disp)',
+          UseLimit: '40',
+          UseLimitStart: '0',
+          search_$disp: function() { return \$("#$name").val()+"*"; }
+      }
+   });
+});
+</script>
+EOF
+      return($input);
+   }
+   $d=[$d] if (ref($d) ne "ARRAY");
+   if ($mode eq "HtmlDetail"){
+      $d=[map({$self->addWebLinkToFacility(quoteHtml($_),$current)} @{$d})];
+   }
+   if ($mode eq "SOAP"){
+      $d=[map({quoteSOAP($_)} @{$d})];
+   }
+   if ($mode eq "HtmlV01"){
+      $d=[map({quoteHtml($_)} @{$d})];
+   }
+   if ($mode ne "XMLV01"){
+      my $vjoinconcat=$self->{vjoinconcat};
+      $vjoinconcat="; " if (!defined($vjoinconcat));
+      $d=join($vjoinconcat,@$d);
+   }
+   
+   if ($mode eq "HtmlV01"){
+      $d=~s/\n/<br>\n/g;
+      if ($self->{htmlnowrap}){
+         $d=~s/[ \t]/&nbsp;/g;
+         $d=~s/-/&minus;/g;
+      }
+   }
+   $d.=" ".$self->{unit} if ($d ne "" && $mode eq "HtmlDetail");
+   return($d);
+}
+
+
+
+
+
+1;
