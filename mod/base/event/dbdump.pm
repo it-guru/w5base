b79fb29feeb2208dc69dafd3943a506c14b3f827
Basedow, Holm <holm.basedow@t-systems.com>
2009-10-01 10:05:58 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@4794 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/base/event/dbdump.pm
+++ mod/base/event/dbdump.pm
@@ -21,7 +21,21 @@ use vars qw(@ISA);
 use Data::Dumper;
 use kernel;
 use kernel::Event;
+use IPC::Open3;
+use IO::Handle;
+use IO::Select;
+use Compress::Zlib;
+use Date::Calc qw( Today_and_Now
+                   Week_of_Year
+                   Day_of_Week_to_Text
+                   Delta_YMDHMS
+                   Delta_DHMS
+                   Day_of_Week );
+use File::Path;
 @ISA=qw(kernel::Event);
+my ($prog);
+my $starttime=sprintf("%04d-%02d-%02d %02d:%02d:%02d",Today_and_Now);
+my @prog=qw(mysql mysqldump);
 
 sub new
 {
@@ -42,24 +56,99 @@ sub Init
 sub tinyDBDump
 {
    my $self=shift;
+   my %param=@_;
+   check_db($self,$param{db},$param{host});
    my $appl=getModuleObject($self->Config,"itil::appl");
    $appl->ResetFilter();
    $appl->SetFilter(name=>'SEPT');
    $appl->SetCurrentOrder("NONE");
 #   $appl->SetCurrentView(qw(id,name,systems));
    my $view=$appl->getHashList(qw(id name systems));
-
-   msg(DEBUG,Dumper($view));
+#   msg(DEBUG,Dumper(\%param));
+   process_engine($self,$param{db},$param{host}); 
    return(0);
 }
 
+sub process_engine
+{
+   my $self=shift;
+   my $db=shift;
+   my $host=shift;
+   my $mydsel=new IO::Select();
+   my $mydrdr=new IO::Handle();
+   my $myderr=new IO::Handle();
+   my (@ready,@myderr,@gz);
+
+   my $mysqldump_pid=open3(undef,$mydrdr,$myderr,
+                     "$prog->{mysqldump} -h $host -c -t $db appl");
+                      $mydsel->add($myderr);
+   $mydsel->add($myderr);
+   # check for mysqldump errors
+   @ready = $mydsel->can_read(0.5);
+   foreach my $fh (@ready){
+      while (my $data=<$fh>){
+         push(@myderr,$data);
+      }
+   }
+   # print mysqldump errors
+   if ($#myderr != -1){
+      foreach my $fi (@myderr){
+         if ($fi){
+            msg(ERROR,"$fi");
+         }
+      }
+      @ready=[];
+   }
+   my $gz=gzopen("/tmp/anonym_db_dump.sql.gz", "w9");
+   while(my $line=<$mydrdr>){
+#       msg(DEBUG,"$line");
+       $gz->gzwrite("$line");
+   }
+   $gz->gzclose();
+}
+
+sub check_db
+{
+   my $self=shift;
+   my $db=shift;
+   my $host=shift;
+ 
+   if (!$db){
+      msg("ERROR","database is not given!");
+      exit(1);
+   }
+   # check database is usable
+   local(*R, *E);
+   open3(undef,*R,*E,"$prog->{mysql} -h $host $db");
+   wait();
+   if ($? != 0){
+      msg(ERROR,"source database $db ".
+                "not useable or not found on host=$host, rc=".$?);
+      exit(1);
+   }
+   close(R);
+ 
+   # clear mysql cache
+   system("$prog->{mysql} -h $host -e 'flush logs' $db") == 0 
+           or die("system command -mysql flush logs- failed:$?");
+   system("$prog->{mysql} -h $host -e 'flush tables' $db") == 0 
+           or die("system command -mysql flush tables- failed:$?");
+   msg(DEBUG,"logs on db=$db host=$host flushed");
+   
+}
+
+# find progamms
+foreach my $p (@prog){
+   my $found=0;
+   foreach my $path (split(/:/,$ENV{PATH})){
+      if ( -x "$path/$p"){
+         $found++;
+         $prog->{$p}="$path/$p";
+      }
+
+   }
+   die("programm $p not found") if (!$found);
+}
+
 1;
-
-
-
-
-
-
-
-
 
