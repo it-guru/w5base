8647a2590848544e0cba5a0ebc7ed0979aeccaa9
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2009-10-02 09:28:05 +0000

    change of DTAG.TSI.ES to DTAG.TSI
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@4800 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/AL_TCom/menu/root.pm
+++ mod/AL_TCom/menu/root.pm
@@ -49,17 +49,17 @@ sub Init
    $self->RegisterObj('base::MyW5Base::myP800$',  # virtureller Eintrag für
                       'base::MyW5Base::myP800$',  # MyW5Base
                       func=>'Main',
-                      defaultacl=>['DTAG.TSI.ES.ITO.CSS.AO.DTAG',
-                                   'DTAG.TSI.ES.SSM']);
+                      defaultacl=>['DTAG.TSI.ITO.CSS.AO.DTAG',
+                                   'DTAG.TSI.SSM']);
 
    $self->RegisterObj('AL_TCom::MyW5Base::ChangeFuture$',
                       'AL_TCom::MyW5Base::ChangeFuture$',
                       func=>'Main',
-                      defaultacl=>['admin','DTAG.TSI.ES.ITO.CSS.AO.DTAG']);
+                      defaultacl=>['admin','DTAG.TSI.ITO.CSS.AO.DTAG']);
 
    $self->RegisterObj("AL_TCom",
                       "tmpl/welcome",
-                      defaultacl=>['admin','DTAG.TSI.ES.ITO.CSS.AO.DTAG']);
+                      defaultacl=>['admin','DTAG.TSI.ITO.CSS.AO.DTAG']);
    
    $self->RegisterObj("AL_TCom.custcontract",
                       "AL_TCom::custcontract",
