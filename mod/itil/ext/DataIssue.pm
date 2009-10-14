301f3bdc1fa6c921acddb73e4543c5986edd447d
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2009-10-14 07:52:26 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@4856 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/itil/ext/DataIssue.pm
+++ mod/itil/ext/DataIssue.pm
@@ -75,88 +75,13 @@ sub DataIssueCompleteWriteRequest
 
    my $affectedobject=effVal($oldrec,$newrec,"affectedobject");
 
-   if ($affectedobject=~m/::appl$/){
-      if ($newrec->{affectedobject}=~m/::appl$/){
-         # create link to config Management
-         $newrec->{directlnktype}=$newrec->{affectedobject};
-         $newrec->{directlnkid}=$newrec->{affectedobjectid};
-         $newrec->{directlnkmode}="DataIssue";
-      }
-      my $obj=getModuleObject($self->getParent->Config,$affectedobject);
-      my $affectedobjectid=effVal($oldrec,$newrec,"directlnkid");
-      $obj->SetFilter(id=>\$affectedobjectid);
-      my ($confrec,$msg)=$obj->getOnlyFirst(qw(databossid mandatorid mandator));
-      if (defined($confrec)){
-         if ($confrec->{databossid} ne ""){
-            $newrec->{fwdtarget}="base::user";
-            $newrec->{fwdtargetid}=$confrec->{databossid};
-         }
-         if ($confrec->{mandatorid} ne ""){
-            $newrec->{kh}->{mandatorid}=$confrec->{mandatorid};
-            if (!defined($newrec->{fwdtargetid}) ||
-                 $newrec->{fwdtargetid} eq ""){
-               # now search a Config-Manager
-               my @confmgr=$self->getParent->getMembersOf(
-                              $confrec->{mandatorid},"RCFManager");
-               my $cfmgr1=shift(@confmgr);
-               my $cfmgr2=shift(@confmgr);
-               if ($cfmgr1 ne ""){
-                  $newrec->{fwdtarget}="base::user";
-                  $newrec->{fwdtargetid}=$cfmgr1;
-               }
-               if ($cfmgr2 ne ""){
-                  $newrec->{fwddebtarget}="base::user";
-                  $newrec->{fwddebtargetid}=$cfmgr2;
-               }
-            }
-         }
-         if ($confrec->{mandator} ne ""){
-            $newrec->{kh}->{mandator}=$confrec->{mandator};
-         }
-      }
-   }
-   if ($affectedobject=~m/::system$/){
-      if ($newrec->{affectedobject}=~m/::system$/){
-         # create link to config Management
-         $newrec->{directlnktype}=$newrec->{affectedobject};
-         $newrec->{directlnkid}=$newrec->{affectedobjectid};
-         $newrec->{directlnkmode}="DataIssue";
-      }
-      my $obj=getModuleObject($self->getParent->Config,$affectedobject);
-      my $affectedobjectid=effVal($oldrec,$newrec,"directlnkid");
-      $obj->SetFilter(id=>\$affectedobjectid);
-      my ($confrec,$msg)=$obj->getOnlyFirst(qw(databossid mandatorid mandator));
-      if (defined($confrec)){
-         if ($confrec->{databossid} ne ""){
-            $newrec->{fwdtarget}="base::user";
-            $newrec->{fwdtargetid}=$confrec->{databossid};
-         }
-         if ($confrec->{mandatorid} ne ""){
-            $newrec->{kh}->{mandatorid}=$confrec->{mandatorid};
-            if (!defined($newrec->{fwdtargetid}) ||
-                 $newrec->{fwdtargetid} eq ""){
-               # now search a Config-Manager
-               my @confmgr=$self->getParent->getMembersOf(
-                              $confrec->{mandatorid},"RCFManager");
-               my $cfmgr1=shift(@confmgr);
-               my $cfmgr2=shift(@confmgr);
-               if ($cfmgr1 ne ""){
-                  $newrec->{fwdtarget}="base::user";
-                  $newrec->{fwdtargetid}=$cfmgr1;
-               }
-               if ($cfmgr2 ne ""){
-                  $newrec->{fwddebtarget}="base::user";
-                  $newrec->{fwddebtargetid}=$cfmgr2;
-               }
-            }
-         }
-         if ($confrec->{mandator} ne ""){
-            $newrec->{kh}->{mandator}=$confrec->{mandator};
-         }
-      }
-   }
-   if ($affectedobject=~m/::asset$/){
-      if ($newrec->{affectedobject}=~m/::asset$/){
+   if (($affectedobject=~m/::appl$/)   ||
+       ($affectedobject=~m/::custcontract$/) ||
+       ($affectedobject=~m/::swinstance$/) ||
+       ($affectedobject=~m/::asset$/) ||
+       ($affectedobject=~m/::system$/)){
+      if (defined($newrec->{affectedobject}) &&
+          $newrec->{affectedobject} eq $affectedobject){
          # create link to config Management
          $newrec->{directlnktype}=$newrec->{affectedobject};
          $newrec->{directlnkid}=$newrec->{affectedobjectid};
