ed30cb891a0aa8f236405f7c904ec62c4734b097
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2010-02-08 16:36:03 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@5118 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- sql/base/master.sql
+++ sql/base/master.sql
@@ -295,3 +295,4 @@ create table mandatordataacl(
 #
 alter table interview add boundpviewgroup varchar(40) default NULL;
 alter table interview add boundpcontact   varchar(40) default NULL;
+alter table interview add addquerydata    varchar(128) default NULL;
