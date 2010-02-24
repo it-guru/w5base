de7bc87642392f7f86eaa0e414d91a097901e1cc
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2010-02-24 15:28:17 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@5186 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/tsacinv/qrule/compareSystem.pm
+++ mod/tsacinv/qrule/compareSystem.pm
@@ -130,63 +130,65 @@ sub qcheckRecord
                              $forcedupd,$wfrequest,
                              \@qmsg,\@dataissue,\$errorlevel,
                              mode=>'string');
-      }
-      if ($rec->{allowifupdate}){
-#printf STDERR ("ac=%s\n",Dumper($parrec->{ipaddresses}));
-#printf STDERR ("w5=%s\n",Dumper($rec->{ipaddresses}));
-         my $net=getModuleObject($self->getParent->Config(),"itil::network");
-         $net->SetCurrentView(qw(id name));
-         my $netarea=$net->getHashIndexed("name");
-#printf STDERR ("netarea=%s\n",Dumper($netarea));
-         my @opList;
-         my $res=OpAnalyse(sub{  # comperator 
-                              my $eq;
-                              if ($a->{name} eq $b->{ipaddress}){
-                                 $eq=0;
-                                 $eq=1 if ($a->{comments} eq $b->{description});
-                              }
-                              return($eq);
-                           },
-                           sub{  # oprec generator
-                              my ($mode,$oldrec,$newrec,%p)=@_;
-                              if ($mode eq "insert" || $mode eq "update"){
-                                 my $networkid=$p{netarea}->{name}->
-                                               {'Insel-Netz/Kunden-LAN'}->{id};
-                                 my $identifyby=undef;
-                                 if ($mode eq "update"){
-                                    $identifyby=$oldrec->{id};
-                                 }
-                                 if ($newrec->{ipaddress}=~m/^\s*$/){
-                                    $mode="nop";
-                                 }
-                                 return({OP=>$mode,
-                                         MSG=>"$mode ip $newrec->{ipaddress} ".
-                                              "in W5Base",
-                                         IDENTIFYBY=>$identifyby,
-                                         DATAOBJ=>'itil::ipaddress',
-                                         DATA=>{
-                                            name      =>$newrec->{ipaddress},
-                                            cistatusid=>4,
-                                            networkid =>$networkid,
-                                            comments  =>$newrec->{description},
-                                            systemid  =>$p{refid}
-                                            }
-                                         });
-                              }
-                              elsif ($mode eq "delete"){
-                                 return({OP=>$mode,
-                                         MSG=>"delete ip $oldrec->{name} ".
-                                              "from W5Base",
-                                         DATAOBJ=>'itil::ipaddress',
-                                         IDENTIFYBY=>$oldrec->{id},
-                                         });
-                              }
-                              return(undef);
-                           },
-                           $rec->{ipaddresses},$parrec->{ipaddresses},\@opList,
-                           refid=>$rec->{id},netarea=>$netarea);
-         if (!$res){
-            my $opres=ProcessOpList($self->getParent,\@opList);
+         if ($rec->{allowifupdate}){
+   #printf STDERR ("ac=%s\n",Dumper($parrec->{ipaddresses}));
+   #printf STDERR ("w5=%s\n",Dumper($rec->{ipaddresses}));
+            my $net=getModuleObject($self->getParent->Config(),"itil::network");
+            $net->SetCurrentView(qw(id name));
+            my $netarea=$net->getHashIndexed("name");
+   #printf STDERR ("netarea=%s\n",Dumper($netarea));
+            my @opList;
+            my $res=OpAnalyse(
+                       sub{  # comperator 
+                          my ($a,$b)=@_;
+                          my $eq;
+                          if ($a->{name} eq $b->{ipaddress}){
+                             $eq=0;
+                             $eq=1 if ($a->{comments} eq $b->{description});
+                          }
+                          return($eq);
+                       },
+                       sub{  # oprec generator
+                          my ($mode,$oldrec,$newrec,%p)=@_;
+                          if ($mode eq "insert" || $mode eq "update"){
+                             my $networkid=$p{netarea}->{name}->
+                                           {'Insel-Netz/Kunden-LAN'}->{id};
+                             my $identifyby=undef;
+                             if ($mode eq "update"){
+                                $identifyby=$oldrec->{id};
+                             }
+                             if ($newrec->{ipaddress}=~m/^\s*$/){
+                                $mode="nop";
+                             }
+                             return({OP=>$mode,
+                                     MSG=>"$mode ip $newrec->{ipaddress} ".
+                                          "in W5Base",
+                                     IDENTIFYBY=>$identifyby,
+                                     DATAOBJ=>'itil::ipaddress',
+                                     DATA=>{
+                                        name      =>$newrec->{ipaddress},
+                                        cistatusid=>4,
+                                        networkid =>$networkid,
+                                        comments  =>$newrec->{description},
+                                        systemid  =>$p{refid}
+                                        }
+                                     });
+                          }
+                          elsif ($mode eq "delete"){
+                             return({OP=>$mode,
+                                     MSG=>"delete ip $oldrec->{name} ".
+                                          "from W5Base",
+                                     DATAOBJ=>'itil::ipaddress',
+                                     IDENTIFYBY=>$oldrec->{id},
+                                     });
+                          }
+                          return(undef);
+                       },
+                       $rec->{ipaddresses},$parrec->{ipaddresses},\@opList,
+                       refid=>$rec->{id},netarea=>$netarea);
+            if (!$res){
+               my $opres=ProcessOpList($self->getParent,\@opList);
+            }
          }
       }
 
@@ -279,169 +281,6 @@ sub qcheckRecord
    }
    return($self->HandleWfRequest($dataobj,$rec,
                                  \@qmsg,\@dataissue,\$errorlevel,$wfrequest));
-}
-
-
-sub OpAnalyse
-{
-   my $fpComperator=shift;
-   my $fpRecGenerator=shift;
-   my $refList=shift;
-   my $cmpList=shift;
-   my $opList=shift;
-   my %param=@_;
- 
-   if (ref($fpComperator) ne "CODE"){
-      return(1); 
-   }
-   if (ref($fpRecGenerator) ne "CODE"){
-      return(2); 
-   }
-   if (ref($refList) ne "ARRAY"){
-      return(10); 
-   }
-   if (ref($cmpList) ne "ARRAY"){
-      return(11); 
-   }
-   if (ref($opList) ne "ARRAY"){
-      return(12); 
-   }
-
-   my %cmpRes;
-   for(my $refC=0;$refC<=$#{$refList};$refC++){
-      my $found=0;
-      cmpCloop: for(my $cmpC=0;$cmpC<=$#{$cmpList};$cmpC++){
-         if (!exists($cmpRes{$refC."-".$cmpC})){
-            $a=$refList->[$refC];
-            $b=$cmpList->[$cmpC];
-            $cmpRes{$refC."-".$cmpC}=&{$fpComperator}();
-            if (defined($cmpRes{$refC."-".$cmpC}) && !$cmpRes{$refC."-".$cmpC}){
-               # do an update   
-               my $mode="update";
-               foreach my $op (&{$fpRecGenerator}($mode,
-                                                  $refList->[$refC],
-                                                  $cmpList->[$cmpC],
-                                                  %param)){
-                  if (ref($op) eq "HASH"){
-                     $op->{OP}=$mode         if (!exists($op->{OP}));
-                     $op->{IDENTIFYBY}=undef if (!exists($op->{IDENTIFYBY}));
-                     push(@{$opList},$op);
-                  }
-               }
-            }
-         }
-         if (defined($cmpRes{$refC."-".$cmpC})){
-            $found=1;
-            last cmpCloop;
-         }
-      }
-      if (!$found){
-         # do a delete
-         my $mode="delete";
-         foreach my $op (&{$fpRecGenerator}($mode,
-                                            $refList->[$refC],
-                                            undef,
-                                            %param)){
-            if (ref($op) eq "HASH"){
-               $op->{OP}=$mode         if (!exists($op->{OP}));
-               $op->{IDENTIFYBY}=undef if (!exists($op->{IDENTIFYBY}));
-               push(@{$opList},$op);
-            }
-         }
-      }
-   }
-   for(my $cmpC=0;$cmpC<=$#{$cmpList};$cmpC++){
-      my $found=0;
-      refCloop: for(my $refC=0;$refC<=$#{$refList};$refC++){
-         if (!exists($cmpRes{$refC."-".$cmpC})){
-            $a=$refList->[$refC];
-            $b=$cmpList->[$cmpC];
-            $cmpRes{$refC."-".$cmpC}=&{$fpComperator}();
-            if (defined($cmpRes{$refC."-".$cmpC}) && !$cmpRes{$refC."-".$cmpC}){
-               # do an update   
-               my $mode="update";
-               foreach my $op (&{$fpRecGenerator}($mode,
-                                                  $refList->[$refC],
-                                                  $cmpList->[$cmpC],
-                                                  %param)){
-                  if (ref($op) eq "HASH"){
-                     $op->{OP}=$mode         if (!exists($op->{OP}));
-                     $op->{IDENTIFYBY}=undef if (!exists($op->{IDENTIFYBY}));
-                     push(@{$opList},$op);
-                  }
-               }
-            }
-         }
-         if (defined($cmpRes{$refC."-".$cmpC})){
-            $found=1;
-            last refCloop;
-         }
-      }
-      if (!$found){
-         # do an insert
-         my $mode="insert";
-         foreach my $op (&{$fpRecGenerator}($mode,
-                                            undef,
-                                            $cmpList->[$cmpC],
-                                            %param)){
-            if (ref($op) eq "HASH"){
-               $op->{OP}=$mode         if (!exists($op->{OP}));
-               $op->{IDENTIFYBY}=undef if (!exists($op->{IDENTIFYBY}));
-               push(@{$opList},$op);
-            }
-         }
-      }
-   }
-}
-
-sub ProcessOpList
-{
-   my $self=shift;
-   my $opList=shift;
-   my $config=$self->Config;
-   my $objCache={};
-   msg(INFO,"ProcessOpList: Start");
-   foreach my $op (@{$opList}){
-      if (!exists($objCache->{$op->{DATAOBJ}})){
-         $objCache->{$op->{DATAOBJ}}=getModuleObject($config,$op->{DATAOBJ});
-      }
-      my $dataobj=$objCache->{$op->{DATAOBJ}};
-      if (defined($dataobj)){
-         $dataobj->ResetFilter();
-         msg(INFO,sprintf("OP:%s\n",Dumper($op)));
-         if ($op->{OP} eq "nop"){
-            msg(INFO,"skipped operation");
-         }
-         elsif ($op->{OP} eq "insert"){
-            my $id=$dataobj->ValidatedInsertRecord($op->{DATA});
-            $op->{IDENTIFYBY}=$id;
-            msg(INFO,"insert id ok = $id");
-         }
-         elsif ($op->{OP} eq "update"){
-            if ($op->{IDENTIFYBY} ne ""){
-               my $idfield=$dataobj->IdField();
-               my $idname=$idfield->Name();
-               $dataobj->SetFilter({$idname=>\$op->{IDENTIFYBY}});
-               my ($oldrec,$msg)=$dataobj->getOnlyFirst(qw(ALL));
-               my $id=$dataobj->ValidatedUpdateRecord($oldrec,$op->{DATA},
-                                                 {$idname=>\$op->{IDENTIFYBY}});
-               msg(INFO,"update id ok = $id");
-            }
-         }
-         elsif ($op->{OP} eq "delete"){
-            if ($op->{IDENTIFYBY} ne ""){
-               my $idfield=$dataobj->IdField();
-               my $idname=$idfield->Name();
-               $dataobj->SetFilter({$idname=>\$op->{IDENTIFYBY}});
-               my ($oldrec,$msg)=$dataobj->getOnlyFirst(qw(ALL));
-               my $id=$dataobj->ValidatedDeleteRecord($oldrec,
-                                                 {$idname=>\$op->{IDENTIFYBY}});
-               msg(INFO,"delete id ok = $id");
-            }
-         }
-      }
-   }
-   msg(INFO,"ProcessOpList: End");
 }
 
 
