3c21bb6e969eda1dfd7ef0d4db12db3f103bf02a
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2010-12-17 13:41:37 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@5810 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- lib/kernel/AutoFormater.pm
+++ lib/kernel/AutoFormater.pm
@@ -1,4 +1,4 @@
-package kernel::AutoFormat;
+package kernel::AutoFormater;
 #  W5Base Framework
 #  Copyright (C) 2010  Hartmut Vogler (it@guru.de)
 #
@@ -19,16 +19,78 @@ package kernel::AutoFormat;
 use strict;
 use vars qw(@ISA);
 use kernel;
+use kernel::XLSReport;
 
-# URL http://HOST/CONIFG/auth/MODULE/OBJECT/AutoFormat/ID/CLASS/File.xls
+#
+# Templates must be stored in webfs:/Template/CLASS/MODULE/APP/FILE.EXT
+#
+
+# URL http://HOST/CONIFG/auth/MODULE/APP/AutoFormat/ID/CLASS/FILE.EXT
+
+# is ViewValid syntax : AutoFormat AutoFormat.CLASS AutoFormat.CLASS.FILE
 
 sub AutoFormat
 {
    my ($self)=@_;
+   my ($func,$p)=$self->extractFunctionPath();
 
-   return($self->AutoFormatError("AutoFormat not implemented"));
+   if (my ($id,$class,$file,$ext)=$p
+       =~m#^/AutoFormat/([^/]+)/([^/]+)/([^\.]+)\.([a-z]{2,4})$#){
+      $class=~s/[^a-z0-9]//gi;
+      $ext=~s/[^a-z0-9]//gi;
+      $file=~s/[^a-z0-9]//gi;
+      $id=~s/[^a-z0-9]//gi;
+      my $idobj=$self->IdField();
+      if (!defined($idobj)){
+         return($self->AutoFormatError("ERROR: can not identify id field"));
+      }
+      my $idname=$idobj->Name();
+      $self->ResetFilter();
+      $self->SetFilter({$idname=>\$id});
+      my ($rec,$msg)=$self->getOnlyFirst(qw(ALL));
+      if (!defined($rec)){
+         return($self->AutoFormatError("ERROR: record not found by id='$id'"));
+      }
+      my @viewgroups=$self->isViewValid($rec);
+      if (!in_array(\@viewgroups,['ALL','AutoFormat','AutoFormat.'.$class,
+                                  'AutoFormat.'.$class.'.'.$file])){
+         return($self->AutoFormatError("ERROR: no access to AutoFormat file"));
+      }
+      if ($ext ne "xls"){
+         return($self->AutoFormatError(
+                "ERROR: AutoFormater only supports xls files"));
+      }
+      my $out;
+      if ($ext eq "xls"){
+         $out=new kernel::XLSReport($self,"$file.$ext");
+         $out->initWorkbook();
+      }
 
+      if ($self->ProcessAutoFormat($class,$file,$ext,$out)){
+         return;
+      }
+      my $msg="no ProcessAutoFormat implemented at current object";
+      return($self->AutoFormatError(
+             "AutoFormat: ".$msg."<br>".
+             "CLASS=$class ID=$id FILE=$file EXT=$ext<br>".
+             "Template='$tfile'<br>"));
+   }
+   return($self->AutoFormatError("AutoFormat '$p' not implemented"));
 }
+
+
+sub ProcessAutoFormat
+{
+   my $self=shift;
+   my ($class,$file,$ext,$out)=@_;
+
+   if ($class eq "Interview"){
+
+   }
+   return(0);
+}
+
+
 
 sub AutoFormatError
 {
