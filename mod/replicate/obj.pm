f6ec96a69155d1aef8f784e1eaf3083e446602bf
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2011-01-24 10:55:36 +0000

    add replication objects
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@5872 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ mod/replicate/obj.pm
@@ -0,0 +1,160 @@
+package replicate::obj;
+#  W5Base Framework
+#  Copyright (C) 2011  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::App::Web;
+use kernel::App::Web::Listedit;
+use kernel::DataObj::DB;
+use kernel::Field;
+use kernel::CIStatusTools;
+@ISA=qw(kernel::App::Web::Listedit kernel::DataObj::DB kernel::CIStatusTools);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   $self->AddFields(
+      new kernel::Field::Linenumber(
+                name          =>'linenumber',
+                label         =>'No.'),
+
+      new kernel::Field::Id(
+                name          =>'id',
+                uivisible     =>0,
+                sqlorder      =>'desc',
+                label         =>'W5BaseID',
+                dataobjattr   =>'replicateobject.id'),
+                                                  
+      new kernel::Field::Text(
+                name          =>'name',
+                label         =>'Dataobject name',
+                dataobjattr   =>'replicateobject.name'),
+
+      new kernel::Field::TextDrop(
+                name          =>'partner',
+                label         =>'Replication-Partner',
+                vjointo       =>'replicate::partner',
+                vjoinon       =>['partnerid'=>'id'],
+                vjoindisp     =>'name'),
+
+      new kernel::Field::Link(
+                name          =>'partnerid',
+                label         =>'Replication Partner ID',
+                dataobjattr   =>'replicateobject.replpartner'),
+
+      new kernel::Field::Boolean(
+                name          =>'allow_phase1',
+                label         =>'allow phase 1 (modified)',
+                dataobjattr   =>'replicateobject.allow_phase1'),
+
+      new kernel::Field::Boolean(
+                name          =>'allow_phase2',
+                label         =>'allow phase 2 (refresh)',
+                dataobjattr   =>'replicateobject.allow_phase2'),
+
+      new kernel::Field::Boolean(
+                name          =>'allow_phase3',
+                label         =>'allow phase 3 (cleanup)',
+                dataobjattr   =>'replicateobject.allow_phase3'),
+
+      new kernel::Field::CDate(
+                name          =>'cdate',
+                group         =>'source',
+                sqlorder      =>'desc',
+                label         =>'Creation-Date',
+                dataobjattr   =>'replicateobject.createdate'),
+                                                  
+      new kernel::Field::MDate(
+                name          =>'mdate',
+                group         =>'source',
+                sqlorder      =>'desc',
+                label         =>'Modification-Date',
+                dataobjattr   =>'replicateobject.modifydate'),
+
+      new kernel::Field::Creator(
+                name          =>'creator',
+                group         =>'source',
+                label         =>'Creator',
+                dataobjattr   =>'replicateobject.createuser'),
+
+      new kernel::Field::Owner(
+                name          =>'owner',
+                group         =>'source',
+                label         =>'Owner',
+                dataobjattr   =>'replicateobject.modifyuser'),
+
+      new kernel::Field::Editor(
+                name          =>'editor',
+                group         =>'source',
+                label         =>'Editor',
+                dataobjattr   =>'replicateobject.editor'),
+
+      new kernel::Field::RealEditor(
+                name          =>'realeditor',
+                group         =>'source',
+                label         =>'RealEditor',
+                dataobjattr   =>'replicateobject.realeditor'),
+
+   );
+   $self->setDefaultView(qw(linenumber partner name allow_phase1 
+                            allow_phase2 allow_phase3 cdate));
+   $self->setWorktable("replicateobject");
+   return($self);
+}
+
+sub Validate
+{
+   my $self=shift;
+   my $oldrec=shift;
+   my $newrec=shift;
+
+   my $name=trim(effVal($oldrec,$newrec,"name"));
+   if ($name=~m/\s/i){
+      $self->LastMsg(ERROR,"invalid name '%s' specified",$name); 
+      return(undef);
+   }
+   $newrec->{'name'}=$name;
+   return(1);
+}
+
+
+sub isViewValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return("header","default") if (!defined($rec) && $self->IsMemberOf("admin"));
+   return("ALL");
+}
+
+sub isWriteValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return("default") if ($self->IsMemberOf("admin"));
+   return(undef);
+}
+
+
+
+
+
+1;
---
+++ sql/replicate/repl.sql
@@ -0,0 +1,51 @@
+use w5base;
+set FOREIGN_KEY_CHECKS=0;
+CREATE TABLE replicatepartner (
+  id   bigint(20)    NOT NULL,
+  name varchar(128)  NOT NULL,cistatus   int(2)      NOT NULL,
+  comments           longtext  default NULL,
+  createdate datetime NOT NULL default '0000-00-00 00:00:00',
+  modifydate datetime NOT NULL default '0000-00-00 00:00:00',
+  createuser bigint(20) default NULL,
+  modifyuser bigint(20) default NULL,
+  editor     varchar(100) NOT NULL default '',
+  realeditor varchar(100) NOT NULL default '',
+  PRIMARY KEY  (id),
+  UNIQUE KEY name (name)
+) ENGINE=INNODB;
+CREATE TABLE replicateobject (
+  id   bigint(20)    NOT NULL default '0',
+  name varchar(40)   NOT NULL,
+  replpartner        bigint(20)    NOT NULL,
+  replview           longtext  default NULL,
+  allow_phase1       int(1)    default '1',
+  allow_phase2       int(1)    default '1',
+  allow_phase3       int(1)    default '1',
+  entrycount         bigint(20) default '0',
+  comments    longtext  default NULL,
+  createdate datetime NOT NULL default '0000-00-00 00:00:00',
+  modifydate datetime NOT NULL default '0000-00-00 00:00:00',
+  createuser bigint(20) default NULL,
+  modifyuser bigint(20) default NULL,
+  editor     varchar(100) NOT NULL default '',
+  realeditor varchar(100) NOT NULL default '',
+  PRIMARY KEY  (id),
+  KEY replpartner (replpartner),
+  UNIQUE KEY name (name)
+) ENGINE=INNODB;
+CREATE TABLE replicatestat (
+  id   bigint(20)    NOT NULL default '0',
+  replobject         bigint(20)    NOT NULL,
+  phase              int(1)   NOT NULL,
+  startdate          datetime NOT NULL default '0000-00-00 00:00:00',
+  enddate            datetime NOT NULL default '0000-00-00 00:00:00',
+  duration           int(15)  NOT NULL,
+  effentries         bigint(20)    NOT NULL,
+  PRIMARY KEY  (id),
+  KEY replobject (replobject)
+) ENGINE=INNODB;
+alter table replicatestat  add FOREIGN KEY fk_replicatestat (replobject)
+          REFERENCES replicateobject (id) ON DELETE CASCADE;
+alter table replicateobject  add FOREIGN KEY fk_replicateobject (replpartner)
+          REFERENCES replicatepartner (id) ON DELETE CASCADE;
+set FOREIGN_KEY_CHECKS=1;
