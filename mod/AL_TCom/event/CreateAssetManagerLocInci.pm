47eed3c6ebbb259f97cb7923ea25a39c7240b67b
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2011-05-02 13:15:12 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@6122 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/AL_TCom/event/CreateAssetManagerLocInci.pm
+++ mod/AL_TCom/event/CreateAssetManagerLocInci.pm
@@ -48,7 +48,11 @@ sub CreateAssetManagerLocInci
    my $self=shift;
    my $acloc=getModuleObject($self->Config,"tsacinv::location");
    my $w5loc=getModuleObject($self->Config,"base::location");
-   my @checkmap=('DE.Bietigheim-Bissingen.Hauptstrasse_30' =>
+   my @checkmap=('DE.Muenster.Wolbecker_Strasse_268' =>
+                      '/DE_MÜNSTER_WOLBECKERSTR_268/*',
+                 'DE.Donauwoerth.Reichsstrasse_24' =>
+                      '/DE_DONAUWÖRTH_REICHSTR_24/*',
+                 'DE.Bietigheim-Bissingen.Hauptstrasse_30' =>
                       '/DE_BIETIGHEIM_HAUPTSTR_30/*',
                  'DE.Frankfurt_am_Main.Hahnstrasse_43.T-Systems'=>
                       '/DE*FRANKFURT*HAHN*43*',
@@ -80,86 +84,123 @@ sub ProcessLocationCompare
    msg(INFO,"checking:");
    msg(INFO," * $w5rec->{name}");
    msg(INFO,"   -> $acrec->{fullname}");
-   my @entxt="";
+   my @entxt;
    foreach my $f (@fieldset){
-      if ($w5rec->{$f} ne $acrec->{$f} && $w5rec->{$f} ne ""){
+      my $accmp=$acrec->{$f};
+      my $w5cmp=$w5rec->{$f};
+      if ($f eq "zipcode"){
+         $accmp=~s/D-//;
+         $w5cmp=~s/D-//;
+      }
+      msg(INFO,"cmp $accmp && $w5cmp");
+      if ($w5cmp ne $accmp && $w5cmp ne ""){
          push(@entxt,"- Change the value of the field '$f' from\n".
-                     "  '$acrec->{$f}' to '$w5rec->{$f}'\n");
+                     "  '$acrec->{$f}' to '$w5cmp'\n");
       }
    }
    if ($#entxt!=-1){
-      my $itxt="The information on masterdata of location ".
+      my $itxt="The postal address information on masterdata of location ".
                "(Code:$acrec->{code})\n".
                "$acrec->{fullname}\n".
                "is incorrect.\n".
-               "Please correct the following field values on data\n".
+               "Please correct the following field values on data ".
                "block 'postal address':\n".
                join("\n",@entxt)."\n".
-               "If you have any questions in relation to this correction,\n".
+               "If you have any questions in relation to this correction, ".
                "feel free to contact me by phone!\n";
-      print "-----------------------------------------".
-            "-------------------------------\n".
-            $itxt.
-            "-----------------------------------------".
-            "-------------------------------\n";
-      my $sc;
-      my $msg;
-      my $IncidentNumber;
-      eval('use SC::API;$sc=new SC::API;');
-      if (!defined($sc)){
-         msg(ERROR,"can't connect to SC::API");
-         exit(1);
-      }
-      my $SCuri=$self->Config->Param("DATAOBJCONNECT");
-      my $SCuser=$self->Config->Param("DATAOBJUSER");
-      my $SCpass=$self->Config->Param("DATAOBJPASS");
-      $SCpass=$SCpass->{tsscui} if (ref($SCpass) eq "HASH");
-      $SCuser=$SCuser->{tsscui} if (ref($SCuser) eq "HASH");
-      $SCuri=$SCuri->{tsscui}   if (ref($SCuri) eq "HASH");
-      if (!$sc->Connect($SCuri,$SCuser,$SCpass)){
-         msg(ERROR,"ServiceCenter Connect failed");
-         exit(1);
-      }
-      else{
-         msg(INFO,"ServiceCenter Connect OK");
-      }
-      if (!$sc->Login()){
-         msg(ERROR,"ServiceCenter Login failed");
-         exit(1);
-      }
-      else{
-         msg(INFO,"ServiceCenter Login OK");
-      }
-      my %Incident=('brief.description'  =>'Correction of location '.
-                                           'informations '.$acrec->{fullname},
-                    'problem.shortname'  =>'TS_DE_BAMBERG_GUTENBERG_13',
-                    'assignment'         =>'CSS.TCOM.ST.DB',
-                    'category'           =>'SOFTWARE','subcategory1'=>'OTHER',
-                    'reported.lastname'  =>'VOGLER',
-                    'contact.lastname'   =>'VOGLER',
-                    'contact.name'       =>'HVOGLER',
-                    'reported.by'        =>'HVOGLER',
-                    'action'             =>$itxt);
-      if (!defined($IncidentNumber=$sc->IncidentCreate(\%Incident))){
+      my $isub='AssetManager CSS: '.
+               'correction of location informations '.
+               $acrec->{fullname}.
+               " - Location code:".$acrec->{code};
+      msg(INFO,"open new connection for incident handling ...");
+      sleep(4);
+      if (0){
+         print "-----------------------------------------".
+               "-------------------------------\n".
+               $itxt.
+               "-----------------------------------------".
+               "-------------------------------\n";
+         my $sc;
+         my $msg;
+         my $IncidentNumber;
+         eval('use SC::Customer::TSystems;$sc=new SC::Customer::TSystems;');
+         if (!defined($sc)){
+            msg(ERROR,"can't connect to SC::API");
+            exit(1);
+         }
+         my $SCuri=$self->Config->Param("DATAOBJCONNECT");
+         my $SCuser=$self->Config->Param("DATAOBJUSER");
+         my $SCpass=$self->Config->Param("DATAOBJPASS");
+         $SCpass=$SCpass->{tsscui} if (ref($SCpass) eq "HASH");
+         $SCuser=$SCuser->{tsscui} if (ref($SCuser) eq "HASH");
+         $SCuri=$SCuri->{tsscui}   if (ref($SCuri) eq "HASH");
+         msg(INFO,"user: ".$SCuser);
+         msg(INFO,"pass: ".$SCpass);
+         msg(INFO,"uri:  ".$SCuri);
+         if (!$sc->Connect($SCuri,$SCuser,$SCpass)){
+            msg(ERROR,"ServiceCenter Connect failed");
+            exit(1);
+         }
+         else{
+            msg(INFO,"ServiceCenter Connect OK");
+         }
+         if (!$sc->Login()){
+            msg(ERROR,"ServiceCenter Login failed");
+            exit(1);
+         }
+         else{
+            msg(INFO,"ServiceCenter Login OK");
+         }
+         my %Incident=(
+            'brief.description'      =>$isub,
+            'problem.shortname'      =>'TS_DE_BAMBERG_GUTENBERG_13',
+            'assignment'             =>'OE.OIM.OSM.F2R.AM-CSS-SK',
+            'home.assignment'        =>'CSS.AO.DTAG.W5BASE',
+            'category'               =>'SERVICE REQUEST',
+            'subcategory1'           =>'ACCESS',
+            'subcategory2'           =>'OTHER',
+            'subcategory3'           =>'OTHER',
+            'category.type'          =>'I_TOOLSUITE-REQUEST',
+            'cause.code'             =>'AM.LOC2',
+            'urgency'                =>'Medium',
+            'reported.lastname'      =>'VOGLER',
+            'dsc.restriction.degree' =>'80',
+            'current.priority'       =>'3',
+            'dsc.service'            =>'ASSETMANAGER_CSS_PROD (APPL008612)',
+            'device.name'            =>'ASSETMANAGER_CSS_PROD (APPL008612)',
+            'referral.no'            =>"W5Base",
+            'contact.lastname'       =>'VOGLER',
+            'contact.phone'          =>'+491709215495',
+            'contact.name'           =>'HVOGLER',
+            'reported.by'            =>'HVOGLER',
+            'action'                 =>$itxt);
+         if (!defined($IncidentNumber=$sc->IncidentCreate(\%Incident))){
+            $msg=$sc->LastMessage();
+            printf STDERR ("ERROR: ServiceCenter CreateIncident failed\n".
+                           $msg."\n");
+            $sc->Logout();
+            exit(1);
+         }
          $msg=$sc->LastMessage();
-         printf STDERR ("ERROR: ServiceCenter CreateIncident failed\n$msg\n");
-         $sc->Logout();
-         exit(1);
-      }
-      $msg=$sc->LastMessage();
-      printf("INFO:  CreateIncident is ok\n");
-      printf("INFO:  Incident Number=%s\n",$IncidentNumber);
-      printf("INFO:  %s\n",$msg);
-      if (!$sc->Logout()){
-         msg(ERROR,"ServiceCenter Logout failed");
-         exit(1);
+         printf("INFO:  CreateIncident is ok\n");
+         printf("INFO:  Incident Number=%s\n",$IncidentNumber);
+         printf("INFO:  %s\n",$msg);
+         if (!$sc->Logout()){
+            msg(ERROR,"ServiceCenter Logout failed");
+            exit(1);
+         }
+         else{
+            msg(INFO,"ServiceCenter Logout OK");
+         }
       }
       else{
-         msg(INFO,"ServiceCenter Logout OK");
+         my $act=getModuleObject($self->Config,"base::workflowaction");
+         $act->Notify('',$isub,$itxt,
+                      emailfrom=>'"ConfigSync" <>',
+                      emailto=>['11634953080001'], 
+                      xadminbcc=>1,
+                     );
       }
-
-      exit(0);
-
    }
 
 }
