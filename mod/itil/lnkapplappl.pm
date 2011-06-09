b0e704fa12bc18348d42597ca09f52b740ef3910
w5base <system@w5base.net>
2011-06-09 06:24:26 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@6188 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/itil/lnkapplappl.pm
+++ mod/itil/lnkapplappl.pm
@@ -77,7 +77,7 @@ sub new
                                      RMI rsh rcp rfc sldap ssh sftp smtp
                                      snmp tuxedo xml X.31 openFT pkix-cmp
                                      utm UCP/SMS DB-Link DB-Connection 
-                                     BCV MFT MQSeries)],
+                                     BCV MFT MQSeries OTHER)],
                 default       =>'online',
                 htmlwidth     =>'50px',
                 htmleditwidth =>'150px',
