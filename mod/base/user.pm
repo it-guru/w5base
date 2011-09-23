f63033a72fabeb306290cab1e5765eb3a2d69f0e
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2011-09-23 12:24:36 +0000

    fix based on data security
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@6414 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/base/user.pm
+++ mod/base/user.pm
@@ -399,36 +399,36 @@ sub new
                 label         =>'privat Phonenumber',
                 dataobjattr   =>'contact.private_phone'),
 
-      new kernel::Field::Text(
-                name          =>'persidentno',
-                group         =>'personrelated',
-                label         =>'personal identifier number',
-                dataobjattr   =>'contact.persidentno'),
+#      new kernel::Field::Text(
+#                name          =>'persidentno',
+#                group         =>'personrelated',
+#                label         =>'personal identifier number',
+#                dataobjattr   =>'contact.persidentno'),
 
       new kernel::Field::Date(
                 name          =>'dateofbirth',
                 dayonly       =>1,
-                group         =>'personrelated',
+                group         =>'private',
                 label         =>'date of birth',
                 dataobjattr   =>'contact.dateofbirth'),
 
-      new kernel::Field::Text(
-                name          =>'driverlicno',
-                group         =>'personrelated',
-                label         =>'driver license number',
-                dataobjattr   =>'contact.driverlicno'),
-
-      new kernel::Field::Text(
-                name          =>'eurocarno',
-                group         =>'personrelated',
-                label         =>'EURO Car-rent driver number',
-                dataobjattr   =>'contact.eurocarno'),
-
-      new kernel::Field::Text(
-                name          =>'sixtcarno',
-                group         =>'personrelated',
-                label         =>'SIXT Car-rent driver number',
-                dataobjattr   =>'contact.sixtcarno'),
+#      new kernel::Field::Text(
+#                name          =>'driverlicno',
+#                group         =>'personrelated',
+#                label         =>'driver license number',
+#                dataobjattr   =>'contact.driverlicno'),
+#
+#      new kernel::Field::Text(
+#                name          =>'eurocarno',
+#                group         =>'personrelated',
+#                label         =>'EURO Car-rent driver number',
+#                dataobjattr   =>'contact.eurocarno'),
+#
+#      new kernel::Field::Text(
+#                name          =>'sixtcarno',
+#                group         =>'personrelated',
+#                label         =>'SIXT Car-rent driver number',
+#                dataobjattr   =>'contact.sixtcarno'),
 
       new kernel::Field::Date(
                 name          =>'dateofworksafty',
