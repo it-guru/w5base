e62028593c794262b4b79cabf1c5ca886fe6b495
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2012-04-27 14:43:03 +0000

    add a universal WebDialer for Google Chrome as extension - first build
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@6769 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ contrib/chromeExtension/uniWebDial/contentscript.js
@@ -0,0 +1,61 @@
+/*
+   Copyright (C) 2012  Hartmut Vogler (it@guru.de)
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+
+chrome.extension.sendRequest({ action: 'options' }, function (response) {
+
+    var contentExp='([\+][0-9]{1,2}[0-9, ]{5,20})';
+    var contentExp = RegExp(contentExp, 'gm');
+
+    var intlReplacement="<span class='uniWebDial'>$&</span>";
+
+    var found = false;
+
+//    // Test the text of the body element against our international regular expression.
+//    var directhr_softphone_meta = $('meta[name=directhr_telephone_plugin]').attr("content");
+//    if (directhr_softphone_meta != undefined && directhr_softphone_meta.toLowerCase().indexOf("[disabled]") >= 0) {
+//        console.log('directhr meta was found, disabled value was found, plugin on this page was disabled');
+//        return;
+//    }
+
+
+
+    if (contentExp.test(document.body.innerText)) {
+        $(document).find(':not(textarea)').replaceText(
+                         contentExp,intlReplacement);
+        $(".uniWebDial").css({cursor:'pointer',
+                              color:'blue'});
+        var tURL=response.options.tURL;
+        $(".uniWebDial").click(function() {
+          var num=$(this).text();
+          num=num.replace(/[ \t\(\)]/g,"");
+          num=num.replace(/^\+49/,"0");
+          var callURL=tURL;
+          callURL=callURL.replace(/%phonenumber%/g,num);
+          var win=window.open(callURL,'_blank',
+                   'height=480,width=640,toolbar=no,status=no,'+
+                   'location=no,resizable=yes,scrollbars=no,'+
+                   'menubar=no');
+        });
+        found = true;
+    }
+    if (found) {
+        chrome.extension.sendRequest({action:'showPageAction' },function(){});
+    }
+});
---
+++ contrib/chromeExtension/uniWebDial/options.js
@@ -0,0 +1,82 @@
+/*
+   Copyright (C) 2012  Hartmut Vogler (it@guru.de)
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+function getDirectDial() {
+   return function (info, tab) {
+      var tURL=localStorage['tURL'] || defaults['tURL'];
+      var url = 'http://127.0.0.1:7069/Dial/' + info.selectionText;
+      url=tURL;
+      // Create a new window to the info page.
+      chrome.windows.create({ url: url, width: 520, height: 660 });
+   };
+};
+
+
+/**
+* Create a context menu which will only show up for images.
+*/
+chrome.contextMenus.create({
+    "title": "Dial %s",
+    "type": "normal",
+    "contexts": ["selection"],
+    "onclick": getDirectDial()
+});
+
+
+
+var defaults = {
+    //intlRegex: '([\+]{0,1}86[0-9. -]{10,18})|([0-9]{2,4}[. -]{0,1}[0-9]{7,8})|(400|800)([2-9][0-9]{6,11})|(1[3-9][0-9]{9})|([0-9]{7,8})',
+    intlRegex: '([\+][0-9]{1,2}[0-9, ]{5,20})',
+    intlReplacement: '$1$2$3$4$5$6',
+    tURL: 'https://meintelefon2.telekom.de/webdialer/Webdialer?destination=0%phonenumber%'
+}
+
+
+function loadOptions() {
+   var tURL = localStorage['tURL'] || defaults['tURL'];
+   document.getElementById('tURL').value=tURL.toString();
+}
+
+
+function saveOptions() {
+   try {
+      localStorage['tURL']=document.getElementById('tURL').value;
+      setStatus('Options Saved.');
+   } catch (error) {
+      alert(error);
+   }
+}
+
+
+function setStatus(message) {
+  var status = document.getElementById('status');
+  status.innerHTML = message;
+  setTimeout(function() {
+    status.innerHTML = '';
+  }, 4000);
+}
+
+
+function clearData() {
+  if (confirm('Clear data in extension? (includes extension settings)')) {
+    localStorage.clear();
+    alert('Extension data cleared.');
+  }
+}
+
