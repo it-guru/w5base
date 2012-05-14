dba9e1ec20e9c94a0e4f1ba23d4c06a8be484566
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2012-05-14 11:47:17 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@6803 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ mod/TS/event/NOR_Report.pm
@@ -0,0 +1,131 @@
+package TS::event::NOR_Report;
+#  W5Base Framework
+#  Copyright (C) 2006  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::Event;
+use kernel::XLSReport;
+
+@ISA=qw(kernel::Event);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   return($self);
+}
+
+sub Init
+{
+   my $self=shift;
+
+
+   $self->RegisterEvent("NOR_Report","NOR_Report");
+   return(1);
+}
+
+sub NOR_Report
+{
+   my $self=shift;
+   my %param=@_;
+   my %flt;
+   msg(DEBUG,"param=%s",Dumper(\%param));
+   if ($param{customer} ne ""){
+      my $c=$param{customer};
+      $flt{customer}="$param{customer} $param{customer}.*";
+   }
+   else{
+      msg(DEBUG,"no customer restriction");
+   }
+   if ($param{name} ne ""){
+      my $c=$param{name};
+      $flt{name}="$param{name}";
+   }
+   else{
+      msg(DEBUG,"no name restriction");
+   }
+
+   $flt{mandator}="!Extern";
+   $flt{cistatusid}="<=5";
+
+   my $appl=getModuleObject($self->Config,"itil::appl");
+   $appl->SetFilter(\%flt);
+   my $t=$appl->getHashIndexed(qw(id));
+
+   %flt=(srcparentid=>[keys(%{$t->{id}})]);
+
+
+   if ($param{'filename'} eq ""){
+      my $names;
+      if ($param{customer} ne ""){
+         $names.="_" if ($names ne "");
+         $names="CUSTOMER-$param{customer}";
+      }
+      if ($param{name} ne ""){
+         $names.="_" if ($names ne "");
+         $names.="APP-$param{name}";
+      }
+      $names="ALL" if ($names eq "");
+      $names=substr($names,0,40)."___" if (length($names)>40);
+      $names=~s/[^a-z0-9]/_/gi;
+      $param{'filename'}="/tmp/W5BaseDarwin-NOR-Report_${names}.xls";
+   }
+   msg(INFO,"start Report to $param{'filename'}");
+ 
+
+   my $out=new kernel::XLSReport($self,$param{'filename'});
+   $out->initWorkbook();
+
+   $flt{'isactive'}='1 [EMPTY]'; 
+
+   my @control=({DataObj=>'TS::appladv',
+                 filter=>\%flt,
+                 view=>[qw(fullname id name isactive dstate databoss 
+                           modules normodelbycustomer itnormodel 
+                           processingpersdata scddata)]},
+
+                {DataObj=>'TS::applnor',
+                 filter=>\%flt,
+                 view=>[qw(fullname id name custcontract isactive 
+                           dstate databoss normodel
+                           SUMMARYdeliveryCountry
+                           SUMMARYisCountryCompliant
+                           SUMMARYisSCDconform
+                           SUMMARYappliedNOR mdate owner)]},
+                );
+
+   $out->Process(@control);
+
+
+#   $self->{workbook}=$self->XLSopenWorkbook();
+#   if (!($self->{workbook}=$self->XLSopenWorkbook())){
+#      return({exitcode=>1,msg=>'fail to create tempoary workbook'});
+#   }
+
+
+   return({exitcode=>0,msg=>'OK'});
+}
+
+
+
+
+
+1;
