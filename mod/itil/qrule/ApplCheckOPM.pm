43a9afff91bb2daf73b75fae1e17c8c7b8aa5fe8
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2012-11-19 14:01:48 +0000

    add TSM/OPM QualityRule based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/13062221320003
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@7131 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ mod/itil/qrule/ApplCheckOPM.pm
@@ -0,0 +1,72 @@
+package itil::qrule::ApplCheckOPM;
+#######################################################################
+=pod
+
+=head3 PURPOSE
+
+Every Application in CI-Status "installed/active","inactiv/stored" 
+or "available", must have a Operation Manager.
+
+=head3 IMPORTS
+
+NONE
+
+=cut
+#######################################################################
+#  W5Base Framework
+#  Copyright (C) 2007  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::QRule;
+@ISA=qw(kernel::QRule);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   return($self);
+}
+
+sub getPosibleTargets
+{
+   return(["itil::appl"]);
+}
+
+sub qcheckRecord
+{
+   my $self=shift;
+   my $dataobj=shift;
+   my $rec=shift;
+
+   return(0,undef) if ($rec->{cistatusid}!=3 && 
+                       $rec->{cistatusid}!=4 &&
+                       $rec->{cistatusid}!=5);
+   if (!defined($rec->{opm}) || $rec->{opm} eq ""){
+      return(3,{qmsg=>['no Operation Manager defined'],
+                dataissue=>['no Operation Manager defined']});
+   }
+   return(0,undef);
+
+}
+
+
+
+1;
