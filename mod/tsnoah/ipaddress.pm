95a906e76ee3b8660b834d577e64e5629ef5bcee
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2013-07-29 14:14:42 +0000

    first modules of tsnoah interface - very minimal
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@7682 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ mod/tsnoah/ipaddress.pm
@@ -0,0 +1,114 @@
+package tsnoah::ipaddress;
+#  W5Base Framework
+#  Copyright (C) 2013  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::App::Web;
+use kernel::DataObj::DB;
+use kernel::Field;
+@ISA=qw(kernel::App::Web::Listedit kernel::DataObj::DB);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   $param{MainSearchFieldLines}=4;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   $self->AddFields(
+      new kernel::Field::Id(
+                name          =>'id',
+                sqlorder      =>'desc',
+                label         =>'ID',
+                dataobjattr   =>"ip_id"),
+
+      new kernel::Field::Text(
+                name          =>'name',
+                label         =>'IP-Address',
+                dataobjattr   =>'ip_adresse'),
+
+      new kernel::Field::Text(
+                name          =>'systemname',
+                label         =>'Systemname',
+                vjointo       =>'tsnoah::system',
+                vjoinon       =>['systemid'=>'id'],
+                vjoindisp     =>'name',
+                uppersearch   =>1),
+
+      new kernel::Field::Link(
+                name          =>'systemid',
+                dataobjattr   =>'device_id'),
+
+      new kernel::Field::Date(
+                name          =>'mdate',
+                group         =>'source',
+                label         =>'Modification-Date',
+                dataobjattr   =>'timestamp'),
+
+   );
+   $self->setWorktable("TSIIMP.darwin_ip");
+   $self->setDefaultView(qw(name systemname mdate));
+   return($self);
+}
+
+
+sub Initialize
+{
+   my $self=shift;
+
+   my @result=$self->AddDatabase(DB=>new kernel::database($self,"tsinet"));
+   return(@result) if (defined($result[0]) eq "InitERROR");
+   return(1) if (defined($self->{DB}));
+   return(0);
+}
+
+
+sub getDetailBlockPriority
+{
+   my $self=shift;
+   my $grp=shift;
+   my %param=@_;
+   return("header","default");
+}
+
+
+sub getRecordImageUrl
+{
+   my $self=shift;
+   my $cgi=new CGI({HTTP_ACCEPT_LANGUAGE=>$ENV{HTTP_ACCEPT_LANGUAGE}});
+   return("../../../public/itil/load/ipaddress.jpg?".$cgi->query_string());
+}
+         
+
+sub isViewValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return("ALL");
+}
+
+sub isWriteValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return(undef);
+}
+
+
+1;
---
+++ mod/tsnoah/system.pm
@@ -0,0 +1,129 @@
+package tsnoah::system;
+#  W5Base Framework
+#  Copyright (C) 2013  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::App::Web;
+use kernel::DataObj::DB;
+use kernel::Field;
+@ISA=qw(kernel::App::Web::Listedit kernel::DataObj::DB);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   $param{MainSearchFieldLines}=4;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   $self->AddFields(
+      new kernel::Field::Id(
+                name          =>'id',
+                sqlorder      =>'desc',
+                label         =>'ID',
+                dataobjattr   =>"id"),
+
+      new kernel::Field::Text(
+                name          =>'name',
+                sqlorder      =>'desc',
+                label         =>'Systemname',
+                dataobjattr   =>'systemname'),
+
+      new kernel::Field::Text(
+                name          =>'systemid',
+                label         =>'SystemID',
+                dataobjattr   =>'systemid'),
+
+      new kernel::Field::Text(
+                name          =>'techconcept',
+                label         =>'technical concept number',
+                dataobjattr   =>'techconcept'),
+
+      new kernel::Field::Date(
+                name          =>'mdate',
+                group         =>'source',
+                label         =>'Modification-Date',
+                dataobjattr   =>'mdate'),
+
+   );
+   $self->setDefaultView(qw(name systemid techconcept));
+   return($self);
+}
+
+
+sub Initialize
+{
+   my $self=shift;
+
+   my @result=$self->AddDatabase(DB=>new kernel::database($self,"tsinet"));
+   return(@result) if (defined($result[0]) eq "InitERROR");
+   return(1) if (defined($self->{DB}));
+   return(0);
+}
+
+sub getSqlFrom
+{
+   my $self=shift;
+   my $from=
+      "(select device_id id, 
+               devicename systemname,
+               devicetyp systemtyp,
+               service_id systemid,
+               wm_concat(distinct fachkonzept) techconcept,
+               max(tsiimp.darwin_device.timestamp) mdate 
+       from tsiimp.darwin_device 
+       group by device_id,devicename,devicetyp,service_id) lsys";
+
+   return($from);
+}
+
+
+
+sub getDetailBlockPriority
+{
+   my $self=shift;
+   my $grp=shift;
+   my %param=@_;
+   return("header","default");
+}
+
+
+sub getRecordImageUrl
+{
+   my $self=shift;
+   my $cgi=new CGI({HTTP_ACCEPT_LANGUAGE=>$ENV{HTTP_ACCEPT_LANGUAGE}});
+   return("../../../public/itil/load/system.jpg?".$cgi->query_string());
+}
+         
+
+sub isViewValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return("ALL");
+}
+
+sub isWriteValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return(undef);
+}
+
+
+1;
