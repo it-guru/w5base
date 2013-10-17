b4ecda86ecd5a79e878b878c36464df82b38b925
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2013-10-17 10:55:35 +0000

    fix username display in msg messages
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@7795 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- lib/W5Kernel.pm
+++ lib/W5Kernel.pm
@@ -25,7 +25,7 @@ sub msg
    return("") if ($type eq "DEBUG" && $W5V2::Debug==0);
    my $d;
    my $u="";
-   $u=" <$ENV{REMOTE_USER}>" if ($ENV{REMOTE_USER} ne "");
+   $u=" ($ENV{REMOTE_USER})" if ($ENV{REMOTE_USER} ne "");
    foreach my $linemsg (split(/\n/,$msg)){
       $d.=sprintf("%-6s %s%s\n",$type.":",$linemsg,$u);
    }
