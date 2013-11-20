c0eadf0f9dd2157088a8e6391905b8aadda8f8d3
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2013-11-20 07:30:15 +0000

    remove topappl from menu https://darwin.telekom.de/darwin/auth/base/workflow/ById/13843308420003
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@7859 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/TS/menu/root.pm
+++ mod/TS/menu/root.pm
@@ -74,13 +74,13 @@ sub Init
                       func=>'New',
                       defaultacl=>['valid_user']);
 
-   $self->RegisterObj("itts.appl.prio",
-                      "TS::topappl",
-                      defaultacl=>['valid_user']);
-
-   $self->RegisterObj("itts.appl.prio.groups",
-                      "TS::topapplgroups",
-                      defaultacl=>['valid_user']);
+#   $self->RegisterObj("itts.appl.prio",
+#                      "TS::topappl",
+#                      defaultacl=>['valid_user']);
+#
+#   $self->RegisterObj("itts.appl.prio.groups",
+#                      "TS::topapplgroups",
+#                      defaultacl=>['valid_user']);
 
    $self->RegisterObj("itts.appl.lnksystem",
                       "itil::lnkapplsystem",
