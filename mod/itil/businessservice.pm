9e74d7f3515f2ab8fe3025bcab0a08f15916d4ff
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2014-07-01 08:41:37 +0000

    add replication keys for itil::businessservice
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@8208 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/itil/businessservice.pm
+++ mod/itil/businessservice.pm
@@ -678,6 +678,18 @@ sub new
                 label         =>'Source-Load',
                 dataobjattr   =>$worktable.'.srcload'),
 
+      new kernel::Field::Interface(
+                name          =>'replkeypri',
+                group         =>'source',
+                label         =>'primary sync key',
+                dataobjattr   =>"$worktable.modifydate"),
+
+      new kernel::Field::Interface(
+                name          =>'replkeysec',
+                group         =>'source',
+                label         =>'secondary sync key',
+                dataobjattr   =>"lpad($worktable.id,35,'0')"),
+
       new kernel::Field::CDate(
                 name          =>'cdate',
                 group         =>'source',
