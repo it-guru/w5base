72d2e46aa72c5663934bc479a468da2f520d60c3
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2014-08-01 09:27:28 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@8283 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- static/xor/xor.js
+++ static/xor/xor.js
@@ -3,7 +3,6 @@ function encrypt(str, pwd){
    for (var i=0;i<pwd.length;i++){
       prand+=pwd.charCodeAt(i).toString();
    }
-   console.log("prand",prand);
    var sPos=Math.floor(prand.length / 5);
 
    var mult=parseInt(prand.charAt(sPos)+prand.charAt(sPos*2)+
@@ -16,25 +15,21 @@ function encrypt(str, pwd){
             "a more complex or longer password.");
       return null;
    }
-   console.log("mult",mult);
 
    var incr=Math.ceil(pwd.length / 2);
    var modu=Math.pow(2, 31) - 1;
    var salt=Math.round(Math.random()*1000000000)%100000000;
-   console.log("salt",salt);
    prand+=salt;
-   console.log("prand",prand);
    while(prand.length>10){
-      prand=(parseBigInt(prand.substring(0, 10),16)+
-             parseBigInt(prand.substring(10,prand.length),16)).toString();
-      console.log("1prand",prand);
+      var a=parseBigInt(prand.substring(0, 10));	   
+      var b=parseBigInt(prand.substring(10, prand.length));	   
+      var c=a.add(b);
+      prand=c.toString();
    }
-   console.log("2prand",prand);
    prand=(mult*prand+incr)%modu;
    var enc_chr="";
    var enc_str="";
    for (var i=0;i<str.length;i++){
-   console.log("i",i);
       enc_chr=parseInt(str.charCodeAt(i)^Math.floor((prand / modu) * 255));
       if (enc_chr<16){
          enc_str+="0"+enc_chr.toString(16);
@@ -71,25 +66,18 @@ function decrypt(str, pwd) {
    var mult=parseInt(prand.charAt(sPos)+
             prand.charAt(sPos*2)+prand.charAt(sPos*3)+
             prand.charAt(sPos*4) + prand.charAt(sPos*5));
-   console.log("mult",mult);
    var incr=Math.round(pwd.length / 2);
-   console.log("incr",incr);
    var modu=Math.pow(2, 31) - 1;
-   console.log("modu",modu);
    var salt=parseInt(str.substring(str.length - 8, str.length), 16);
-   console.log("salt",salt);
    str=str.substring(0, str.length - 8);
    prand+=salt;
-   console.log("prand pre while",prand);
    while(prand.length>10){
-      console.log("prand:"+prand);
-      console.log("s1:"+prand.substring(0, 10));
-      console.log("s2:"+prand.substring(10, prand.length));
-      prand=(parseBigInt(prand.substring(0, 10),16)+ 
-             parseBigInt(prand.substring(10, prand.length)),16).toString();
+      var a=parseBigInt(prand.substring(0, 10));	   
+      var b=parseBigInt(prand.substring(10, prand.length));	   
+      var c=a.add(b);
+      prand=c.toString();
    }
    prand=(mult*prand+incr)%modu;
-   console.log("prand",prand);
    var enc_chr="";
    var enc_str="";
    for(var i=0;i<str.length;i+=2){
@@ -101,7 +89,11 @@ function decrypt(str, pwd) {
    return(enc_str);
 }
 
+function parseBigInt(str) {
+   return new BigInteger(str,16);
+}
+
 function parseBigInt(str,r) {
-  return new BigInteger(str,r);
+   return new BigInteger(str,r);
 }
 
--- static/xor/xor.pl
+++ static/xor/xor.pl
@@ -1,83 +1,85 @@
 #!/usr/bin/perl
 use strict;
-use Math::Round qw/round/;
+use POSIX;
 use Math::BigInt;
 
 my $code="Cryptkey";
-my $ctxt="9b81c57692d8d0f1f7ba73ff2eb1eda6e0b3d7061b042b1f17";
+my $ctxt="a7a02122c7ad1477c7e1b0b91ea46fbbb03054b87a00f0eead";
 
+my $ptext = decrypt($code,$ctxt);
+printf("text=%s\n",$ptext);
+my $ctext =encrypt($code,$ptext);
+printf("text=%s\n",$ctext);
+printf("text=%s\n",decrypt($code,$ctext));
 
-printf("text=%s\n",decrypt($code,$ctxt));
-
-sub decrypt
+sub encrypt
 {
    my $code=shift;
-   my $ctext=shift;
-   printf("fifi: ctext=$ctext\n");
+   my $text=shift;
    my $prand="";
    foreach my $c (split(//,$code)){ $prand.=ord($c);}
-   printf("fifi: prand=$prand\n");
-
    my $sPos=int(length($prand)/5);
-
-   printf("fifi: sPos=$sPos\n");
-
    my $mult=int(substr($prand,$sPos,1).
                 substr($prand,$sPos*2,1).
                 substr($prand,$sPos*3,1).
                 substr($prand,$sPos*4,1).
                 substr($prand,$sPos*5,1));
-
-   printf("fifi: mutl=$mult\n");
-
-   my $incr=round(length($code)/2);
-
-   printf("fifi: incr=$incr\n");
-
+   my $incr=ceil(length($code)/2);
    my $modu=2**31-1;
-   printf("fifi: modu=$modu\n");
-   
-
-   my $salt=hex(substr($ctext,length($ctext)-8));
-
-   printf("fifi: salt=$salt\n");
-
-   my $str=substr($ctext,0,length($ctext)-8);
-   printf("fifi: str=$str\n");
-
+   my $salt=sprintf("%.0f",(rand()*1000000000)%100000000);
    $prand.=$salt;
-   printf("prand pre while %s\n" ,$prand);
    while(length($prand)>10){
-      printf("1prand:".$prand."\n");
       my $a=Math::BigInt->new(substr($prand,0,10));
       my $b=Math::BigInt->new(substr($prand,10));
-
-      print("s1:".$a."\n");
-      print("s2:".$b."\n");
-      $prand=$a+$b;
-      print($prand,"\n--\n"); 
-      printf("2prand:".$prand."\n");
+      $prand=($a+$b);
    }
+   $prand=sprintf("%d",$prand);
    $prand=($mult*$prand+$incr)%$modu;
-   printf("fifi: prand=$prand\n");
-
-
    my $enc_str="";
+   foreach my $c (split(//,$text)){
+      my $x = floor(($prand / $modu) * 255);
+      my $enc_chr = int(ord($c)^$x);
+      $enc_str .= sprintf("%02x", $enc_chr);
+      $prand=($mult*$prand+$incr)%$modu;
+   }
+   $salt = sprintf("%08x",$salt);
+   $enc_str.=$salt;
+   return($enc_str);
+}
 
+sub decrypt
+{
+   my $code=shift;
+   my $ctext=shift;
+   my $prand="";
+   foreach my $c (split(//,$code)){ $prand.=ord($c);}
+   my $sPos=int(length($prand)/5);
+   my $mult=int(substr($prand,$sPos,1).
+                substr($prand,$sPos*2,1).
+                substr($prand,$sPos*3,1).
+                substr($prand,$sPos*4,1).
+                substr($prand,$sPos*5,1));
+   my $incr=ceil(length($code)/2);
+   my $modu=2**31-1;
+   my $salt=hex(substr($ctext,length($ctext)-8));
+   my $str=substr($ctext,0,length($ctext)-8);
+   $prand.=$salt;
+   while(length($prand)>10){
+      my $a=Math::BigInt->new(substr($prand,0,10));
+      my $b=Math::BigInt->new(substr($prand,10));
+      $prand=($a+$b);
+   }
+   $prand=sprintf("%d",$prand);
+   $prand=($mult*$prand+$incr)%$modu;
+   my $enc_str="";
    for(my $i=0;$i<length($str);$i+=2){
       my $h=substr($str,$i,2);
       my $d=hex($h);
-      printf("fifi h=$h d=$d\n");
-      my $enc_chr=int(hex(substr($str,$i,2))^int(($prand/$modu)*255));
-      $enc_str.=chr($enc_chr);
+      my $x = floor(($prand / $modu) * 255);
+      $enc_str.=chr($d ^ $x);
+      $prand=($mult*$prand+$incr)%$modu;
    }
-   printf("fifi enc_str=$enc_str\n");
-
-
+   return($enc_str);
 }
-
-
-
-
 
 1;
