d85d518fb7012e24cfc57d8f65c487aa080371c8
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2014-09-22 11:46:21 +0000

    add W5Warehouse systemcheck interface
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@8411 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ mod/W5Warehouse/system.pm
@@ -0,0 +1,107 @@
+package W5Warehouse::system;
+#  W5Base Framework
+#  Copyright (C) 2014  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::App::Web;
+use kernel::DataObj::DB;
+use kernel::Field;
+@ISA=qw(kernel::App::Web::Listedit kernel::DataObj::DB);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   $param{MainSearchFieldLines}=4;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   
+   $self->AddFields(
+      new kernel::Field::Linenumber(
+                name          =>'linenumber',
+                label         =>'No.'),
+
+      new kernel::Field::Id(
+                name          =>'id',
+                label         =>'ID',
+                group         =>'source',
+                align         =>'left',
+                dataobjattr   =>"(systemid||'-'||systemname)"),
+
+      new kernel::Field::Text(
+                name          =>'systemname',
+                label         =>'Systemname',
+                lowersearch   =>1,
+                size          =>'16',
+                dataobjattr   =>'systemname'),
+
+      new kernel::Field::Text(
+                name          =>'systemid',
+                label         =>'SystemId',
+                searchable    =>1,
+                uppersearch   =>1,
+                align         =>'left',
+                dataobjattr   =>'systemid'),
+
+      new kernel::Field::Text(
+                name          =>'saphier',
+                label         =>'SAP Hier',
+                uppersearch   =>1,
+                align         =>'left',
+                dataobjattr   =>'saphier'),
+
+      new kernel::Field::Boolean(
+                name          =>'is_w5',
+                label         =>'found in W5Base/Darwin',
+                dataobjattr   =>'is_w5'),
+
+      new kernel::Field::Boolean(
+                name          =>'is_am',
+                label         =>'found in AssetManager',
+                dataobjattr   =>'is_am'),
+
+      new kernel::Field::Boolean(
+                name          =>'is_t4dp',
+                label         =>'found in TAD4D Prod',
+                dataobjattr   =>'is_t4dp'),
+
+      new kernel::Field::Boolean(
+                name          =>'is_t4di',
+                label         =>'found in TAD4D Integ',
+                dataobjattr   =>'is_t4di'),
+
+   );
+   $self->setWorktable("system_universum");
+   $self->setDefaultView(qw(systemname systemid is_w5 is_am is_t4dp is_t4di
+                            saphier));
+   return($self);
+}
+
+
+sub Initialize
+{
+   my $self=shift;
+
+   my @result=$self->AddDatabase(DB=>new kernel::database($self,"w5warehouse"));
+   return(@result) if (defined($result[0]) eq "InitERROR");
+   return(1) if (defined($self->{DB}));
+   return(0);
+}
+
+1;
