413101e5ce146cc75fe9c2fea3993062d36461d9
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2014-09-23 10:43:05 +0000

    add qualityRule to ensure completness of logical systems of one costobject based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/14087131770001
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@8416 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ mod/tsacinv/qrule/checkSystemsComplete.pm
@@ -0,0 +1,151 @@
+package tsacinv::qrule::checkSystemsComplete;
+#######################################################################
+=pod
+
+=head3 PURPOSE
+
+Checks if all systems on a specifice costobject are documented in
+it-invtory.
+All systems in AM are defined as ...
+state="!out of operation"
+systemola=!*ONLY
+... as requested in ...
+https://darwin.telekom.de/darwin/auth/base/workflow/ById/14087131770001
+
+
+=head3 IMPORTS
+
+NONE
+
+=head3 HINTS
+
+Es müssen ALLE Systeme die in AssetManager dem aktuellen 
+Kontierungsobjekt zugeordnet sind, auch in W5Base/Darwin
+unter IT-Inventar->System erfasst sein!
+
+Achtung: Sollte es sich um Infrastruktursysteme handeln, so 
+müssen diese auch als solche in Darwin gekennzeichnet werden 
+(Systemklassifizierung).
+
+
+Die Datenerfassung von logischen Systemen in Darwin erfolgt
+i.d.R. durch den TSM der betreffenden Anwendung. Der 
+Datenverantwortliche des Kontierungsobjektes hat die 
+Verantwortung einen TSM zu finden, der die logischen Systeme
+erfasst. Alternativ muß der Datenverantwortliche des 
+Kontierungsobjektes die betreffenden logischen Systeme
+selbst erfassen und in Darwin pflegen!
+
+=cut
+#######################################################################
+#
+#  W5Base Framework
+#  Copyright (C) 2014  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::QRule;
+@ISA=qw(kernel::QRule);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   return($self);
+}
+
+sub getPosibleTargets
+{
+   return(["finance::costcenter"]);
+}
+
+
+sub qcheckRecord
+{
+   my $self=shift;
+   my $dataobj=shift;
+   my $rec=shift;
+   my $checksession=shift;
+   my $autocorrect=$checksession->{autocorrect};
+
+   my $wfrequest={};
+   my $forcedupd={};
+   my @qmsg;
+   my @dataissue;
+   my $errorlevel=0;
+
+
+   my $o=getModuleObject($self->getParent->Config,"tsacinv::system");
+
+   printf STDERR ("fifi 01 $self\n");
+
+   my $co=$rec->{name};
+   $co=~s/^[a-z]-//i;
+   $co=~s/-.*$//;
+
+   $o->SetFilter({
+      status=>'!"out of operation"',
+      systemola=>'!*ONLY',
+      conumber=>$co
+   });
+   my @soll=$o->getHashList(qw(systemname systemid));
+
+   print STDERR Dumper(\@soll);
+   my %sollsystemid;
+
+   foreach my $r (@soll){
+      $sollsystemid{$r->{systemid}}=$r->{systemname} if ($r->{systemid} ne "");
+   }
+   if (keys(%sollsystemid)){
+      my $o=getModuleObject($self->getParent->Config,"itil::system");
+      $o->SetFilter({
+         systemid=>[keys(%sollsystemid)],
+         conumber=>\$rec->{name}
+      });
+      my @ist=$o->getHashList(qw(name systemid));
+      my %istsystemid;
+      foreach my $r (@ist){
+         $istsystemid{$r->{systemid}}++ if ($r->{systemid} ne "");
+      }
+      my @miss;
+      foreach my $r (sort(keys(%sollsystemid))){
+         if (!exists($istsystemid{$r})){
+            push(@miss,$sollsystemid{$r}." ($r)");
+         }
+      }
+      if ($#miss!=-1){
+         $errorlevel=3;
+         foreach my $s (@miss){
+            my $msg="missing logical system in it-invtory: ".$s;
+            push(@qmsg,$msg);
+            push(@dataissue,$msg);
+         }
+      }
+   }
+   my @result=$self->HandleQRuleResults("AssetManagerCDS",
+                 $dataobj,$rec,$checksession,
+                 \@qmsg,\@dataissue,\$errorlevel,$wfrequest,$forcedupd);
+
+   return(@result);
+}
+
+
+
+1;
