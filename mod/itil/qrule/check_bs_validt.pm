aed881e7db216ae99c511dded18a1c4fcb50c3b1
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2014-09-24 08:57:49 +0000

    add QualityRule to check duration from/to in businessservices
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@8430 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ mod/itil/qrule/check_bs_validt.pm
@@ -0,0 +1,98 @@
+package itil::qrule::check_bs_validt;
+#######################################################################
+=pod
+
+=head3 PURPOSE
+
+Checks if a Business-Service in "installed/active" state is between
+the validfrom and validto range.
+
+=head3 IMPORTS
+
+NONE
+
+=cut
+#######################################################################
+#  W5Base Framework
+#  Copyright (C) 2014  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::QRule;
+@ISA=qw(kernel::QRule);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   return($self);
+}
+
+sub getPosibleTargets
+{
+   return(["itil::businessservice"]);
+}
+
+sub qcheckRecord
+{
+   my $self=shift;
+   my $dataobj=shift;
+   my $rec=shift;
+   my @msg;
+
+   return(0,undef) if ($rec->{cistatusid}!=4);
+
+   my $validfrom=$rec->{validfrom};
+   my $validto=$rec->{validto};
+
+   my $valid=1;
+   if ($validfrom ne ""){
+      my $duration=CalcDateDuration($validfrom,NowStamp("en"));
+      if ($duration->{totalminutes}<0){
+         $valid=0;
+      }
+      print STDERR "from:".Dumper($duration); 
+   }
+   if ($validto ne ""){
+      my $duration=CalcDateDuration(NowStamp("en"),$validto);
+      if ($duration->{totalminutes}<0){
+         $valid=0;
+      }
+      print STDERR "to:".Dumper($duration); 
+   }
+   if (!$valid){
+      push(@msg,"businessservice outside duration start/end ".
+                "in state installed/active");
+   }
+
+
+
+
+
+   if ($#msg!=-1){
+      return(3,{qmsg=>\@msg, dataissue=>\@msg});
+   }
+   return(0,undef);
+
+}
+
+
+
+1;
