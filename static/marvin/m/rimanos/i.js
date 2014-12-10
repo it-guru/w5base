e2f9a6d8b781cd62a6426ad10dbaf889dbf0985a
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2014-12-10 15:17:14 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@8653 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- static/marvin/m/rimanos/i.js
+++ static/marvin/m/rimanos/i.js
@@ -1,16 +1,146 @@
-var W5Base;
-
 
 function Application(){
-   this.run=function(){
-      var J5BaseConnect={ 
-            useUTF8:false,
-            mode:'auth',
-            transfer:'JSON',
-            baseURL:J5Base_baseUrl
-      };
-      eval("W5Base=createConfig(J5BaseConnect,J5Base_baseUrl);");
+
+   this.searchApplication=function(){
+      var listview="name,id";
+      var detailview="name,description,id";
+      var curview=listview;
+      var o=this.W5Base().getModuleObject("itil::appl");
+     
+      if (0){ // directer call
+         curview=detailview;
+         // url auswerten, welche ID geladen werden soll
+      }
+      else{
+         var search_name=$('#search_application_name').val();
+         o.SetFilter({name:search_name, cistatusid:"!6"});
+      }
+      o.findRecord(curview,function(l){
+         if (l.length==0){
+            alert("not found");
+         }
+         else if (l.length==1){
+            if (curview!=detailview){
+               o.SetFilter({id:l[0].id});  // detail Felder hinzuladen
+               o.findRecord(detailview,displayApplication);
+            }
+            else{  // direct anzeigen
+               displayApplication(l);
+            }
+         }
+         else{
+            $.mobile.navigate("#applist");
+            var label="Anwendungsliste";
+            $("#applist-content").html("");
+            var ul=$('<ul id="listview" data-role="listview" '+
+                     'data-inset="true" style="height:100%"/>');
+            ul.append("<li data-role='list-divider'>"+label+"</li>");
+            for(c=0;c<l.length;c++){
+               var li=$('<li />').attr({
+                  id:l[c].id,
+                  name:l[c].name
+               });
+               li.html(App.searchApplicationListEntry(l[c]));
+               ul.append(li);
+            }
+            $("#applist-content").append(ul);
+            $('#applist').trigger("create");
+         }
+      });
+      return(false);
    }
+
+   this.displayApplication=function(rec){
+      $.mobile.navigate("#appdetail");
+      $("#appdetail-content").html("OK found "+rec[0].name);
+   }
+
+
+   this.searchApplicationListEntry=function(rec){
+      var d="<a class='list-href' rel='external' id='"+rec.id+"' "+
+            "href='?A:"+rec.id+"#applist"+
+            "'>"+rec.name+"</a>";
+      return(d);
+   }
+
+
+
+   this.searchTopLists=function(){
+      var listview="name,applications,id";
+      var detailview="name,description,id";
+      var curview=listview;
+      $.mobile.loading('show');
+      var o=this.W5Base().getModuleObject("itil::mgmtitemgroup");
+     
+      if (0){ // directer call
+         curview=detailview;
+         // url auswerten, welche ID geladen werden soll
+      }
+      else{
+         o.SetFilter({name:"top*", cistatusid:"4"});
+      }
+      o.findRecord(curview,function(l){
+         $.mobile.loading('hide');
+         if (l.length==0){
+            alert("not found");
+         }
+         else if (l.length==1){
+            if (curview!=detailview){
+               o.SetFilter({id:l[0].id});  // detail Felder hinzuladen
+               o.findRecord(detailview,App.displayTopList);
+            }
+            else{  // direct anzeigen
+               displayTopList(l);
+            }
+         }
+         else{
+            var label="Top-Listen:";
+            var ul=$('<ul id="listview" data-role="listview" '+
+                     'data-inset="true" style="height:100%"/>');
+            ul.append("<li data-role='list-divider'>"+label+"</li>");
+            for(c=0;c<l.length;c++){
+               if (l[c].applications.length){ 
+                  var li=$('<li />').attr({
+                     id:l[c].id,
+                     name:l[c].name
+                  });
+                  li.html(App.searchTopListsEntry(l[c]));
+                  ul.append(li);
+               }
+            }
+            $("#toplists-content").append(ul);
+            $('#toplists').trigger("create");
+         }
+      });
+      return(false);
+   }
+
+   this.displayTopList=function(rec){
+      $.mobile.navigate("#toplist");
+      $("#toplist-content").html("OK found "+rec[0].name);
+   }
+
+
+   this.searchTopListsEntry=function(rec){
+      var d="<a class='list-href' rel='external' id='"+rec.id+"' "+
+            "href='?T:"+rec.id+"#toplist"+
+            "'>"+rec.name+"</a>";
+      return(d);
+   }
+
+
+
+
+
+   $('#toplists').live('pagebeforeshow',function(event, ui){
+      $("#toplists-content").html("");
+   });
+
+   $('#toplists').live('pageshow',function(event, ui){
+       App.searchTopLists();
+   });
+
+
    $("input").keypress(function(event) {
      //  if (event.which == 13) {
      //     console.log("key 13 event");
@@ -22,71 +152,6 @@ function Application(){
    return(this);
 };
 
-
-function displayApplication(rec)
-{
-   $.mobile.navigate("#appdetail");
-   $("#appdetail-content").html("OK found "+rec[0].name);
-
-}
-
-function searchApplication()
-{
-   var listview="name,id";
-   var detailview="name,description,id";
-   var curview=listview;
-   var o=W5Base.getModuleObject("itil::appl");
-
-   if (0){ // directer call
-      curview=detailview;
-      // url auswerten, welche ID geladen werden soll
-   }
-   else{
-      var search_name=$('#search_application_name').val();
-      o.SetFilter({name:search_name, cistatusid:"!6"});
-   }
-   o.findRecord(curview,function(l){
-      if (l.length==0){
-         alert("not found");
-      }
-      else if (l.length==1){
-         if (curview!=detailview){
-            o.SetFilter({id:l[0].id});  // detail Felder hinzuladen
-            o.findRecord(detailview,displayApplication);
-         }
-         else{  // direct anzeigen
-            displayApplication(l);
-         }
-      }
-      else{
-         $.mobile.navigate("#applist");
-         var label="Anwendungsliste";
-         $("#applist-content").html("");
-         var ul=$('<ul id="listview" data-role="listview" '+
-                  'data-inset="true" style="height:100%"/>');
-         ul.append("<li data-role='list-divider'>"+label+"</li>");
-         for(c=0;c<l.length;c++){
-            var li=$('<li />').attr({
-               id:l[c].id,
-               name:l[c].name
-            });
-            li.html(searchApplicationListEntry(l[c]));
-            ul.append(li);
-         }
-         $("#applist-content").append(ul);
-         $('#applist').trigger("create");
-      }
-   });
-   return(false);
-}
-
-function searchApplicationListEntry(rec)
-{
-   var d="<a class='list-href' rel='external' id='"+rec.id+"' "+
-         "href='?searchApplication("+rec.id+")#applist"+
-         "'>"+rec.name+"</a>";
-   return(d);
-}
 
 
 
--- static/marvin/m/root/run.js
+++ static/marvin/m/root/run.js
@@ -17,6 +17,28 @@ i18n.init({
    }
    },function (t){
       App=new Application();   // Appl Objekt erzeugen
+
+      if (!App.W5Base){
+         App.W5Base=function(){
+            if (!this._W5Base){
+               var tempW5Base;
+               var J5BaseConnect={
+                     useUTF8:false,
+                     mode:'auth',
+                     transfer:'JSON',
+                     baseURL:J5Base_baseUrl
+               };
+               eval("tempW5Base=createConfig(J5BaseConnect,J5Base_baseUrl);");
+               this._W5Base=tempW5Base;
+            }
+            return(this._W5Base);
+         }
+      }
+      if (!App.run){
+         App.run=function(){};
+      }
+
+
       $("*").i18n();           // Übersetzungen durchführen
       $("*[i18n]").hide();                   // i18n Elemente ausblenden
       $("*[i18n|='"+i18n.lng()+"']").show(); // die passenden i18n Elemente anz.
@@ -32,5 +54,16 @@ i18n.init({
       window.setTimeout(function(){App.run();},1000);
    }
 );
+
+
+$(document).on( "mobileinit", function() {
+   // ----------------------------------------------------
+   // configure Loading box
+   $.mobile.loader.prototype.options.text = "loading ...";
+   $.mobile.loader.prototype.options.textVisible = true;
+   $.mobile.loader.prototype.options.theme = "b";
+   $.mobile.loader.prototype.options.html = "";
+   // ----------------------------------------------------
+});
 
 
