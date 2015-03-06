a88b2fa91b169b82c0a94127583e246ef4f49171
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2015-03-06 11:03:17 +0000

    tcc Report interpreation based on infos from Fr. Wiebe - Telko from 26.02.
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@8790 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/tssmartcube/tcc.pm
+++ mod/tssmartcube/tcc.pm
@@ -547,7 +547,7 @@ sub new
 
    );
    $self->setWorktable("tcc_report");
-   $self->setDefaultView(qw(systemid systemname roadmap_color check_status_color));
+   $self->setDefaultView(qw(systemid systemname roadmap check_status_color));
    return($self);
 }
 
