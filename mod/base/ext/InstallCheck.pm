06db24d7f8520ae89b3ee5f05df7591b0607d0fc
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2015-06-12 11:28:14 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@9032 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/base/ext/InstallCheck.pm
+++ mod/base/ext/InstallCheck.pm
@@ -49,7 +49,7 @@ sub getOptionalModules
 
    return qw(Spreadsheet::WriteExcel::Big 
              Spreadsheet::ParseExcel Spreadsheet::ParseExcel::Utility
-             DTP JSON PDF::Reuse
+             DTP JSON PDF::Reuse Spreadsheet::ParseExcel::Workbook
              Digest::SHA1 HTML::TreeBuilder  HTML::FormatText
              pdflib_pl DTP::pdf Archive::Zip
              GD DTP::jpg DTP::png);
