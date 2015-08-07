c5532e8f33d274d85c18af192c64ba622e781568
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2015-08-07 08:26:08 +0000

    add IssueState for base::grp
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@9112 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/base/grp.pm
+++ mod/base/grp.pm
@@ -331,6 +331,7 @@ sub new
 
       new kernel::Field::QualityText(),
       new kernel::Field::QualityState(),
+      new kernel::Field::IssueState(),
       new kernel::Field::QualityOk(),
       new kernel::Field::QualityLastDate(
                 dataobjattr   =>'grp.lastqcheck'),
