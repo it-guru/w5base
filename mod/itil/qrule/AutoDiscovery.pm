08a499eb20872c382160729e83c54f38ee7e261c
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2015-08-12 13:56:46 +0000

    1st sone release of AutoDiscoveryManager commited - this is no prod version!
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@9122 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ mod/itil/qrule/AutoDiscovery.pm
@@ -0,0 +1,356 @@
+package itil::qrule::AutoDiscovery;
+#######################################################################
+=pod
+
+=head3 PURPOSE
+
+Every Application in in CI-Status "installed/active" or "available", needs
+to set a valid primary operation mode.
+
+=head3 IMPORTS
+
+NONE
+
+=cut
+#######################################################################
+#  W5Base Framework
+#  Copyright (C) 2007  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::QRule;
+@ISA=qw(kernel::QRule);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   return($self);
+}
+
+sub getPosibleTargets
+{
+   return(['itil::system','itil::swinstance']);
+}
+
+sub qenrichRecord
+{
+   my $self=shift;
+   my $dataobj=shift;
+   my $rec=shift;
+   my $checksession=shift;
+
+   return($self->qcheckRecord($dataobj,$rec,$checksession));
+}
+
+
+sub qcheckRecord
+{
+   my $self=shift;
+   my $dataobj=shift;
+   my $rec=shift;
+   my $checksession=shift;
+   my $dataobjname=$dataobj->SelfAsParentObject();
+
+   return(0) if ($rec->{cistatusid}==6);
+
+   my $ade=getModuleObject($dataobj->Config,"itil::autodiscengine");
+   if (defined($ade)){ # itil:: seems to be installed
+      print STDERR ("fifi 01\n");
+      my @AdPreData=();
+      my %engines;
+      $ade->SetFilter({localdataobj=>\$dataobjname,
+                   #    addataobj=>\'TAD4D::system',
+                       cistatusid=>\'4'});
+      foreach my $engine ($ade->getHashList(qw(ALL))){  # found aktive Engine
+         $engines{$engine->{id}}={
+            name=>$engine->{name}
+         };
+         #print STDERR Dumper($engine);
+         my $ado=getModuleObject($dataobj->Config,$engine->{addataobj});
+         if (!exists($rec->{$engine->{localkey}})){
+            # autodisc key data does not exists in local object
+            msg(ERROR,"preQualityCheckRecord failed for $dataobjname ".
+                      "local key $engine->{localkey} does not exists");
+            next;
+         }
+         if ($rec->{$engine->{localkey}} ne ""){
+            if (defined($ado)){  # check if autodisc object name is OK
+               if ($ado->can("extractAutoDiscData")){
+                  my $adokey=$ado->getField($engine->{adkey});
+                  if (defined($adokey)){ # check if autodisc key is OK
+                     $ado->SetFilter({
+                        $engine->{adkey}=>\$rec->{$engine->{localkey}}
+                     });
+                     push(@AdPreData,map({
+                        $_->{engineid}=$engine->{id};
+                        $_;
+                     } $ado->extractAutoDiscData()));
+                  #   }
+                  }
+               }
+               else{
+                  msg(ERROR,"$ado is not AutoDiscovery compatibel : extractAutoDiscData");
+               }
+            }
+         }
+      }
+      #
+      # create Software-Mappings and remove douplicate informations
+      #
+      $self->MapAutoDiscoveryPreData($dataobj,$rec,\@AdPreData); 
+
+
+      #
+      # load entry records based on engineids
+      #
+printf STDERR ("fifi AutoDisc 01\n");
+
+      my %adentry=();
+      {
+         my $ade=getModuleObject($dataobj->Config,'itil::autodiscent');
+         foreach my $adrec (grep({$_->{valid}} @AdPreData)){
+            if (!exists($adentry{$adrec->{engineid}})){
+               $ade->ResetFilter();
+               $ade->SetFilter({
+                  disc_on_systemid=>\$rec->{id},
+                  engineid=>$adrec->{engineid}
+               });
+               my ($adent)=$ade->getOnlyFirst(qw(ALL));
+              
+               if (!defined($adent)){
+                  if ($ade->ValidatedInsertRecord({
+                         disc_on_systemid=>$rec->{id},
+                         engineid=>$adrec->{engineid}
+                      })){
+                     ($adent)=$ade->getOnlyFirst(qw(ALL));
+                  }
+               }
+               $adentry{$adrec->{engineid}}=$adent;
+            }
+         }
+      }
+
+      #
+      # load old discovery entries
+      #
+printf STDERR ("fifi AutoDisc 02\n");
+
+
+      my %oldrecs;
+      my $ad=getModuleObject($dataobj->Config,'itil::autodiscrec');
+      $ad->ResetFilter();
+      $ad->SetFilter({
+         disc_on_systemid=>\$rec->{id},
+         #entryid=>[map({$_->{id}} values(%adentry))]
+      });
+      foreach my $r ($ad->getHashList(qw(ALL))){
+         $oldrecs{$r->{id}}={
+            state=>$r->{state},
+            misscount=>$r->{misscount},
+            scanextra1=>$r->{scanextra1},
+            scanextra2=>$r->{scanextra2}
+         };
+      }
+
+printf STDERR ("fifi AutoDisc 03\n");
+      #
+      # process all autodiscovery entries
+      #
+      foreach my $adrec (grep({$_->{valid}} @AdPreData)){
+         print STDERR Dumper($adrec);
+         my $adent=$adentry{$adrec->{engineid}};
+         $self->DiscoverData($ad,$rec,$adrec,$adent,
+                             $engines{$adrec->{engineid}},\%oldrecs);
+      }
+
+
+      #
+      # cleanup old autodiscovery entries
+      #
+printf STDERR ("oldrecs=%s\n",Dumper(\%oldrecs));
+
+      foreach my $id (keys(%oldrecs)){
+         if ($oldrecs{$id}->{misscount}>2){
+            $ad->BulkDeleteRecord({id=>\$id});
+         }
+         else{
+            $ad->UpdateRecord({misscount=>\'misscount+1'},{id=>\$id});
+         }
+      }
+
+
+
+      #printf STDERR ("AdPreData:%s\n",Dumper(\@AdPreData));
+   }
+   return(0);
+}
+
+sub MapAutoDiscoveryPreData
+{
+   my $self=shift;
+   my $dataobj=shift;
+   my $rec=shift;
+   my $AdPreData=shift;
+
+   my @flt;
+
+   foreach my $ad (@{$AdPreData}){
+      $ad->{valid}=1;
+      if ($ad->{section} eq "SOFTWARE"){
+         push(@flt,{
+            engineid=>\$ad->{engineid},
+            scanname=>\$ad->{scanname},
+            softwareid=>'![EMPTY]'
+         });
+      }
+   }
+   my %ADMAP;
+
+   if ($#flt!=-1){
+      my $admap=getModuleObject($dataobj->Config,'itil::autodiscmap');
+      $admap->SetFilter(\@flt);
+      $admap->SetCurrentView(qw(id engineid scanname));
+      foreach my $m ($admap->getHashList(qw(engineid scanname softwareid))){
+         my $k=$m->{engineid}.";".$m->{scanname};
+         if (!exists($ADMAP{$k})){
+            $ADMAP{$k}=[$m->{softwareid}];
+         }
+         else{
+            push(@{$ADMAP{$k}},$m->{softwareid});
+         }
+      }
+   }
+   foreach my $ad (@{$AdPreData}){    # Level1: Filtern der gemappten Software
+      my $k=$ad->{engineid}.";".$ad->{scanname};
+      if ($ad->{section} eq "SOFTWARE"){ # software needs a mappging
+         if (!exists($ADMAP{$k})){
+            $ad->{valid}=0;
+         }
+         else{
+            $ad->{alt}=$ADMAP{$k};
+         }
+      }
+      if ($ad->{section} eq "SYSTEMNAME"){ 
+         if ($ad->{scanname}=~m/\./){  # systemname with dots are not allowed
+            $ad->{valid}=0;
+         }
+         if ($rec->{name} eq lc($ad->{scanname})){  #system already ok
+            $ad->{valid}=0;
+         }
+      }
+   }
+
+   my %paths;
+   foreach my $ad (@{$AdPreData}){    # Level2: Mögliche Pfade detectieren
+      next if (!$ad->{valid});
+      my @targets=($ad->{scanname});
+      if (exists($ad->{alt})){
+         @targets=@{$ad->{alt}};
+      }
+      foreach my $target (@targets){
+         my $t=$ad->{scanname}." - ".$target;
+         if (!exists($paths{$t})){
+            $paths{$t}=$ad;
+         }
+         else{
+            if ($paths{$t}->{quality}==$ad->{quality} &&   # Geliche Engine
+                $paths{$t}->{engineid}==$ad->{engineid} && # mit untersch.
+                $paths{$t}->{scanextra1} ne $ad->{scanextra1}){ # produkten
+                $t.=" @ ".$ad->{scanextra1};
+                $paths{$t}=$ad;
+            }
+            else{
+               if ($paths{$t}->{quality}<$ad->{quality}){
+                  $paths{$t}=$ad;
+               }
+            }
+         }
+      }
+   }
+   printf STDERR ("paths:\n%s\n",join("\n",keys(%paths)));
+   foreach my $ad (values(%paths)){
+      $ad->{valid}++;
+   }
+   #print STDERR Dumper(\%ADMAP);
+}
+
+
+
+
+
+
+sub DiscoverData
+{
+   my $self=shift;
+   my $ad=shift;
+   my $rec=shift;
+   my $adrec=shift;
+   my $adent=shift;
+   my $engine=shift;
+   my $oldrecs=shift;
+
+   my $flt={
+      section=>\$adrec->{section},
+      scanname=>\$adrec->{scanname},
+      entryid=>\$adent->{id}
+   };
+   my $newrec={
+      section=>$adrec->{section},
+      scanname=>$adrec->{scanname},
+      entryid=>$adent->{id},
+      srcsys=>$engine->{name},
+      srcload=>NowStamp("en")
+   };
+
+   if (exists($adrec->{scanextra1})){
+      my $scanextra1=limitlen($adrec->{scanextra1},128,1);
+      $flt->{scanextra1}=\$scanextra1;
+      $newrec->{scanextra1}=$scanextra1;
+   }
+   if (exists($adrec->{scanextra2})){
+      $newrec->{scanextra2}=$adrec->{scanextra2};
+   }
+   $ad->ResetFilter();
+   $ad->SetFilter($flt);
+   my @l=$ad->getHashList(qw(ALL));
+   if ($#l==-1){
+      $ad->ValidatedInsertRecord($newrec);
+   }
+   else{
+      foreach my $r (@l){
+         my %updrec=();
+         if ($oldrecs->{$r->{id}}->{misscount}>0){
+            $updrec{misscount}=0;
+         }
+         if ($oldrecs->{$r->{id}}->{scanextra2} ne $adrec->{scanextra2}){
+            $updrec{scanextra2}=$adrec->{scanextra2};
+         }
+         $updrec{srcload}=NowStamp("en");
+         if (keys(%updrec)){
+printf STDERR ("Start Update in $ad %s\n",Dumper(\%updrec));
+            $ad->ValidatedUpdateRecord($r,\%updrec,{id=>$r->{id}});
+         }
+         delete($oldrecs->{$r->{id}});
+      }
+   }
+}
+
+1;
