2fa6b0821614101f47a70d47c3830c73d7814548
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2015-10-23 11:22:15 +0000

    add SearchReplace function to PassX Acls based on request https://darwin.telekom.de/darwin/auth/base/workflow/ById/14455099680001
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@9260 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ mod/passx/ext/ReplaceTool.pm
@@ -0,0 +1,94 @@
+package passx::ext::ReplaceTool;
+#  W5Base Framework
+#  Copyright (C) 2015  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::Universal;
+@ISA=qw(kernel::Universal);
+
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless({%param},$type);
+   return($self);
+}
+
+sub getControlRecord
+{
+   my $self=shift;
+   my $d=[ 
+           userpassxacl=>{
+             replaceoptype=>'base::user',
+             dataobj      =>'passx::acl',
+             target       =>'acltargetname',
+             idfield      =>'acltargetid',
+             targetlabel  =>'aclid',
+             baseflt      =>{aclparentobj=>\'passx::entry',
+                             acltarget=>\'base::user'}
+           },
+           grppassxacl=>{
+             replaceoptype=>'base::grp',
+             dataobj      =>'passx::acl',
+             target       =>'acltargetname',
+             idfield      =>'acltargetid',
+             targetlabel  =>'aclid',
+             baseflt      =>{aclparentobj=>\'passx::entry',
+                             acltarget=>\'base::grp'}
+           }
+         ];
+   return($d);
+}
+
+sub doReplaceOperation
+{
+   my $self=shift;
+   my $tag=shift;
+   my $data=shift;
+   my ($replacemode,$search,$searchid,$replace,$replaceid)=@_;
+   my $count=0;
+
+   my $dataobj=getModuleObject($self->getParent->Config,$data->{dataobj});
+   my $opdataobj=getModuleObject($self->getParent->Config,$data->{dataobj});
+   if (defined($dataobj)){
+      my $idname=$dataobj->IdField->Name();
+      my %flt=($data->{idfield}=>\$searchid);
+      my $cistatusobj=$dataobj->getField("cistatusid");
+      if (defined($cistatusobj)){
+         $flt{cistatusid}='<=5';
+      }
+      $dataobj->SetFilter(\%flt);
+      $dataobj->SetCurrentView(qw(ALL));
+      my ($rec,$msg)=$dataobj->getFirst(unbuffered=>1);
+      if (defined($rec)){
+         do{
+            $opdataobj->ValidatedUpdateRecord($rec,
+                                  {$data->{target}=>$replace},
+                                  {$idname=>\$rec->{$idname}});
+            $count++;
+            ($rec,$msg)=$dataobj->getNext();
+         }until(!defined($rec));
+      }
+   }
+
+   return($count);
+}
+
+1;
