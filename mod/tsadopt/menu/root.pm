0131a46b481d04c9ec2c938ef2916ae06371aaa3
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2015-11-04 10:13:32 +0000

    add 1st eCMDB Interface https://darwin.telekom.de/darwin/auth/base/workflow/ById/12925811110005
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@9290 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ mod/tsadopt/menu/root.pm
@@ -0,0 +1,58 @@
+package tsadopt::menu::root;
+#  W5Base Framework
+#  Copyright (C) 2015  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::MenuRegistry;
+@ISA=qw(kernel::MenuRegistry);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+   return($self);
+}
+
+sub Init
+{
+   my $self=shift;
+
+   $self->RegisterObj("itu.tsadopt",
+                      "tmpl/welcome",
+                      defaultacl=>['valid_user']);
+   
+   $self->RegisterObj("itu.tsadopt.vfarm",
+                      "tsadopt::vfarm",
+                      defaultacl=>['valid_user']);
+
+   $self->RegisterObj("itu.tsadopt.vhost",
+                      "tsadopt::vhost",
+                      defaultacl=>['valid_user']);
+
+   $self->RegisterObj("itu.tsadopt.vsys",
+                      "tsadopt::vsys",
+                      defaultacl=>['valid_user']);
+
+   return($self);
+}
+
+
+
+1;
