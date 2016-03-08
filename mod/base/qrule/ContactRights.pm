1ecd5026dd4a9a134a1d57546a0a30613b5350af
Zeis,Markus <markus.zeis@t-systems.com>
2016-03-08 09:34:10 +0000

    add hints for qrule ContactRights based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/14573513970001
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@9499 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/base/qrule/ContactRights.pm
+++ mod/base/qrule/ContactRights.pm
@@ -4,13 +4,37 @@ package base::qrule::ContactRights;
 
 =head3 PURPOSE
 
-Checks if there is an "installed/active" CO-Nummer is selected in
-every application with an CI-Status "installed/active" or "available".
-If there is no valid CO-Number defined, an error will be procceded.
+Checks if there is an "installed/active" CO-Number selected in
+every application with CI-Status "installed/active" or "available".
+If there is no valid CO-Number defined, an error will be procceeded.
 
 =head3 IMPORTS
 
 NONE
+
+=head3 HINTS
+
+[de:]
+
+Bitte hinterlegen Sie mindestens einen Kontakt mit der Rolle "schreiben", 
+der nicht gleichzeitig Datenverantwortlicher für diesen Datensatz ist. 
+
+Verantwortlich: Datenverantwortlicher
+
+Bei Fragen wenden Sie sich bitte an den Darwin Support:
+https://darwin.telekom.de/darwin/auth/base/user/ById/12390966050001
+
+
+[en:]
+
+Please enter at least one person, other than the current databoss, 
+into the field Contacts with the role application write.
+
+Accountable: Databoss
+
+If you have any questions please contact the Darwin Support:
+https://darwin.telekom.de/darwin/auth/base/user/ById/12390966050001
+
 
 =cut
 #######################################################################
