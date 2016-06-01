9136b3c52e3b03285e93b68eab7ea8d47b52a187
Zeis,Markus <markus.zeis@t-systems.com>
2016-06-01 08:45:05 +0000

    add Hints for qrule ApplResponseTeam based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/14579726400001
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@9695 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/itil/qrule/ApplResponseTeam.pm
+++ mod/itil/qrule/ApplResponseTeam.pm
@@ -11,6 +11,41 @@ have a defined response team.
 
 NONE
 
+=head3 HINTS
+
+[en:]
+
+An entry for either 'IT-Servicemanagement Team' or 
+'Customer Business Management Team' has to be available.
+
+The IT-SeM Team is the organizational Team of the IT-Service Manager. 
+You can find it in the contacts of the IT-Service Manager under 
+'Group Memberships'.
+The CBM Team is the organizational Team of the Customer Business Manager. 
+You can find it in the contacts of the CBM under 'Group Memberships'.
+
+The CBM and their team can be edited directly on the Application under 
+'Customer contact / Contract management'. 
+The IT-Service Manager and their Team has to be entered at the CO-Number 
+directly and is automatically mirrored from there.
+
+[de:]
+
+Es muss entweder ein Eintrag für 'IT-Servicemanagement Team' oder
+'Customer Business Management Team' vorhanden sein.
+
+Das IT-SeM Team ist das organisatorische Team des IT Servicemanagers - 
+dieses finden Sie im Kontakt des IT-Servicemanagers unter 
+'Gruppenmitgliedschaften'.
+Das CBM Team ist das organisatorische Team des Customer Business Managers - 
+dieses finden Sie im Kontakt des CBMs unter 'Gruppenmitgliedschaften'.
+
+Ein Customer Business Manager und sein Team können direkt an der Anwendung 
+unter 'Kundenbetreuung/Vertragsgestaltung' eingetragen werden.
+Ein IT-Servicemanager und sein Team müssen direkt am Kontierungsobjekt 
+eingetragen sein und werden von diesem automatisch an die Anwendung übertragen.
+
+
 =cut
 #######################################################################
 #  W5Base Framework
