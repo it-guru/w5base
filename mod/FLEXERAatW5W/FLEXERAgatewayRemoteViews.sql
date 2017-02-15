510549127949a1ae75ac7a3c50ad59dd4a4b591c
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2017-02-15 10:38:14 +0000

    add Flexera Logo to Flexera Interface based on request https://darwin.telekom.de/darwin/auth/base/workflow/ById/14811094740001
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@10094 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/FLEXERAatW5W/FLEXERAgatewayRemoteViews.sql
+++ mod/FLEXERAatW5W/FLEXERAgatewayRemoteViews.sql
@@ -7,18 +7,18 @@ create materialized view "mview_FLEXERA_system"
 select * from w5base.system@flexerap;
 
 CREATE INDEX "FLEXERA_system_id1"
-   ON "mview_FLEXERA_system"(id) online;
+   ON "mview_FLEXERA_system"(flexerasystemid) online;
 CREATE INDEX "FLEXERA_system_id2"
    ON "mview_FLEXERA_system"(systemname) online;
 
 -- drop view "W5I_FLEXERA_system";
 create view "W5I_FLEXERA_system" as
 select "mview_FLEXERA_system".*,
-       "W5I_FLEXERAsup__system_of".systemid
+       "W5I_FLEXERA__systemidmap_of".systemid
 from "mview_FLEXERA_system"
-   left outer join "W5I_FLEXERAsup__system_of"
+   left outer join "W5I_FLEXERA__systemidmap_of"
         on "mview_FLEXERA_system".flexerasystemid=
-           "W5I_FLEXERAsup__system_of".flexerasystemid;
+           "W5I_FLEXERA__systemidmap_of".flexerasystemid;
 
 grant select on "W5I_FLEXERA_system" to W5I;
 create or replace synonym W5I.FLEXERA_system for "W5I_FLEXERA_system";
