bc1d1acf31417ea27ce099b114743d6b62674c6e
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2017-05-30 11:53:55 +0000

    add work script to check if there are workflow colisions while update prod enviroment
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@10296 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ sbin/repoHeadCheck.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+CHKWF=""
+ALLOTHERS=""
+if echo "$1" | egrep -q 'https://'; then
+   CHKWF=$1
+   shift 
+fi
+L="$*"
+
+if [ ! -z "W5BASEINSTDIR" ]; then
+   echo "change to $W5BASEINSTDIR"
+   cd $W5BASEINSTDIR
+fi
+BASE=$(svn info .| egrep '^URL:' | awk '{print $2}');
+HAVECOLISTIONS=0
+OKFILES="";
+
+for f in $L; do 
+   CREV=""
+   BADFILE=0
+   if [ -f $f ]; then
+      CREV=$(svn info $f | egrep 'Revision:' | awk '{print $2}')
+   fi
+   if [ "$CREV" = "" ]; then
+      WF=$(svn log $BASE/$f 2>/dev/null | \
+         sed -e "s#\(https://..*\?/.*/.*\?/workflow/ById/[0-9]\+\)#\n\1\n#g"|\
+         egrep '^https://.*auth' | sort | uniq)
+      if [ -z "$WF" ]; then
+         echo -e "ERROR: file $f have not any Workflow logs\n" >&2
+         BADFILE=1
+      fi
+   else
+      WF=$(svn log -r $CREV:HEAD $f | \
+         sed -e "s#\(https://..*\?/.*/.*\?/workflow/ById/[0-9]\+\)#\n\1\n#g"|\
+         egrep '^https://.*auth' | sort | uniq)
+   fi
+   ALLOTHERS="$ALLOTHERS\n$WF\n" 
+   if [ ! -z "$CHKWF" ]; then
+      OTHERWF=$(echo "$WF" | grep -v "^$CHKWF")
+      if [ ! -z "$OTHERWF" ]; then
+         echo -e "WARN:  workflow colision in $f\n       $OTHERWF\n" >&2
+         HAVECOLISTIONS=1
+      else
+         if [ "$BADFILE" != "1" ]; then
+            OKFILES="$OKFILES $f"
+         fi
+      fi
+   fi
+done
+
+if [ -z "$CHKWF" ]; then
+   if [ ! -z "$ALLOTHERS" ]; then
+      echo "All related outstanding workflows:"
+      echo -e "$ALLOTHERS" | sort | uniq
+      echo ""
+   fi
+   exit 0
+else 
+   if [ "$HAVECOLISTIONS" = "1" -a ! -z "$OKFILES" ]; then
+      echo "Files with no colisions:"
+      echo -e "$OKFILES" 
+      echo ""
+
+   fi
+fi
+if [ "$HAVECOLISTIONS" = "1" ]; then
+   exit 1    # Einspielung ist NICHT OK
+fi
+exit 0       # Einspielung ist OK
+
+
