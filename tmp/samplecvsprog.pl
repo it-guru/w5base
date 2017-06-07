5f3a05a8204fffb7f500aae0b9f1e25c51f7c15c
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2017-06-07 12:27:06 +0000

    git-svn-id: file:///opt/w5base-svn-repo/HEAD@10318 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- tmp/samplecvsprog.pl
+++ tmp/samplecvsprog.pl
@@ -11,8 +11,8 @@ Zeile 10
 Dies ist ein Text
 Dies ist ein Text
 Dies ist ein Text
-Dies ist ein Text
-Dies ist ein Text
+Dies ist ein Text Hallo Welt
+Dies ist ein Text Hallo Welt (hv) Commit 2
 Dies ist ein Text
 Dies ist ein Text
 Dies ist ein Text
