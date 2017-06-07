3e90e207b4bfaa9ba4d9d848fb82aaf08a70aa86
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2017-06-07 11:43:12 +0000

    svn test1
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@10307 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ tmp/samplecvsprog.pl
@@ -0,0 +1,21 @@
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Zeile 10
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Dies ist ein Text
+Zeile 20
+
