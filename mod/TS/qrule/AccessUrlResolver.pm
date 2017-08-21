c252cc5520cf649a0372cb968b2a786c9c62d2e8
Zeis,Markus <markus.zeis@t-systems.com>
2017-08-21 09:02:45 +0000

    temp host for resolving internet dns
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@10511 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/TS/qrule/AccessUrlResolver.pm
+++ mod/TS/qrule/AccessUrlResolver.pm
@@ -146,12 +146,13 @@ sub qcheckRecord
             return(undef);
          }
          else{
-            #my $proxy=$self->getParent->Config->Param("http_proxy");
-            #if ($proxy ne ""){
-            #   msg(INFO,"set proxy to $proxy");
-            #   $ua->proxy(['http', 'ftp'],$proxy);
-            #}
-            my $url="http://10.175.42.25/dns/resolv.php?q=".$host;
+            my $proxy=$self->getParent->Config->Param("http_proxy");
+            if ($proxy ne ""){
+               msg(INFO,"set proxy to $proxy");
+               $ua->proxy(['http', 'ftp'],$proxy);
+            }
+            #my $url="http://10.175.42.25/dns/resolv.php?q=".$host;
+            my $url="http://w5base.buckenhofen.de/resolv.php?q=".$host;
             my $response=$ua->request(GET($url));
             if ($response->code ne "200"){
                msg(ERROR,"$self URL request $url failed code(".
