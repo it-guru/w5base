cdf5b077bde6b4d17742729badeb3be28697649f
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2017-12-13 14:48:07 +0000

    Interface Mods for Qualiys interface
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@10688 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

--- mod/tssiem/W5SIEMadW5W.sql
+++ mod/tssiem/W5SIEMadW5W.sql
@@ -1,15 +1,26 @@
 -- drop table "W5SIEM_secscan";
 create table "W5SIEM_secscan" (
- scanid               VARCHAR2(28) not null,
- ictoid               VARCHAR2(20) not null,
- label                VARCHAR2(128) not null,
- pdfsummary           BLOB,
- xmlsummary           CLOB,
- importdate           DATE not null,
- scandate             DATE not null,
- constraint "W5SIEM_secscan_pk" primary key (scanid)
+   ref                VARCHAR2(32) not null,
+   ictoid             VARCHAR2(20) not null,
+   type               VARCHAR2(32) not null,
+   title              VARCHAR2(128) not null,
+   user_login         VARCHAR(32) not null,
+   launch_datetime    DATE not null,
+   duration           DATE not null,
+   target             CLOB not null,
+   pdfvfwifull        BLOB,
+   pdfvfwifull_level  INT,
+   pdfvfwidelta       BLOB,
+   pdfvfwidelta_level INT,
+   pdfstdfull         BLOB,
+   pdfstdfull_level   INT,
+   pdfstddelta        BLOB,
+   pdfstddelta_level  INT,
+   creationdate       DATE not null,
+   importdate         DATE,
+   rcvdate            DATE,
+   constraint W5SIEM_secscan_pk primary key (ref)
 );
-
 grant select on "W5SIEM_secscan" to W5I;
 grant select,insert,update,delete on "W5SIEM_secscan" to W5SIEM;
 create or replace synonym W5SIEM.secscan for "W5SIEM_secscan";
@@ -18,34 +29,44 @@ create or replace synonym W5I.W5SIEM_secscan for "W5SIEM_secscan";
 
 -- drop table "W5SIEM_secent";
 create table "W5SIEM_secent" (
- scanid               VARCHAR2(28) not null,
- ipaddress            VARCHAR2(45) not null,
- tracking_method      VARCHAR2(40),
- osname               VARCHAR2(128),
- ipstatus             VARCHAR2(128),
- qid                  NUMBER(20,0) not null,
- title                VARCHAR2(4000) not null,
- vuln_status          VARCHAR2(40),
- ent_type             VARCHAR2(40),
- severity             NUMBER(2,0) not null,
- port                 NUMBER(20,0),
- ssl                  NUMBER(1,0) default 0,
- first_detect         DATE not null,
- last_detect          DATE not null,
- vendor_reference     VARCHAR2(4000),
- threat               clob,
- impact               clob,
- exploitability       clob,
- associated_malware   VARCHAR2(255),
- pci_vuln             NUMBER(1,0) default 0,
- category             VARCHAR2(255),
- constraint "W5SIEM_secent_pk" foreign key (scanid) 
-    REFERENCES "W5SIEM_secscan" (scanid)
+   ref                  VARCHAR2(32) not null,
+   ipaddress            VARCHAR2(45) not null,
+   tracking_method      VARCHAR2(40),
+   osname               VARCHAR2(128),
+   ipstatus             VARCHAR2(128),
+   qid                  NUMBER(20,0) not null,
+   title                VARCHAR2(4000) not null,
+   vuln_status          VARCHAR2(40),
+   ent_type             VARCHAR2(40),
+   severity             NUMBER(2,0) not null,
+   port                 NUMBER(20,0),
+   ssl                  VARCHAR(10),
+   first_detect         DATE,
+   last_detect          DATE,
+   vendor_reference     VARCHAR2(4000),
+   threat               clob,
+   impact               clob,
+   exploitability       clob,
+   associated_malware   VARCHAR2(4000),
+   pci_vuln             VARCHAR2(10),
+   category             VARCHAR2(2000),
+   constraint W5SIEM_secent_pk foreign key (ref)
+   REFERENCES "W5SIEM_secscan" (ref)
 );
-
 grant select on "W5SIEM_secent" to W5I;
 grant select,insert,update,delete on "W5SIEM_secent" to W5SIEM;
 create or replace synonym W5SIEM.secent for "W5SIEM_secent";
 create or replace synonym W5I.W5SIEM_secent for "W5SIEM_secent";
+
+
+-- drop table "W5SIEM_secscanruntime";
+create table "W5SIEM_secscanruntime" (
+   lastrun     DATE not null,
+   exitcode    INT
+);
+grant select on "W5SIEM_secscanruntime" to W5I;
+grant select,insert,update,delete on "W5SIEM_secscanruntime" to W5SIEM;
+create or replace synonym W5SIEM.secscanruntime for "W5SIEM_secscanruntime";
+create or replace synonym W5I.W5SIEM_secscanruntime for "W5SIEM_secscanruntime";
 
 
