644c1b464312ab7f69b7140efd3b745422b685d1
Vogler, Hartmut <hartmut.vogler@t-systems.com>
2018-03-05 07:56:48 +0000

    first try of graphical ui
    git-svn-id: file:///opt/w5base-svn-repo/HEAD@10845 0dab0bae-74ca-4a1c-8b4a-1454ec714b61

---
+++ mod/base/Explore.pm
@@ -0,0 +1,62 @@
+package base::Explore;
+#  W5Base Framework
+#  Copyright (C) 2017  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::App::Web;
+use CGI;
+@ISA=qw(kernel::App::Web);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+   $self->LoadSubObjs("Research","Research");
+   return($self);
+}
+
+sub getValidWebFunctions
+{
+   my ($self)=@_;
+   return(qw(Main));
+}
+
+
+#
+# Explore Engine
+#
+
+sub Main
+{
+   my ($self)=@_;
+
+   print $self->HttpHeader("text/html",charset=>'UTF-8');
+
+   my $getAppTitleBar=$self->getAppTitleBar();
+
+   my $opt={};
+
+   my $prog=$self->getParsedTemplate("tmpl/base.Explore",$opt);
+   utf8::encode($prog);
+   print($prog);
+}
+
+
+1;
