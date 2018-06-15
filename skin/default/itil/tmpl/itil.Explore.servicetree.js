09fd09077242767e410d545e37c0cc4b83e8c89e
Hartmut Vogler <hartmut.vogler@t-systems.com>
2018-06-15 12:59:04 +0200

    GUI add 1st version of BusinessServiceTree - very poor

---
+++ skin/default/itil/tmpl/itil.Explore.servicetree.js
@@ -0,0 +1,175 @@
+   var applet='%SELFNAME%';
+   ClassAppletLib[applet].class=function(app){
+      ClassApplet.call(this,app);
+   };
+   $.extend(ClassAppletLib[applet].class.prototype,ClassApplet.prototype);
+
+   ClassAppletLib[applet].class.prototype.searchFilter='';
+   ClassAppletLib[applet].class.prototype.searchResult='';
+   ClassAppletLib[applet].class.prototype.loadItems=function(param){
+     var dataobj=param[0];
+     var dataobjid=param[1];
+     var app=this.app;
+     return(
+        new Promise(function(ok,reject){
+           app.Config().then(function(cfg){
+              var w5obj=getModuleObject(cfg,dataobj);
+                w5obj.SetFilter({ id:dataobjid });
+                w5obj.findRecord("id,fullname", function(data){
+                   var cnt=data.length;
+                   for(c=0;c<cnt;c++){
+                      app.addNode(dataobj,data[c].id,data[c].fullname);
+                  //    for(s=0;s<data[c].systems.length;s++){
+                  //       app.addNode("itil::system",
+                  //                   data[c].systems[s].systemid,
+                  //                   data[c].systems[s].system)
+                  //       app.addEdge(
+                  //          app.toObjKey(dataobj,dataobjid),
+                  //          app.toObjKey('itil::system',
+                  //                       data[c].systems[s].systemid)
+                  //       );
+                  //    }
+                   } 
+                   ok(1);
+                });
+             }).catch(function(e){
+                console.log("get config failed",e);
+                app.console.log("can not get config");
+                reject(e); 
+             });
+          })
+       );
+   };
+
+
+
+
+   ClassAppletLib[applet].class.prototype.setSearchResult=function(dialog,res){
+      var appletobj=this;
+      $(dialog).find("#SearchResult").html(res);
+      $(dialog).find(".appstart").click(function(e){
+         console.log("click eon ",$(this).attr("data-id"));
+         console.log("click eon ",$(this).attr("data-dataobj"));
+
+         var id=$(this).attr("data-id");
+         var dataobj=$(this).attr("data-dataobj");
+         appletobj.run([dataobj,id]);
+      });
+      appletobj.searchResult=$(dialog).find("#SearchResult").html();
+   }
+
+   ClassAppletLib[applet].class.prototype.searchItems=function(dialog,flt){
+      var appletobj=this;
+      this.app.Config().then(function(cfg){
+         var w5obj=getModuleObject(cfg,'itil::businessservice');
+         appletobj.searchFilter=flt;
+         if (flt.indexOf("*")==-1 && flt.indexOf(" ")==-1){
+            flt="*"+flt+"*";
+         }
+         w5obj.SetFilter({
+            cistatusid:"4",
+            fullname:flt
+         });
+         w5obj.findRecord("id,fullname",function(data){
+            var cnt=data.length;
+            var res="";
+            for(c=0;c<cnt;c++){
+               res+="<div class='purebtn appstart' "+
+                    "data-id='"+data[c].id+"'"+
+                    "data-dataobj='itil::businessservice'"+
+                    ">"+
+                    data[c].fullname+"</div>";
+            }
+            appletobj.setSearchResult(dialog,res);
+         },function(e){
+            $(dialog).find("#SearchResult").html("Fail");
+         });
+      }).catch(function(e){
+         $(dialog).find("#SearchResult").html("Fail2");
+      });
+   }
+
+   ClassAppletLib[applet].class.prototype.run=function(){
+      var appletobj=this;
+      var app=this.app;
+      this.app.node.clear();
+      this.app.edge.clear();
+      if (arguments.length){
+         var dataobj=arguments[0][0];
+         var dataobjid=arguments[0][1];
+         this.app.ShowNetworkMap({
+            physics: {
+               barnesHut:{
+                  gravitationalConstant:-50000
+               },
+               enabled: true   // || "once"
+            }
+         });
+         this.app.console.log("INFO","loading scenario ...");
+         console.log(" run in "+dataobj+" and id="+dataobjid);
+         appletobj.app.setMPath({
+               label:ClassAppletLib['%SELFNAME%'].desc.label,
+               mtag:'%SELFNAME%'
+            },
+            {
+               label:"loading ...",
+               mtag:dataobj+"/"+dataobjid
+            }
+         );
+         this.loadItems(arguments[0]).then(function(d){
+             app.processOpStack(function(d){
+                app.console.log("INFO","scenario is loaded");
+                app.setMPath({
+                      label:ClassAppletLib['%SELFNAME%'].desc.label,
+                      mtag:'%SELFNAME%'
+                   },
+                   {
+                      label:d[0].label,
+                      mtag:dataobj+"/"+d[0].dataobjid
+                   }
+                );
+             });
+         }).catch(function(e){
+             console.log("fail to load scenario",e);
+             $(".spinner").hide();
+         });
+      }
+      else{
+         this.app.showDialog(function(){
+            var dialog = document.createElement('div');
+            $(dialog).css("height","100%");
+            $(dialog).append("<table id=SearchTab width=97% height=90% "+
+                              "border=0>"+  
+                              "<tr height=1%><td >"+
+           "<h1>"+ClassAppletLib['%SELFNAME%'].desc.label+"</h1>"+
+                              "</td></tr>"+
+                              "<tr height=1%><td width=10%>"+
+                              "<form id=SearchFrm><div class='SearchLabel'>"+
+                              "Suchen:</div></td><td>"+
+                              "<div class='SearchLabel'>"+
+                              "<input type=text name=SearchInp id=SearchInp>"+
+                              "</div></form></td></tr>"+
+                              "<tr><td colspan=2 valign=top>"+
+                              "<div id=SearchContainer>"+
+                              "<div id=SearchResult></div>"+
+                              "</div>"+
+                              "</td></tr>"+
+                              "</table>");
+            $(dialog).find("#SearchInp").val(appletobj.searchFilter);
+            $(dialog).find("#SearchInp").focus();
+            appletobj.setSearchResult(dialog,appletobj.searchResult);
+            $(dialog).find("#SearchInp").on('keypress', function (e) {
+               if(e.which === 13){
+                  $(this).attr("disabled", "disabled");
+                  appletobj.searchItems(dialog,$(this).val());
+                  $(this).removeAttr("disabled");
+                  $(dialog).find("#SearchInp").focus();
+               }
+            });
+            $(".spinner").hide();
+            return(dialog);
+         },function(){
+            appletobj.exit();
+         });
+      }
+   };
