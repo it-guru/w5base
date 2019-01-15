3aed18fb5dbc80bf3074c2a950df8a07f8f46dc7
hvogler <hartmut.vogler@t-systems.com>
2019-01-15 12:09:05 +0100

    remove OFI Interface based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/15174820390001

--- mod/tssapp01/W5I_costelement_saphier.sql
+++ mod/tssapp01/W5I_costelement_saphier.sql
@@ -1,38 +1,3 @@
-create materialized view "W5I_costelement_saphier"
-   refresh complete start with sysdate
-   next sysdate+(1/24)*4
-   as
-select "ID","SAPNAME","NAME","SHORTNAME",
-       "BPMARK","SAPHIER","COTYPE","OFIENTITY" from (
-
-   select "tssapp01::psp".id ID,
-          "tssapp01::psp".name SAPNAME,
-          "tssapp01::psp".name NAME,
-          REGEXP_REPLACE("tssapp01::psp".name,'^[a-zA-Z]-','') SHORTNAME,
-          "tssapp01::psp".saphier SAPHIER,
-          "tssapp01::psp".bpmark BPMARK,
-          'psp' COTYPE,
-          "tssapp01::psp".rawofientity RAWOFIENTITY,
-          "tssapp01::psp".ofientity OFIENTITY
-         from (select max(id) id from (
-         select id,REGEXP_REPLACE(name,'^[a-zA-Z]-','') SHORTNAME
-      from "tssapp01::psp"  where regexp_like(name,'^[a-z]-[a-z0-9]+$','i')
-   ) group by shortname) pspid join "tssapp01::psp" 
-     on pspid.id="tssapp01::psp".id
-   union
-   select id ID,
-          name SAPNAME,
-          REGEXP_REPLACE(name,'^0+','') NAME,
-          REGEXP_REPLACE(name,'^0+','') SHORTNAME,
-          saphier SAPHIER,
-          '-none-' BPMARK,
-          'costcenter' COTYPE,
-          NULL RAWOFIENTITY,
-          NULL OFIENTITY
-   from "tssapp01::costcenter"
-) costelement;
-
-
 create or replace view "W5I_tssapp01::costcenter" as
   select 
     rowid                            w5id,
