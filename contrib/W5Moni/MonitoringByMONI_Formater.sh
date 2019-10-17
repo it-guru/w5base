564f42dd6d93ca330ebb7e6e022b0b04d0ba85ef
hvogler <hartmut.vogler@t-systems.com>
2019-10-17 13:31:51 +0200

    add sample MonitoringByMONI_Formater.sh Script based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/15712164120017

--- contrib/W5Moni/MonitoringByMONI_Formater.sh
+++ contrib/W5Moni/MonitoringByMONI_Formater.sh
@@ -13,7 +13,16 @@
 W5HOST="w5base-testenv.telekom.de"
 W5CONF="darwin"
 DATAOBJ="tsdina::system"
-MONQUERY="search_name=qde8hv&CurrentView=(systemid,name)"
+MONQUERY="search_name=qde8hv&CurrentView=(name)"
+
+# Der MONI Foramter setzt vorraus, dass mit search_xxx ein
+# Datensatz gesucht wird und exakt das was in der Suche gesucht
+# wird, auch in EINEM Ergebnis-Datensatz zurück geliefert
+# wird. Aus diesem Grund muss auch das Suchfeld in CurrentView
+# angeben werden.
+# Ist dies der Fall, so wird ein HTTP-Code=200 erzeugt. Alle 
+# anderen Zustände erzeugen einen HTTP-Code ungleich 200.
+# 
 
 MONQUERY="${MONQUERY}&FormatAs=MONI"
 while true; do
