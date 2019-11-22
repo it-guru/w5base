21f515f4ee1483b1da4ebbdb71551db2a9817ab2
hvogler <hartmut.vogler@t-systems.com>
2019-11-22 13:08:39 +0100

    remove w5sharescan from w5base repo - it is now at https://github.com/it-guru/TSecScan

--- contrib/PowerShell/w5sharescan/testscript.ps1
+++
@@ -1,7 +0,0 @@
-[CmdletBinding()]
-Param()
-
-$d=Get-Date -Format "yyyyMMdd-HH:mm:ss"
-Write-Host ("{0} Start Write-Host {1} - Verbose=$Verbose" -f $d,"aaa");
-Write-Error ("{0} Start Write-Error {1}" -f $d,"aaa");
-Write-Verbose ("{0} Start Write-Verbose {1}" -f $d,"aaa");
