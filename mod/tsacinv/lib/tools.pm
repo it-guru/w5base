81297cd61851deb1fbbe6f9709f0483957af43ca
hvogler <hartmut.vogler@t-systems.com>
2020-08-10 11:22:53 +0200

    rmove optimizer_max_permutations on assetmanager access based on a mail from Schmied Rainer

--- mod/tsacinv/lib/tools.pm
+++ mod/tsacinv/lib/tools.pm
@@ -25,7 +25,7 @@ sub amInitializeOraSession
 
    if (defined($self->{DB})){
       $self->{DB}->do("alter session set optimizer_features_enable='11.2.0.1'");
-      $self->{DB}->do("alter session set optimizer_max_permutations=100");
+     # $self->{DB}->do("alter session set optimizer_max_permutations=100");
       $self->{DB}->do("alter session set cursor_sharing=force");
    }
 }
