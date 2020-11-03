31836d1de38e5f63ab3b4019980738865039254e
hvogler <hartmut.vogler@t-systems.com>
2020-11-03 08:20:08 +0100

    DigitalHUB Mapping to Mandator "TelekomIT"

--- mod/tscape/qrule/CapeBasedMandatorRules.pm
+++ mod/tscape/qrule/CapeBasedMandatorRules.pm
@@ -141,6 +141,9 @@ sub qcheckRecord
          if (defined($ictor) && $ictor->{organisation} ne ""){
             $grp=$ictor->{orgareaid};
          }
+         if ($ictor->{orgarea}=~m/\.DTIT\.DigitalHub\./i){ # seems a hub
+            $grp="200"; # map all TelIT Hubs to Mandator TelekomIT group
+         }
          if ($grp ne ""){
             my $m=getModuleObject($dataobj->Config,"base::mandator");
             $m->SetFilter({grpid=>\$grp,cistatusid=>['3','4','5']});
