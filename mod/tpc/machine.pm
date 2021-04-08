3761470251f720005ce81f78088863de6a353eaf
hvogler <hartmut.vogler@t-systems.com>
2021-04-08 08:47:12 +0200

    prevent 404 errors, if machines are deleted on tpc based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16101139690001

--- mod/tpc/machine.pm
+++ mod/tpc/machine.pm
@@ -170,7 +170,22 @@ sub DataCollector
              $_->{osrelease}=$_->{customProperties}->{softwareName};
          } @$data);
          return($data);
-      }
+      },
+      onfail=>sub{
+         my $self=shift;
+         my $code=shift;
+         my $statusline=shift;
+         my $content=shift;
+         my $reqtrace=shift;
+
+         if ($code eq "404"){  # 404 bedeutet nicht gefunden
+            return([],"200");
+         }
+         msg(ERROR,$reqtrace);
+         $self->LastMsg(ERROR,"unexpected data TPC response");
+         return(undef);
+      },
+
    );
    #printf STDERR ("rawdata=%s\n",Dumper($d));
 
