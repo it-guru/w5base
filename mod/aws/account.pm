87de1d9100e8a37b1339343693c538c66717286c
hvogler <hartmut.vogler@t-systems.com>
2021-04-22 16:41:11 +0200

    reimplement Trigger Handler based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/15681076330001

--- mod/aws/account.pm
+++ mod/aws/account.pm
@@ -168,5 +168,43 @@ sub getDetailBlockPriority
 }
 
 
+sub getValidWebFunctions
+{
+   my ($self)=@_;
+   return(qw(TriggerEndpoint),$self->SUPER::getValidWebFunctions());
+}
+
+#
+# Endpoint URL to handle Trigger Events from AWS
+#
+
+sub TriggerEndpoint
+{
+   my $self=shift;
+   my %param;
+
+   $param{charset}="UTF8";
+
+   my $q=Query->MultiVars();
+
+   delete($q->{MOD});
+   delete($q->{FUNC});
+   print $self->HttpHeader("application/javascript",%param);
+
+   my $json=new JSON;
+   $json->utf8(1);
+
+   my $d=$json->pretty->encode({
+      request=>$q,
+      exitcode=>0,
+      ptimestamp=>NowStamp(),
+      exitmsg=>'OK'
+   });
+   print $d;
+   return(0);
+}
+
+
+
 
 1;
