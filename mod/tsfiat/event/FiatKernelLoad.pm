1cb6cec56a1e9e640d7b2d0e92254b53125da368
hvogler <hartmut.vogler@t-systems.com>
2021-08-30 12:37:36 +0200

    add loader for Firewall Table based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/15853202880002
    (cherry picked from commit e47b1c2f540f878fe35b242f51f30348b33b91f2)

--- mod/tsfiat/event/FiatKernelLoad.pm
+++ mod/tsfiat/event/FiatKernelLoad.pm
@@ -20,6 +20,7 @@ use strict;
 use vars qw(@ISA);
 use kernel;
 use kernel::Event;
+use MIME::Base64;
 @ISA=qw(kernel::Event);
 
 
@@ -30,6 +31,90 @@ sub FiatKernelLoad
    my $self=shift;
    my $queryparam=shift;
 
+   my $obj=getModuleObject($self->Config,"tsfiat::firewall");
+
+   my $start=NowStamp("en");
+
+   my $d=$obj->CollectREST(
+      dbname=>'tsfiat',
+      useproxy=>0,
+      verify_hostname=>0,
+      url=>sub{
+         my $self=shift;
+         my $baseurl=shift;
+         my $apikey=shift;
+         $baseurl.="/"  if (!($baseurl=~m/\/$/));
+         my $dataobjurl=$baseurl."W5Base/devices?model=fmg_firewall";
+         return($dataobjurl);
+      },
+
+      headers=>sub{
+         my $self=shift;
+         my $baseurl=shift;
+         my $apikey=shift;
+         my $apiuser=shift;
+         my $headers=[Authorization =>'Basic '.
+                                      encode_base64($apiuser.':'.$apikey)];
+ 
+         return($headers);
+      },
+      success=>sub{  # DataReformaterOnSucces
+         my $self=shift;
+         my $data=shift;
+         my @l;
+
+         foreach my $device ($data->{devices}->nodes()){
+            my $name=$device->{name}; 
+            if ($name ne ""){
+               my $isoffline=$device->{offline};
+               $isoffline=lc($isoffline) eq "false"  ? 0:1;
+               my $istopology=$device->{topology};
+               $istopology=lc($istopology) eq "false"  ? 0:1;
+               push(@l,{
+                  id=>"".$device->{id},
+                  parent_id=>"".$device->{parent_id},
+                  ipaddress=>"".$device->{ip},
+                  name=>"".$device->{name},
+                  domainname=>"".$device->{domain_name},
+                  virtualtype=>"".$device->{virtual_type},
+                  contextname=>"".$device->{context_name},
+                  domainid=>"".$device->{domain_id},
+                  isoffline=>$isoffline,
+                  istopology=>$istopology,
+                  vendor=>"".$device->{vendor},
+                  model=>"".$device->{model},
+                  srcload=>NowStamp("en")
+               });
+            }
+            #printf STDERR ("name=%s\n",$device->{name});
+            #printf ("node=%s\n",join(",",$device->nodes_keys()));
+         }
+
+         return(\@l);
+      },
+      #onfail=>sub{
+      #   my $self=shift;
+      #   my $code=shift;
+      #   my $statusline=shift;
+      #   my $content=shift;
+      #   my $reqtrace=shift;
+#
+#         if ($code eq "404"){  # 404 bedeutet nicht gefunden
+#            return([],"200");
+#         }
+#         msg(ERROR,$reqtrace);
+#         $self->LastMsg(ERROR,"unexpected data TPC project response");
+#         return(undef);
+#      }
+   );
+
+   foreach my $rec (@$d){
+       $obj->ValidatedInsertOrUpdateRecord($rec,{id=>$rec->{id}}); 
+
+   }
+   $obj->BulkDeleteRecord({srcload=>'<"'.$start.' GMT"'});
+   
+   #print STDERR ("rec=%s\n",Dumper($d));
 
 
 
