2dc49c91c34e5cdbfd70cb524682581750ce7f1e
hvogler <hartmut.vogler@t-systems.com>
2021-09-14 11:26:50 +0200

    add new qrule to check ITSeM/DelMgr filled in costcenters of type pspelement based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16294708710000
    (cherry picked from commit e27ba6095e8ef9075a1a379f0c7687aba0ba0a51)

---
+++ mod/finance/qrule/costcITSeM_DelMgr.pm
@@ -0,0 +1,93 @@
+package finance::qrule::costcITSeM_DelMgr;
+#######################################################################
+=pod
+
+=encoding latin1
+
+=head3 PURPOSE
+
+Validation of costcenter to haven a IT-SeM or DelMgr.
+
+
+=cut
+
+#######################################################################
+#
+#  W5Base Framework
+#  Copyright (C) 2021  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::QRule;
+@ISA=qw(kernel::QRule);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   return($self);
+}
+
+sub getPosibleTargets
+{
+   return(["finance::costcenter"]);
+}
+
+sub qcheckRecord
+{
+   my $self=shift;
+   my $dataobj=shift;
+   my $rec=shift;
+   my $checksession=shift;
+   my $autocorrect=$checksession->{autocorrect};
+
+   my $wfrequest={};
+   my $forcedupd={};
+   my @qmsg;
+   my @dataissue;
+   my $errorlevel=0;
+
+   my $par;
+   my $parrec;
+
+   if ($rec->{cistatusid}<6){
+      if ($rec->{costcentertype} ne "pspelement"){
+         return(undef);
+      }
+      if ($rec->{itsem} eq "" && 
+          $rec->{itsem2} eq "" && 
+          $rec->{itsemteam} eq "" &&
+          $rec->{delmgrid} eq "" && 
+          $rec->{delmgr2id} eq "" && 
+          $rec->{delmgrteam} eq ""){
+         my $msg="missing Service Delivery Manager or IT Servicemanager";
+         push(@qmsg,$msg);
+         push(@dataissue,$msg);
+         $errorlevel=3;
+      }
+   }
+
+   return($self->HandleWfRequest($dataobj,$rec,
+                                 \@qmsg,\@dataissue,\$errorlevel,$wfrequest));
+}
+
+
+
+1;
