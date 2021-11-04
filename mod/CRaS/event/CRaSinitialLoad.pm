eacd4eef865158aad4951ef8882cc81031a5110b
hvogler <hartmut.vogler@t-systems.com>
2021-11-04 08:38:54 +0100

    1st dataload version of CRaS based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16330926040001

---
+++ mod/CRaS/event/CRaSinitialLoad.pm
@@ -0,0 +1,324 @@
+package CRaS::event::CRaSinitialLoad;
+#  W5Base Framework
+#  Copyright (C) 2021  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::Event;
+use File::Temp;
+use LWP::UserAgent;
+use MIME::Base64;
+use Spreadsheet::ParseExcel;
+
+@ISA=qw(kernel::Event);
+
+
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   return($self);
+}
+
+
+sub applyMap
+{
+   my $map=shift;
+   my $var=shift;
+
+   my @map=@{$map};
+
+   while(my $exp=shift(@map)){
+      my $rep=shift(@map);
+      $$var=~s/$exp/$rep/i;
+   }
+   
+
+}
+
+
+sub CRaSinitialLoad
+{
+   my $self=shift;
+
+   $self->{notFoundICTO}={};
+   $self->{CertCnt}=0;
+   $self->{CertNoRel}=0;
+
+   $self->{employeeMap}=[
+      '^.*young.*$'        , 'A. Young',
+      '^y$'                , 'A. Young',
+      '^.*schneider.*$'    , 'A. Schneider',
+      '^.*gutsche.*$'      , 'K. Gutsche',
+      '^.*nosal.*$'        , 'M. Nosal',
+      '^.*kyjovsk.*$'      , 'M. Kyjovsky',
+      '^.*hudak.*$'        , 'J. Hudak',
+      '^.*hybenova.*$'     , 'K. Hybenova',
+      '^.*belej.*$'        , 'L. Belejkanic',
+      '^.*olsavska.*$'     , 'E. Olsavska',
+      '^.*bujnak.*$'       , 'P. Bujnak',
+      '^.*Gocik.*$'        , 'P. Gocik',
+      '^.*Hornak.*$'       , 'P. Hornak',
+      '^.*m.*kovac.*$'     , 'M. Kovac',
+      '^.*kuch.*$'         , 'V. Kuchanuk',
+      '^.*seman.*$'        , 'R. Seman',
+      '^.*r.*petrus.*$'    , 'R. Petrus',
+      '^.*s.*kristof.*$'   , 'S. Kristof',
+      '^.*a.*gradov.*$'    , 'A. Gradov',
+      '^.*c.*jansen.*$'    , 'C. Jansen',
+      '^.*s.*zelenak.*$'   , 'S. Zelenak',
+      '^.*k.*kleinova.*$'  , 'K. Kleinova',
+      '^.*m.*puchlak.*$'   , 'M. Puchlak',
+      '^.*m.*simonak.*$'   , 'M. Simonak',
+      '^[a-z]\.\s*'        , '',
+   ];
+
+   
+
+   my ($fh,$filename)=File::Temp::tempfile('tempXXXXX',SUFFIX =>'.xls',
+                                           TMPDIR=>1);
+
+   $self->loadRemoteFile($fh);
+
+   $self->ProcessXLS($filename);
+
+   unlink($filename);
+   return({exitcode=>0});
+}
+
+sub ProcessXLS
+{
+   my $self=shift;
+   my $filename=shift;
+
+   my $parser=Spreadsheet::ParseExcel->new();
+   my $workbook=$parser->parse($filename);
+
+   if (!defined($workbook)){
+       die($parser->error(),".\n");
+   }
+   my $start=NowStamp("en");
+   my $cnt=0;
+   my %fullindex;
+   my @colname=();
+   my %rec;
+   my %lastrec;
+   my %employee;
+   my %projectname;
+   for my $worksheet (reverse($workbook->worksheets())){
+      my $worksheetname=$worksheet->get_name();
+
+      my ($row_min,$row_max)=$worksheet->row_range();
+      my ($col_min,$col_max)=$worksheet->col_range();
+      for(my $row=$row_min;$row<=$row_max;$row++){
+         if ($row==0){
+            @colname=(); 
+            %lastrec=();
+         }
+         %rec=();
+         for(my $col=$col_min;$col<=$col_max;$col++){
+            my $cell=$worksheet->{'Cells'}[$row][$col];
+            if (defined($cell)){
+               my $val=trim($cell->Value());
+               if ($val ne ""){
+                  if ($row==0){  # fixups for header line
+                     $val=~s/^\s*ip address\s*$/ipaddr/i;
+                     $val=~s/^lfd\. Nr\.$/SharePointLfdNr/i;
+                     $val=trim($val);
+                     $val=~s/\s*:$//;
+                     $colname[$col]=$val;
+                  }
+                  else{
+                     if ($colname[$col] ne ""){
+                        $rec{$colname[$col]}=$val; 
+                     }
+                  }
+                  $cnt++;
+#if ($row==0 && $col==0){
+#                  printf STDERR ("%08d : wb(%s) %s/%s = %s\n",$cnt,
+#                                 $worksheet->get_name(),$row,$col,$val);
+#}
+               }
+            }
+         }
+         $rec{xlsrow}=$row+1;
+         $rec{xlssheet}=$worksheet->get_name();
+         # record fixups
+         if (exists($rec{ICTO})){
+            $rec{ICTO}=~s/:.*$//;
+            $rec{ICTO}=~s/^ICTO /ICTO-/;
+            $rec{ICTO}=~s/-+/-/;
+            $rec{ICTO}=~s/^ICTO-ICTO-/ICTO-/;
+
+            $rec{ICTO}=~s/[^0-9a-z]*$//i;
+
+            $rec{ICTO}=trim($rec{ICTO});
+         }
+         if ($rec{CommonName} ne ""){
+            my $aurl=$self->getPersistentModuleObject("aur","itil::lnkapplurl");
+            $aurl->SetFilter({hostname=>'"'.$rec{CommonName}.'"'});
+            my @l=$aurl->getHashList(qw(applid));
+            if ($#l>=0){
+               $rec{applid}=$l[0]->{applid};
+            }
+         }
+         if ($rec{applid} eq "" && $rec{ICTO} ne ""){
+            my $appl=$self->getPersistentModuleObject("ap","TS::appl");
+            $appl->SetFilter({ictono=>\$rec{ICTO},cistatusid=>"3 4 5"});
+            my @l=$appl->getHashList(qw(opmode id));
+            if ($#l>=0){
+               $rec{applid}=$l[0]->{id};
+            }
+         }
+         if ($rec{csteamid} eq ""){
+            my $csteam=$self->getPersistentModuleObject("cst","CRaS::csteam");
+            $csteam->SetFilter({});
+            my @l=$csteam->getHashList(qw(id));
+            if ($#l>=0){
+               $rec{csteamid}=$l[0]->{id};
+            }
+         }
+
+ 
+
+
+
+         # process record
+         foreach my $k (keys(%rec)){
+            if ($rec{$k}=~m/^\s*"\s*$/){
+               $rec{$k}=$lastrec{$k};
+            }
+         }
+
+         my $errmsg;
+         if ($rec{CommonName} ne ""){
+            $self->{CertCnt}++;
+            if ($rec{applid} eq ""){
+               $self->{CertNoRel}++;
+               $self->{notFoundICTO}->{$rec{ICTO}}++;
+               #print STDERR Dumper(\%rec);
+               printf STDERR ("CommonName: %s\n", $rec{CommonName});
+            }
+            my $srcid=$rec{SharePointLfdNr};
+            my $csr=$self->getPersistentModuleObject("csr","CRaS::csr");
+            my %csrrec=(
+               name=>$rec{CommonName},
+               sslcertcommon=>$rec{CommonName},
+               srcid=>$srcid,
+               srcload=>NowStamp("en")
+            );
+            if ($rec{applid} ne ""){
+               $csrrec{applid}=$rec{applid};
+            }
+            if ($rec{csteamid} ne ""){
+               $csrrec{csteamid}=$rec{csteamid};
+            }
+            $csrrec{state}=1;
+            $csrrec{refno}=$rec{Referenznummer};
+            if ($csrrec{refno} ne ""){
+               $csrrec{state}=4;
+            }
+            if ($rec{'Seriennummer (hex)'} ne ""){
+               $csrrec{ssslserialno}=$rec{'Seriennummer (hex)'};
+            }
+            if ($rec{'Service-Passwort'} ne ""){
+               $csrrec{spassword}=$rec{'Service-Passwort'};
+            }
+            $csrrec{spassword}=~s/./*/g;
+
+
+            #{
+            #   'Anforderer' => 'Adrian Vogel',
+            #   'Ausgestellt am' => '15.10.2021',
+            #   'CommonName' => 'xxxxxxxxxxxxxxxkom.net',
+            #   'Gültig bis' => '19.10.2022',
+            #   'ICTO' => 'One.ERP_CERP_MyStore',
+            #   'PSP Element' => 'xxxxxxxxxxxxxxx-10-0001',
+            #   'Referenznummer' => '295764',
+            #   'Seriennummer (hex)' => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            #   'Service-Passwort' => 'xxxxxxxxxx',
+            #   'SharePointLfdNr' => '1229',
+            #   'initiale Organisation Alias' => '(Standard & SAN)',
+            #   'xlsrow' => 1231,
+            #   'xlssheet' => 'Tabelle1'
+            # };
+
+            
+
+            if ($rec{applid} ne ""){
+               $csr->ValidatedInsertOrUpdateRecord(\%csrrec,{
+                  srcid=>\$srcid
+               });
+            }
+         }
+
+
+         %lastrec=(%rec);  # store last line
+      }
+   }
+   {
+      my $csr=$self->getPersistentModuleObject("csr","CRaS::csr");
+      $csr->BulkDeleteRecord({'srcload'=>"<$start"});
+   }
+
+
+   #print "\nEmployee=".Dumper(\%employee)."\n";
+   #print "\nProjectname=".Dumper(\%projectname)."\n";
+   printf("MissICTO: %s\n",join(", ",sort(keys(%{$self->{notFoundICTO}}))));
+   printf("cnt/fail  %d/%d\n",$self->{CertCnt},$self->{CertNoRel});
+}
+
+
+
+sub loadRemoteFile
+{
+   my $self=shift;
+   my $fh=shift;
+
+   my $dataobjconnect=$self->Config->Param("DATAOBJCONNECT");
+   my $dataobjuser=$self->Config->Param("DATAOBJUSER");
+   my $dataobjpass=$self->Config->Param("DATAOBJPASS");
+
+   my $url=$dataobjconnect->{CRaSinitialLoad};
+   my $user=$dataobjuser->{CRaSinitialLoad};
+   my $pass=$dataobjpass->{CRaSinitialLoad};
+   if ($url eq "" || $user eq "" || $pass eq ""){
+      printf STDERR ("URL: %s\n",$url);
+      printf STDERR ("User: %s   -  Pass: %s\n",$user,$pass);
+   }
+   my $ua=LWP::UserAgent->new();
+
+   my $resp=$ua->get($url,
+     "Authorization" => "Basic ".MIME::Base64::encode($user.':'.$pass,'')
+   );
+   if ($resp->is_success()){
+      print $fh $resp->decoded_content();
+      seek($fh,0,0);
+      printf STDERR ("OK\n");
+   }
+   else{
+     printf("%s\n",$resp->status_line());
+   }
+}
+
+
+
+
