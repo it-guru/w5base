9bf90b9821ff15d679fc41089df12f2b4addcd0f
a295897 <hartmut.vogler@t-systems.com>
2022-05-03 16:58:18 +0200

    1st version of SIMon based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16430346250001

---
+++ mod/SIMon/lnkmonpkgrec.pm
@@ -0,0 +1,311 @@
+package SIMon::lnkmonpkgrec;
+#  W5Base Framework
+#  Copyright (C) 2022  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::App::Web;
+use kernel::DataObj::DB;
+use kernel::Field;
+use kernel::Field::TextURL;
+use kernel::CIStatusTools;
+@ISA=qw(kernel::App::Web::Listedit kernel::DataObj::DB kernel::CIStatusTools);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   
+
+   $self->AddFields(
+      new kernel::Field::Linenumber(
+                name          =>'linenumber',
+                label         =>'No.'),
+
+      new kernel::Field::Id(
+                name          =>'id',
+                label         =>'LinkID',
+                searchable    =>0,
+                dataobjattr   =>'lnksimonpkgrec.id'),
+                                                 
+      new kernel::Field::TextDrop(
+                name          =>'system',
+                htmlwidth     =>'250px',
+                label         =>'System',
+                vjointo       =>'itil::system',
+                vjoinon       =>['systemid'=>'id'],
+                vjoindisp     =>'name',
+                readonly      =>1,
+                dataobjattr   =>'system.name'),
+                                                   
+      new kernel::Field::Link(
+                name          =>'systemid',
+                label         =>'System ID',
+                dataobjattr   =>'system.id',
+                wrdataobjattr =>'lnksimonpkgrec.system'),
+
+      new kernel::Field::TextDrop(
+                name          =>'monpkg',
+                htmlwidth     =>'250px',
+                label         =>'Installationpackage',
+                vjoineditbase =>{'cistatusid'=>"<5"},
+                vjointo       =>'SIMon::monpkg',
+                vjoinon       =>['monpkgid'=>'id'],
+                vjoindisp     =>'name',
+                readonly      =>1,
+                dataobjattr   =>'simonpkg.name'),
+                                                   
+      new kernel::Field::Link(
+                name          =>'monpkgid',
+                label         =>'MonPkg ID',
+                dataobjattr   =>'simonpkg.id',
+                wrdataobjattr =>'lnksimonpkgrec.simonpkg'),
+
+      new kernel::Field::Link(
+                name          =>'monpkgrestrictarget',
+                label         =>'MonPkg Restriction target',
+                readonly      =>1,
+                dataobjattr   =>'simonpkg.restrictarget'),
+
+      new kernel::Field::Link(
+                name          =>'monpkgrestriction',
+                label         =>'MonPkg Restriction',
+                readonly      =>1,
+                dataobjattr   =>'simonpkg.restriction'),
+
+
+      new kernel::Field::Link(
+                name          =>'needrefresh',
+                label         =>'kg Restriction',
+                readonly      =>1,
+                dataobjattr   =>"if (lnksimonpkgrec.modifydate<simonpkg.modifydate,1,0)"),
+
+
+
+      new kernel::Field::Select(
+                name          =>'reqtarget',
+                label         =>'target state',
+                transprefix   =>'TARGET.',
+                value         =>[qw(MAND NEDL RECO)],
+                allownative   =>1,
+                allowempty    =>1,
+                readonly      =>1,
+                useNullEmpty  =>1,
+                dataobjattr   =>'lnksimonpkgrec.reqtarget'),
+
+      new kernel::Field::Interface(
+                name          =>'rawreqtarget',
+                label         =>'raw request target',
+                dataobjattr   =>'lnksimonpkgrec.reqtarget'),
+
+      new kernel::Field::Text(
+                name          =>'comments',
+                searchable    =>0,
+                label         =>'Comments',
+                dataobjattr   =>'lnksimonpkgrec.comments'),
+
+      new kernel::Field::Creator(
+                name          =>'creator',
+                group         =>'source',
+                label         =>'Creator',
+                dataobjattr   =>'lnksimonpkgrec.createuser'),
+                                   
+      new kernel::Field::Owner(
+                name          =>'owner',
+                group         =>'source',
+                label         =>'last Editor',
+                dataobjattr   =>'lnksimonpkgrec.modifyuser'),
+                                   
+      new kernel::Field::Text(
+                name          =>'srcsys',
+                group         =>'source',
+                label         =>'Source-System',
+                dataobjattr   =>'lnksimonpkgrec.srcsys'),
+                                                   
+      new kernel::Field::Text(
+                name          =>'srcid',
+                group         =>'source',
+                htmldetail    =>'NotEmpty',
+                label         =>'Source-Id',
+                dataobjattr   =>'lnksimonpkgrec.srcid'),
+                                                   
+      new kernel::Field::Date(
+                name          =>'srcload',
+                group         =>'source',
+                htmldetail    =>'NotEmpty',
+                label         =>'Last-Load',
+                dataobjattr   =>'lnksimonpkgrec.srcload'),
+                                                   
+      new kernel::Field::CDate(
+                name          =>'cdate',
+                group         =>'source',
+                label         =>'installation monitoring start',
+                dataobjattr   =>'lnksimonpkgrec.createdate'),
+                                                
+      new kernel::Field::MDate(
+                name          =>'mdate',
+                group         =>'source',
+                label         =>'Modification-Date',
+                dataobjattr   =>'lnksimonpkgrec.modifydate'),
+                                                   
+      new kernel::Field::Editor(
+                name          =>'editor',
+                group         =>'source',
+                label         =>'Editor Account',
+                dataobjattr   =>'lnksimonpkgrec.editor'),
+                                                  
+      new kernel::Field::RealEditor(
+                name          =>'realeditor',
+                group         =>'source',
+                label         =>'real Editor Account',
+                dataobjattr   =>'lnksimonpkgrec.realeditor'),
+   );
+   $self->setDefaultView(qw(system monpkg reqtarget cdate));
+   $self->setWorktable("lnksimonpkgrec");
+   return($self);
+}
+
+
+sub initSqlWhere
+{
+   my $self=shift;
+   my $mode=shift;
+   my $where="";
+   if ($mode eq "select"){
+      $where="((system.cistatus in (3,4) and simonpkg.cistatus=4) ".
+             "or lnksimonpkgrec.id is not null)";
+   }
+   return($where);
+}
+
+
+sub getSqlFrom
+{
+   my $self=shift;
+   my $mode=shift;
+   my @flt=@_;
+   my ($worktable,$workdb)=$self->getWorktable();
+
+
+   my $from="system cross join simonpkg left outer join lnksimonpkgrec ".
+            "on (lnksimonpkgrec.simonpkg=simonpkg.id and ".
+            "    lnksimonpkgrec.system=system.id) ";
+
+#   my $from="$worktable join simonpkg ".
+#            "on lnksimonpkgrec.simonpkg=simonpkg.id ";
+
+   return($from);
+}
+
+
+#sub getRecordImageUrl
+#{
+#   my $self=shift;
+#   my $cgi=new CGI({HTTP_ACCEPT_LANGUAGE=>$ENV{HTTP_ACCEPT_LANGUAGE}});
+#   return("../../../public/itil/load/lnkitfarmasset.jpg?".$cgi->query_string());
+#}
+
+#sub SelfAsParentObject    # this method is needed because existing derevations
+#{
+#   return("itil::lnkitfarmasset");
+#}
+#
+
+sub isQualityCheckValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return(0);
+}
+
+
+sub getDetailBlockPriority
+{  
+   my $self=shift;
+   return(qw(header default source));
+}
+
+
+sub Validate
+{
+   my $self=shift;
+   my $oldrec=shift;
+   my $newrec=shift;
+   my $origrec=shift;
+
+   my $monpkgid=effVal($oldrec,$newrec,"monpkgid");
+
+   if ($self->isDataInputFromUserFrontend()){
+      if (!$self->isWriteOnMonPkgValid($monpkgid,"software")){
+         $self->LastMsg(ERROR,"no access");
+         return(undef);
+      }
+   }
+
+
+   return(1);
+}
+
+
+sub isViewValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return("ALL");
+}
+
+
+sub isWriteValid
+{
+   my $self=shift;
+   my $oldrec=shift;
+   my $newrec=shift;
+   my @editgroup=("default");
+
+   return(@editgroup) if (!defined($oldrec) && !defined($newrec));
+   my $monpkgid=$oldrec->{monpkgid};
+   return(@editgroup) if ($self->IsMemberOf("admin"));
+   return(@editgroup) if ($self->isWriteOnMonPkgValid($monpkgid,"software"));
+
+   return(undef);
+}
+
+
+sub isWriteOnMonPkgValid
+{
+   my $self=shift;
+   my $monpkgid=shift;
+   my $group=shift;
+
+   my $monpkg=$self->getPersistentModuleObject("SIMon::monpkg");
+   $monpkg->SetFilter({id=>\$monpkgid});
+   my ($arec,$msg)=$monpkg->getOnlyFirst(qw(ALL));
+   my @g=$monpkg->isWriteValid($arec);
+   if (grep(/^ALL$/,@g) || grep(/^$group$/,@g)){
+      return(1);
+   }
+   return(0);
+}
+
+
+
+
+
+1;
---
+++ mod/SIMon/monpkg.pm
@@ -0,0 +1,433 @@
+package SIMon::monpkg;
+#  W5Base Framework
+#  Copyright (C) 2022  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::App::Web;
+use kernel::DataObj::DB;
+use kernel::Field;
+use kernel::CIStatusTools;
+@ISA=qw(kernel::App::Web::Listedit kernel::DataObj::DB kernel::CIStatusTools);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   $param{MainSearchFieldLines}=3;
+
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   $self->AddFields(
+      new kernel::Field::Linenumber(
+                name          =>'linenumber',
+                label         =>'No.'),
+
+      new kernel::Field::Id(
+                name          =>'id',
+                sqlorder      =>'desc',
+                group         =>'source',
+                label         =>'W5BaseID',
+                dataobjattr   =>'simonpkg.id'),
+                                                  
+      new kernel::Field::Text(
+                name          =>'name',
+                label         =>'Name',
+                dataobjattr   =>'simonpkg.name'),
+
+      new kernel::Field::Mandator(),
+
+      new kernel::Field::Link(
+                name          =>'mandatorid',
+                dataobjattr   =>'simonpkg.mandator'),
+
+      new kernel::Field::Select(
+                name          =>'cistatus',
+                htmleditwidth =>'40%',
+                label         =>'CI-State',
+                vjoineditbase =>{id=>">0 AND <7"},
+                default       =>'4',
+                vjointo       =>'base::cistatus',
+                vjoinon       =>['cistatusid'=>'id'],
+                vjoindisp     =>'name'),
+
+      new kernel::Field::Link(
+                name          =>'cistatusid',
+                label         =>'CI-StateID',
+                dataobjattr   =>'simonpkg.cistatus'),
+
+      new kernel::Field::Databoss(),
+
+      new kernel::Field::Select(
+                name          =>'restrictarget',
+                label         =>'target state',
+                transprefix   =>'TARGET.',
+                translation   =>'SIMon::lnkmonpkgrec',
+                value         =>[qw(MAND RECO)],
+                dataobjattr   =>'simonpkg.restrictarget'),
+
+      new kernel::Field::Textarea(
+                name          =>'restriction',
+                label         =>'logical system restriction',
+                dataobjattr   =>'simonpkg.restriction'),
+
+      new kernel::Field::Link(
+                name          =>'databossid',
+                dataobjattr   =>'simonpkg.databoss'),
+
+      new kernel::Field::SubList(
+                name          =>'software',
+                label         =>'Software',
+                group         =>'software',
+                subeditmsk    =>'subedit.software',
+                vjointo       =>'SIMon::lnkmonpkgsoftware',
+                vjoinon       =>['id'=>'monpkgid'],
+                vjoindisp     =>['software']),
+
+      new kernel::Field::ContactLnk(
+                name          =>'contacts',
+                label         =>'Contacts',
+                class         =>'mandator',
+                vjoinbase     =>[{'parentobj'=>\'SIMon::monpkg'}],
+                vjoininhash   =>['targetid','target','roles'],
+                group         =>'contacts'),
+
+      new kernel::Field::Textarea(
+                name          =>'comments',
+                group         =>'misc',
+                label         =>'Comments',
+                dataobjattr   =>'simonpkg.comments'),
+
+      new kernel::Field::Container(
+                name          =>'additional',
+                label         =>'Additionalinformations',
+                uivisible     =>sub{
+                   my $self=shift;
+                   my $mode=shift;
+                   my %param=@_;
+                   my $rec=$param{current};
+                   if (!defined($rec->{$self->Name()})){
+                      return(0);
+                   }
+                   return(0);
+                   return(1);
+                },
+                dataobjattr   =>'simonpkg.additional'),
+
+      new kernel::Field::Text(
+                name          =>'srcsys',
+                group         =>'source',
+                label         =>'Source-System',
+                dataobjattr   =>'simonpkg.srcsys'),
+                                                   
+      new kernel::Field::Text(
+                name          =>'srcid',
+                group         =>'source',
+                label         =>'Source-Id',
+                dataobjattr   =>'simonpkg.srcid'),
+                                                   
+      new kernel::Field::Date(
+                name          =>'srcload',
+                group         =>'source',
+                history       =>0,
+                label         =>'Source-Load',
+                dataobjattr   =>'simonpkg.srcload'),
+
+      new kernel::Field::CDate(
+                name          =>'cdate',
+                group         =>'source',
+                sqlorder      =>'desc',
+                label         =>'Creation-Date',
+                dataobjattr   =>'simonpkg.createdate'),
+                                                  
+      new kernel::Field::MDate(
+                name          =>'mdate',
+                group         =>'source',
+                sqlorder      =>'desc',
+                label         =>'Modification-Date',
+                dataobjattr   =>'simonpkg.modifydate'),
+
+      new kernel::Field::Creator(
+                name          =>'creator',
+                group         =>'source',
+                label         =>'Creator',
+                dataobjattr   =>'simonpkg.createuser'),
+
+      new kernel::Field::Owner(
+                name          =>'owner',
+                group         =>'source',
+                label         =>'last Editor',
+                dataobjattr   =>'simonpkg.modifyuser'),
+
+      new kernel::Field::Editor(
+                name          =>'editor',
+                group         =>'source',
+                label         =>'Editor Account',
+                dataobjattr   =>'simonpkg.editor'),
+
+      new kernel::Field::RealEditor(
+                name          =>'realeditor',
+                group         =>'source',
+                label         =>'real Editor Account',
+                dataobjattr   =>'simonpkg.realeditor'),
+
+      new kernel::Field::Link(
+                name          =>'sectarget',
+                noselect      =>'1',
+                dataobjattr   =>'lnkcontact.target'),
+
+      new kernel::Field::Link(
+                name          =>'sectargetid',
+                noselect      =>'1',
+                dataobjattr   =>'lnkcontact.targetid'),
+
+      new kernel::Field::Link(
+                name          =>'secroles',
+                noselect      =>'1',
+                dataobjattr   =>'lnkcontact.croles'),
+
+
+   #   new kernel::Field::IssueState(),
+   #   new kernel::Field::QualityText(),
+   #   new kernel::Field::QualityState(),
+   #   new kernel::Field::QualityOk(),
+   #   new kernel::Field::QualityLastDate(
+   #             dataobjattr   =>'simonpkg.lastqcheck'),
+   #   new kernel::Field::QualityResponseArea()
+   );
+   $self->{use_distinct}=1;
+   $self->{history}={
+      update=>[
+         'local'
+      ]
+   };
+   $self->setDefaultView(qw(name cistatus restrictarget mdate));
+   $self->setWorktable("simonpkg");
+   return($self);
+}
+
+
+sub getDetailBlockPriority
+{
+   my $self=shift;
+   return(qw(header default software contacts misc source));
+}
+
+
+sub getSqlFrom
+{
+   my $self=shift;
+   my ($worktable,$workdb)=$self->getWorktable();
+   my $from="$worktable left outer join lnkcontact ".
+            "on lnkcontact.parentobj in ('SIMon::monpkg') ".
+            "and $worktable.id=lnkcontact.refid";
+
+   return($from);
+}
+
+
+sub initSearchQuery
+{
+   my $self=shift;
+   if (!defined(Query->Param("search_cistatus"))){
+     Query->Param("search_cistatus"=>
+                  "\"!".$self->T("CI-Status(6)","base::cistatus")."\"");
+   }
+}
+
+
+
+sub Validate
+{
+   my $self=shift;
+   my $oldrec=shift;
+   my $newrec=shift;
+
+   ########################################################################
+   # standard security handling
+   #
+   if ($self->isDataInputFromUserFrontend()){
+      my $userid=$self->getCurrentUserId();
+      if (!defined($oldrec)){
+         if (!defined($newrec->{databossid}) ||
+             $newrec->{databossid}==0){
+            $newrec->{databossid}=$userid;
+         }
+      }
+      if (!$self->IsMemberOf("admin")){
+         if (defined($newrec->{databossid}) &&
+             $newrec->{databossid}!=$userid &&
+             $newrec->{databossid}!=$oldrec->{databossid}){
+            $self->LastMsg(ERROR,"you are not authorized to set other persons ".
+                                 "as databoss");
+            return(0);
+         }
+      }
+   }
+   ########################################################################
+   my $name=effVal($oldrec,$newrec,"name");
+
+   if (effVal($oldrec,$newrec,"cistatusid")>5){
+      $name=~s/\[[0-9]+\]$//;
+   }
+   if (!($name=~m/[a-z0-9_-]/i)){
+      $self->LastMsg(ERROR,"invalid installation package name");
+      return(0);
+   }
+
+   my $rest=effVal($oldrec,$newrec,"restriction");
+   if ($rest ne ""){
+      my $p=new Text::ParseWhere();
+      if (!defined($p->compileExpression($rest))){
+         $self->LastMsg(ERROR,"invalid restriction expression");
+         return(undef);
+      }
+   }
+
+
+
+
+
+
+   return(0) if (!$self->HandleCIStatusModification($oldrec,$newrec,"name"));
+
+
+   return(1);
+}
+
+
+
+sub SecureSetFilter
+{
+   my $self=shift;
+   my @flt=@_;
+
+   if (!$self->IsMemberOf("admin") && 
+       $W5V2::OperationContext ne "W5Replicate"){
+      my @mandators=$self->getMandatorsOf($ENV{REMOTE_USER},"read");
+      my %grps=$self->getGroupsOf($ENV{REMOTE_USER},
+          [orgRoles(),qw(RCFManager RCFManager2 RAuditor RMonitor)],"both");
+      my @grpids=keys(%grps);
+      my $userid=$self->getCurrentUserId();
+      push(@flt,[
+         {mandatorid=>\@mandators},
+         {databossid=>$userid},
+         {sectargetid=>\$userid,sectarget=>\'base::user',
+          secroles=>"*roles=?write?=roles* *roles=?privread?=roles* ".
+                    "*roles=?read?=roles*"},
+         {sectargetid=>\@grpids,sectarget=>\'base::grp',
+          secroles=>"*roles=?write?=roles* *roles=?privread?=roles* ".
+                    "*roles=?read?=roles*"},
+      ]);
+   }
+   return($self->SetFilter(@flt));
+}
+
+
+
+
+sub FinishWrite
+{
+   my $self=shift;
+   my $oldrec=shift;
+   my $newrec=shift;
+   my $bak=$self->SUPER::FinishWrite($oldrec,$newrec);
+   $self->NotifyOnCIStatusChange($oldrec,$newrec);
+   return($bak);
+}
+
+#sub getRecordImageUrl
+#{
+#   my $self=shift;
+#   my $cgi=new CGI({HTTP_ACCEPT_LANGUAGE=>$ENV{HTTP_ACCEPT_LANGUAGE}});
+#   return("../../../public/itil/load/simonpkg.jpg?".$cgi->query_string());
+#}
+
+
+
+sub isViewValid
+{
+   my $self=shift;
+   my $rec=shift;
+
+   return("header","default") if (!defined($rec));
+   return("ALL");
+}
+
+sub isWriteValid
+{
+   my $self=shift;
+   my $rec=shift;
+   my $userid=$self->getCurrentUserId();
+
+   my @databossedit=qw(default software contacts misc);
+   if (!defined($rec)){
+      return(@databossedit);
+   }
+   else{
+      if ($rec->{databossid}==$userid){
+         return(@databossedit);
+      }
+      if ($self->IsMemberOf("admin")){
+         return(@databossedit);
+      }
+      if (defined($rec->{contacts}) && ref($rec->{contacts}) eq "ARRAY"){
+         my %grps=$self->getGroupsOf($ENV{REMOTE_USER},
+                                     ["RMember"],"both");
+         my @grpids=keys(%grps);
+         foreach my $contact (@{$rec->{contacts}}){
+            if ($contact->{target} eq "base::user" &&
+                $contact->{targetid} ne $userid){
+               next;
+            }
+            if ($contact->{target} eq "base::grp"){
+               my $grpid=$contact->{targetid};
+               next if (!grep(/^$grpid$/,@grpids));
+            }
+            my @roles=($contact->{roles});
+            @roles=@{$contact->{roles}} if (ref($contact->{roles}) eq "ARRAY");
+            return(@databossedit) if (grep(/^write$/,@roles));
+         }
+      }
+      my @chkgroups;
+      push(@chkgroups,$rec->{mandatorid}) if ($rec->{mandatorid} ne "");
+      if ($#chkgroups!=-1){
+         if ($self->IsMemberOf(\@chkgroups,["RDataAdmin"],"down")){
+            return(@databossedit);
+         }
+      }
+   }
+   return(undef);
+}
+
+
+
+sub SelfAsParentObject    # this method is needed because existing derevations
+{
+   return("SIMon::monpkg");
+}
+
+
+
+
+
+
+
+
+
+1;
