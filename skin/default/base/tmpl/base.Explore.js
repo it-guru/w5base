edd5cb80fb537653cd536c1b63760127b2ec8f20
a295897 <hartmut.vogler@t-systems.com>
2022-08-19 16:51:27 +0200

    play around with W5Export in servicetree context based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16584909740001
    (cherry picked from commit 84f5914b627052f1adc9b5cd1e07a46981e740c2)

--- skin/default/base/tmpl/base.Explore.js
+++ skin/default/base/tmpl/base.Explore.js
@@ -527,37 +527,36 @@ div#SearchResult{
 <script langauge="JavaScript">
 
 
-function wrapText(text, limit, breakpoints) {
+function wrapText(long_string, max_char) {
 
-  var parts = text.split(' ');
-  var lines = [];
-  text = parts[0];
-  parts.shift();
+  var sumchar_of_words=function(word_array){
+    var out=0;
+    if (word_array.length!=0){
+      for (var i=0; i<word_array.length; i++){
+        var word = word_array[i];
+        out=out+word.length;
+      }
+    };
+    return out;
+  }
 
-  while (parts.length > 0) {
-    var newText = `${text} ${parts[0]}`;
-
-    if (newText.length > limit) {
-      lines.push(`${text}\n`);
-      breakpoints--;
-
-      if (breakpoints === 0) {
-        lines.push(parts.join(' '));
-        break;
-      } else {
-         text = parts[0];
-     }
-    } else {
-      text = newText;
+  var split_out=[[]];
+  var split_string = long_string.split(' ');
+  for (var i=0; i<split_string.length; i++){
+    var word = split_string[i];
+    
+    if ((sumchar_of_words(split_out[split_out.length-1])+word.length)>max_char){
+      split_out=split_out.concat([[]]);
     }
-     parts.shift();
+    
+    split_out[split_out.length-1]=split_out[split_out.length-1].concat(word);
   }
-
-  if (lines.length === 0) {
-    return text;
-  } else {
-    return lines.join('');
+  
+  for (var i=0;i<split_out.length;i++){
+    split_out[i]=split_out[i].join(" ");
   }
+  
+  return split_out.join('\n');
 }
 
 
