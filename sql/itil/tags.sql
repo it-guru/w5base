022b5d3268e86011d45facc651ec51b907907ee4
a295897 <hartmut.vogler@t-systems.com>
2022-09-14 09:52:51 +0200

    next level of ItemTag handling based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16626511350001
    (cherry picked from commit 0182557011f657fc97bc6810a4212e8141f42b00)

--- sql/itil/tags.sql
+++ sql/itil/tags.sql
@@ -6,6 +6,7 @@ create table tag_appl (
   uname        varchar(40),
   name         varchar(40) NOT NULL,
   value        varchar(1024) NOT NULL,
+  ishidden     int(1)      default '0',
   comments     longtext    default NULL,
   createdate   datetime NOT NULL default '0000-00-00 00:00:00',
   modifydate   datetime NOT NULL default '0000-00-00 00:00:00',
@@ -30,6 +31,7 @@ create table tag_system (
   uname        varchar(40),
   name         varchar(40) NOT NULL,
   value        varchar(1024) NOT NULL,
+  ishidden     int(1)      default '0',
   comments     longtext    default NULL,
   createdate   datetime NOT NULL default '0000-00-00 00:00:00',
   modifydate   datetime NOT NULL default '0000-00-00 00:00:00',
@@ -45,6 +47,31 @@ create table tag_system (
   UNIQUE KEY `uname` (uname,refid),
   FOREIGN KEY refrecord (refid)
   REFERENCES appl (id) ON DELETE CASCADE,
+  KEY refid (refid),
+  UNIQUE KEY `srcsys` (srcsys,srcid)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+create table tag_swinstance (
+  id           bigint(20) NOT NULL,
+  refid        bigint(20) NOT NULL,
+  uname        varchar(40),
+  name         varchar(40) NOT NULL,
+  value        varchar(1024) NOT NULL,
+  ishidden     int(1)      default '0',
+  comments     longtext    default NULL,
+  createdate   datetime NOT NULL default '0000-00-00 00:00:00',
+  modifydate   datetime NOT NULL default '0000-00-00 00:00:00',
+  createuser   bigint(20) default NULL,
+  modifyuser   bigint(20) default NULL,
+  editor       varchar(100) NOT NULL default '',
+  realeditor   varchar(100) NOT NULL default '',
+  srcsys       varchar(100) default 'w5base',
+  srcid        varchar(20) default NULL,
+  srcload      datetime    default NULL,
+  PRIMARY KEY  (id),
+  KEY name (name),
+  UNIQUE KEY `uname` (uname,refid),
+  FOREIGN KEY refrecord (refid)
+  REFERENCES swinstance (id) ON DELETE CASCADE,
   KEY refid (refid),
   UNIQUE KEY `srcsys` (srcsys,srcid)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
