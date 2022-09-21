0091a7c720a3f5c3aea72048d11458c843b9dbbb
a295897 <hartmut.vogler@t-systems.com>
2022-09-21 10:23:13 +0200

    fixup spelling Assingment->Assignment based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16637397280003

--- mod/tsacinv/itclustservice.pm
+++ mod/tsacinv/itclustservice.pm
@@ -301,7 +301,7 @@ sub Import
          return(undef);
       }
       #printf STDERR Dumper($sysrec);
-      # check 2: Assingment Group active
+      # check 2: Assignment Group active
       my $acgroup=getModuleObject($self->Config,"tsacinv::group");
       $acgroup->SetFilter({lgroupid=>\$sysrec->{lassignmentid}});
       my ($acgrouprec,$msg)=$acgroup->getOnlyFirst(qw(supervisorldapid));
