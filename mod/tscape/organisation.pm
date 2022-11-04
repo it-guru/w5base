09a506456cfb758784517c3edb068a9b07f6a52b
a295897 <hartmut.vogler@t-systems.com>
2022-11-04 13:56:33 +0100

    add new dataobj tscape::organisation based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16675654400001
    (cherry picked from commit 16db8c7436a46c25a0553f6d6341438db4b9b079)

---
+++ mod/tscape/organisation.pm
@@ -0,0 +1,70 @@
+package tscape::organisation;
+#  W5Base Framework
+#  Copyright (C) 2022  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::App::Web;
+use kernel::DataObj::DB;
+use kernel::Field;
+@ISA=qw(kernel::App::Web::Listedit kernel::DataObj::DB);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   $self->AddFields(
+      new kernel::Field::Text(
+                name          =>'name',
+                label         =>'Name',
+                dataobjattr   =>'Organisation'),
+   );
+   $self->setDefaultView(qw(name));
+   $self->setWorktable("V_DARWIN_EXPORT");
+   return($self);
+}
+
+
+sub Initialize
+{
+   my $self=shift;
+
+   my @result=$self->AddDatabase(DB=>new kernel::database($self,"tscape"));
+   return(@result) if (defined($result[0]) && $result[0] eq "InitERROR");
+   return(1) if (defined($self->{DB}));
+   return(0);
+}
+
+
+sub isQualityCheckValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return(0);
+}
+
+
+
+
+         
+
+
+
+1;
