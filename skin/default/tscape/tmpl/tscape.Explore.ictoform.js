83990b4ac03f1d8d3824f3a8ee01a157debc004b
a295897 <hartmut.vogler@t-systems.com>
2022-11-08 11:04:07 +0100

    add AppletClass concept for Formulars based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16675654400001

--- skin/default/tscape/tmpl/tscape.Explore.ictoform.js
+++ skin/default/tscape/tmpl/tscape.Explore.ictoform.js
@@ -1,82 +1,22 @@
 var applet='%SELFNAME%';
 
-define(["datadumper","W5ExploreForms","AjaxAutocomplete"],function (Dumper,W5ExploreForms,AjaxAutocomplete){
+define(["datadumper","W5ExploreForms","AjaxAutocomplete"],
+ function (Dumper,W5ExploreForms,AjaxAutocomplete){
+   var W5Form=new W5ExploreForms();   // instance AppletClass object
+
    ClassAppletLib[applet].class=function(app){
       ClassApplet.call(this,app);
    };
    $.extend(ClassAppletLib[applet].class.prototype,ClassApplet.prototype);
-
-
-
-   ClassAppletLib[applet].class.prototype.removeForm=function(formname){
-      var appletobj=this;
-      var workspace=this.app.workspace;
-      $("#formspace #"+formname).remove();
-      $("#formspace>div:last").find('.readonly').remove();
-      $("#formspace>div:last").removeAttr('inert');
-      $("#formspace>div").last().find('button, a, input, '+
-                           'select, textarea').removeAttr('readonly');
-      $("#formspace>div").last().find('button, a, input, select, textarea,'+
-                                   ' [tabindex]:not([tabindex="-1"])').first()
-                                   .focus(); 
-      $("#formspace>div").last().each(function(){
-         appletobj.activateForm(this,$(this).attr("id"));
-      });
+   ClassAppletLib[applet].class.prototype.AppletClass=W5Form;
+   ClassAppletLib[applet].class.prototype.SELFNAME=function(){return(applet);};
+   ClassAppletLib[applet].class.prototype.TemplBase=function(){
+      return('tscape/load/tmpl/tscape.Explore.ictoform');
    };
 
-
-
-   ClassAppletLib[applet].class.prototype.setLastDivReadOnly=function(){
-      $(".detailframe").append("<div class=readonly "+
-          "style=\"position:absolute; top:0px; left:0px; "+
-          "background-color: rgba(192, 192, 192, 0.1); "+
-          "width: 100%; height: 100%; z-index: 2;\">"+
-          "</div>");
-      $("#formspace>div").last().attr('inert','1');
-      //$("#formspace>div").last().find('button, a, input, '+
-      //                             'select, textarea')
-      //                   .attr('readonly', true);
-   }
-
-
-   ClassAppletLib[applet].class.prototype.addForm=function(formname,callback){
-      var appletobj=this;
-      var workspace=this.app.workspace;
-      this.removeForm(formname);
-      $.get("../../tscape/load/tmpl/tscape.Explore.ictoform."+formname+"?RAW=1",
-           function(data, textStatus, jqXHR) {
-              appletobj.setLastDivReadOnly();
-              $("#formspace").append("<div "+
-                   "style=\"position:relative\" id=\""+formname+"\">"+
-                   data+"</div>");
-              $(".detailframe").css({position:'relative'});
-              $("#"+formname).find('button, a, input, select, textarea,'+
-                                   ' [tabindex]:not([tabindex="-1"])').first()
-                                   .focus(); 
-              $("#"+formname).find('div[data-type=template]').each(function(){
-                 var templ=$(this).attr("data-template");
-                 var target=this;
-                 $.ajax({
-                    url:"../../tscape/load/tmpl/"+templ,
-                    data:{RAW:'1'},
-                    headers:{Accept:'text/html; charset=UTF-8;'},
-                    success:function(d){
-                       $(target).html(d);
-                    }
-                 });
-              });
-              appletobj.initForm($("#"+formname),formname);
-              appletobj.activateForm($("#"+formname),formname);
-              if (callback){
-                 callback();
-              }
-           }
-      );
-   };
 
    /////////////////////////////////////////////////////////////////
    // Form Event methods
-
    ClassAppletLib[applet].class.prototype.ValidateFormStep=function(formname){
       if (formname=='subform1'){
          var formok=true;
@@ -113,12 +53,14 @@ define(["datadumper","W5ExploreForms","AjaxAutocomplete"],function (Dumper,W5Exp
          var formname=$("#formspace>div").last().attr("id");
          if (formname=='subform1'){
             if (this.ValidateFormStep(formname)){
-               this.addForm("subform2");
+              // this.addForm("subform2");
+               this.AppletClass.addForm(this,"subform2");
             }
          }
          else if (formname=='subform2'){
             if (this.ValidateFormStep(formname)){
-               this.addForm("subformCheck");
+               //this.addForm("subformCheck");
+               this.AppletClass.addForm(this,"subformCheck");
             }
          }
       }
@@ -255,7 +197,6 @@ define(["datadumper","W5ExploreForms","AjaxAutocomplete"],function (Dumper,W5Exp
    };
 
 
-
    ClassAppletLib[applet].class.prototype.activateForm=function(frm,formname){
       var appletobj=this;
 
@@ -277,67 +218,12 @@ define(["datadumper","W5ExploreForms","AjaxAutocomplete"],function (Dumper,W5Exp
    /////////////////////////////////////////////////////////////////
 
 
-
-
    ClassAppletLib[applet].class.prototype.run=function(){
       var appletobj=this;
       var app=this.app;
 
-      this.app.LayoutSimple();
-      this.app.console.log("INFO","loading scenario ...");
-      appletobj.app.setMPath(
-          {
-             label:ClassAppletLib['%SELFNAME%'].desc.label,
-             mtag:'%SELFNAME%'
-          }
-      );
-      app.loadCss("public/base/load/Output.HtmlDetail.css");
-      app.loadCss("public/base/load/jquery.AjaxAutocomplete.css");
-      this.app.workspace.innerHTML="<div id=formspace "+
-         "style=\"width:100%;height:10px;border-style:solid:border-width:1px;"+
-         "border-color:darkgray;overflow:auto\"></div>"+
-         "<div id=formcontroler style=\"height:2px;border-top-style:solid;"+
-         "border-top-width:2px;border-top-color:black\">"+
-         "<table width=100% border=0 cellspacing=5 cellpadding=5>"+
-         "<tr>"+
-         "<td wdth=25%>"+
-         "<input style=\"width:80px\" type=button class=FormOperation "+
-         "value='abort' id='FormOperationBreak'>"+
-         "</td>"+
-         "<td wdth=25% align=center>"+
-         "<input style=\"width:80px\" type=button class=FormOperation "+
-         "value='back' id='FormOperationBack'>"+
-         "</td>"+
-         "<td width=25% align=center>"+
-         "<input style=\"width:80px\" type=button class=FormOperation "+
-         "value='next' id='FormOperationNext'>"+
-         "</td>"+
-         "<td width=25% align=right>"+
-         "<input style=\"width:80px\" type=button class=FormOperation "+
-         "value='save' id='FormOperationSave'>"+
-         "</td>"+
-         "</tr>"+
-         "</table>"+
-         "</div>";
-      $(".FormOperation").css({cursor:'pointer'});
-      $(".FormOperation").click(function(e){
-         return(appletobj.FormOperation(e));
-      });
-      $(".spinner").hide();
-
-      function resizeModalHandler(e){
-         var workspace=$("#workspace");
-         var h=$(workspace).height();
-         $(workspace).find('#formcontroler').height(40);
-         $(workspace).find('#formspace').height((h-40));
-         if (e){
-            e.stopPropagation();
-         }
-      }
-      $(window).on('resize',resizeModalHandler);
-      resizeModalHandler();
-      // init the 1st subform
-      appletobj.addForm("subform1");
+      W5Form.run(appletobj);   // default runhandler for AppletClass
+      appletobj.AppletClass.addForm(appletobj,"subform1");
    };
    return(ClassAppletLib[applet].class);
 });
