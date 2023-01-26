efa92417073d8c73d0e7e8d5d0af2abd34172dc7
a295897 <hartmut.vogler@t-systems.com>
2023-01-26 14:35:03 +0100

    add itnormodel derivation from appl based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16690449760001
    (cherry picked from commit 70f60ffcf6c90d943e43e70915e9956bea06a025)

--- sql/itil/itinv.sql
+++ sql/itil/itinv.sql
@@ -1842,3 +1842,4 @@ alter table accessurl add ssl_certissuerdn varchar(256) default null;
 alter table asset add notifyplandecons1 datetime default NULL,add notifyplandecons2 datetime default NULL;
 alter table asset add eohscomments longtext;
 alter table system add issoximpl int(2) default NULL;
+alter table system add reqitnormodel bigint(20);
