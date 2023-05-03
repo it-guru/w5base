078348b6a597e45a3fd9160e76ab3875e9cca344
a295897 <hartmut.vogler@t-systems.com>
2023-05-03 15:47:08 +0200

    tread 404 responses as successfull empty answer based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16831180690001
    (cherry picked from commit 8f4dffb94471760371b38e463e38400cfd51b7bc)

--- mod/tpc/deployment.pm
+++ mod/tpc/deployment.pm
@@ -159,6 +159,20 @@ sub DataCollector
              }
          } @$data);
          return($data);
+      },
+      onfail=>sub{
+         my $self=shift;
+         my $code=shift;
+         my $statusline=shift;
+         my $content=shift;
+         my $reqtrace=shift;
+
+         if ($code eq "404"){  # 404 bedeutet nicht gefunden
+            return([],"200");
+         }
+         msg(ERROR,$reqtrace);
+         $self->LastMsg(ERROR,"unexpected data TPC machine response");
+         return(undef);
       }
    );
    #print STDERR Dumper($d);
