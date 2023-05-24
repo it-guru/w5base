2330da95db20e39ad4c20e8446d6e33ddfec10ec
a295897 <hartmut.vogler@t-systems.com>
2023-05-24 14:54:14 +0200

    add event CleanupServiceAndSupport (bzw. Cleanup) based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16831140260002

---
+++ mod/itil/event/cleanup.pm
@@ -0,0 +1,94 @@
+package itil::event::cleanup;
+#  W5Base Framework
+#  Copyright (C) 2023  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::Event;
+@ISA=qw(kernel::Event);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   return($self);
+}
+
+sub Init
+{
+   my $self=shift;
+
+   $self->RegisterEvent("CleanupServiceAndSupport","CleanupServiceAndSupport");
+   $self->RegisterEvent("Cleanup","CleanupServiceAndSupport");
+   return(1);
+}
+
+
+sub CleanupServiceAndSupport
+{
+   my $self=shift;
+   msg(INFO,"ServiceAndSupport");
+
+
+   my $obj=getModuleObject($self->Config,"itil::servicesupport");
+   my $objop=$obj->Clone();
+
+
+   my %flt;
+   %flt=(mdate=>"<now-2M",cdate=>"<now-2M",cistatusid=>'4');
+
+   my @fields=$obj->getFieldObjsByView([qw(ALL)]);
+
+   foreach my $fld (@fields){
+      if ($fld->{name}=~m/^usecount/){
+         $flt{$fld->{name}}=\'0';
+      }
+   }
+
+   msg(INFO,"Cleanup itil::servicesupport filter:".Dumper(\%flt)); 
+
+   $obj->SetFilter(\%flt);
+   $obj->SetCurrentView(qw(ALL));
+
+   my ($rec,$msg)=$obj->getFirst(unbuffered=>1);
+   my $c=0;
+   my %o;
+   my $deletecount=0;
+   if (defined($rec)){
+      do{
+         $W5V2::HistoryComments="marked as disposed of wasted ".
+                                "because all usecounts are 0";
+         if ($objop->ValidatedUpdateRecord($rec,{cistatusid=>'6'},{
+                id=>$rec->{id}
+             })){
+            $deletecount++;
+         }
+         $W5V2::HistoryComments=undef;
+         ($rec,$msg)=$obj->getNext();
+      } until(!defined($rec));
+   }
+
+
+   return({exitcode=>0,msg=>"deletecount=>$deletecount"});
+}
+
+
+
+1;
