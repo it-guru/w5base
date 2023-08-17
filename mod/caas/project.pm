2a83c1c52ea465c91d45cd5b8673a9d4a25727eb
a295897 <hartmut.vogler@t-systems.com>
2023-08-17 16:08:58 +0200

    add 1st version of IT-Universe->Clouds->CaaS interface based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/16801137870001
    (cherry picked from commit c83a5c04743386d931c7deaed64a2b8714fc7dc2)

---
+++ mod/caas/project.pm
@@ -0,0 +1,206 @@
+package caas::project;
+#  W5Base Framework
+#  Copyright (C) 2023  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::App::Web;
+use kernel::App::Web::Listedit;
+use kernel::DataObj::Static;
+use kernel::Field;
+use MIME::Base64;
+use kernel::Field::TextURL;
+use Text::ParseWords;
+use Time::HiRes qw(usleep);
+@ISA=qw(kernel::App::Web::Listedit kernel::DataObj::Static);
+
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   $param{MainSearchFieldLines}=4 if (!exists($param{MainSearchFieldLines}));
+   my $self=bless($type->SUPER::new(%param),$type);
+
+
+   $self->AddFields(
+      new kernel::Field::Linenumber(
+                name          =>'linenumber',
+                label         =>'No.'),
+
+      new kernel::Field::RecordUrl(),
+
+      new kernel::Field::Id(
+                name          =>'id',
+                group         =>'source',
+                label         =>'id'),
+                                                  
+      new kernel::Field::Text(
+                name          =>'cloudid',
+                label         =>'CloudID',
+                dataobjattr   =>'cloudId'),
+
+      new kernel::Field::Text(
+                name          =>'name',
+                label         =>'Name',
+                dataobjattr   =>'fullName'),
+
+      new kernel::Field::Text(
+                name          =>'cluster',
+                label         =>'Cluster',
+                htmldetail    =>'NotEmpty',
+                dataobjattr   =>'cluster'),
+
+      new kernel::Field::Text(
+                name          =>'project',
+                label         =>'Projectname',
+                htmldetail    =>'NotEmpty',
+                dataobjattr   =>'project'),
+
+   );
+   $self->{'data'}=\&DataCollector;
+   $self->setDefaultView(qw(name id cluster project));
+   return($self);
+}
+
+
+
+sub getCredentialName
+{
+   my $self=shift;
+
+   return("CAASGATE");
+}
+
+
+
+sub isWriteValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return(undef);
+}
+
+
+
+sub Validate
+{
+   my $self=shift;
+   my $oldrec=shift;
+   my $newrec=shift;
+
+   return(1);
+}
+
+
+
+sub getDetailBlockPriority
+{
+   my $self=shift;
+   my $grp=shift;
+   my %param=@_;
+   return("header","default","soure");
+}
+
+
+
+sub isQualityCheckValid
+{
+   my $self=shift;
+   my $rec=shift;
+   return(0);
+}
+
+
+
+sub DataCollector
+{
+   my $self=shift;
+   my $filterset=shift;
+
+   my $CredName=$self->getCredentialName();
+
+   my @view=$self->GetCurrentView();
+
+   my $d=$self->CollectREST(
+      dbname=>$CredName,
+      requesttoken=>"QC".time(),
+      url=>sub{
+         my $self=shift;
+         my $baseurl=shift;
+         my $apikey=shift;
+         my $base=shift;
+      
+         my $dataobjurl=$baseurl;
+         $dataobjurl.="/" if (!($dataobjurl=~m/\/$/));
+         $dataobjurl.="v1/cloudAreas";
+         return($dataobjurl);
+      },
+
+      headers=>sub{
+         my $self=shift;
+         my $baseurl=shift;
+         my $apikey=shift;
+         my $apiuser=shift;
+         my $headers=[Authorization =>'Basic '.
+                                      encode_base64($apiuser.':'.$apikey)];
+         return($headers);
+      },
+      success=>sub{  # DataReformaterOnSucces
+         my $self=shift;
+         my $data=shift;
+
+         my @atomicSpecs=qw(cluster project);
+         if (ref($data) eq "ARRAY"){
+            foreach my $rec (@$data){
+               foreach my $k (@atomicSpecs){
+                  $rec->{$k}=undef;
+               }
+               if (ref($rec->{atomicSpecs}) eq "HASH"){
+                  foreach my $k (@atomicSpecs){
+                     if (exists($rec->{atomicSpecs}->{$k})){
+                        $rec->{$k}=$rec->{atomicSpecs}->{$k};
+                     }
+                  }
+                 
+               }
+               if ($rec->{operationalModel} eq "unknown"){
+                  $rec->{operationalModel}=undef;
+               }
+               $rec->{operationalModel}=uc($rec->{operationalModel});
+            }
+         }
+         return();
+      }
+   );
+
+   return($d);
+}
+
+
+
+#sub initSearchQuery
+#{
+#   my $self=shift;
+#   if (!defined(Query->Param("search_isexcluded"))){
+#     Query->Param("search_isexcluded"=>$self->T("no"));
+#   }
+#}
+
+
+1;
