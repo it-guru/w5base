4b1a56c692183c1b07a196e12f28478dc814bb1c
hartmut.vogler@telekom.de <hartmut.vogler@t-systems.com>
2025-03-18 14:04:45 +0100

    add TPC10 interface - 1st version based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/17419455690007

---
+++ mod/TPC10/menu/root.pm
@@ -0,0 +1,67 @@
+package TPC10::menu::root;
+#  W5Base Framework
+#  Copyright (C) 2023  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::MenuRegistry;
+@ISA=qw(kernel::MenuRegistry);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+   return($self);
+}
+
+sub Init
+{
+   my $self=shift;
+
+   $self->RegisterObj("itu.cloud.tpc.TPC10",
+                      "tmpl/welcome",
+                      defaultacl=>['valid_user']);
+
+   $self->RegisterObj("itu.cloud.tpc.TPC10.project",
+                      "TPC10::project",
+                      defaultacl=>['valid_user']);
+
+   $self->RegisterObj("itu.cloud.tpc.TPC10.machine",
+                      "TPC10::machine",
+                      defaultacl=>['valid_user']);
+
+   $self->RegisterObj("itu.cloud.tpc.TPC10.kernel",
+                      "tmpl/welcome",
+                      prio=>9999,
+                      defaultacl=>['admin']);
+
+   $self->RegisterObj("itu.cloud.tpc.TPC10.kernel.depl",
+                      "TPC10::deployment",
+                      defaultacl=>['valid_user']);
+
+   $self->RegisterObj("itu.cloud.tpc.TPC10.kernel.res",
+                      "TPC10::resource",
+                      defaultacl=>['valid_user']);
+
+   return($self);
+}
+
+
+
+1;
