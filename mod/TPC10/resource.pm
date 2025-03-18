8f0d4d3137d8ed74f2e8dcbbf96748523ec68ea4
hartmut.vogler@telekom.de <hartmut.vogler@t-systems.com>
2025-03-18 14:04:45 +0100

    add TPC10 interface - 1st version based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/17419455690007
    (cherry picked from commit 4b1a56c692183c1b07a196e12f28478dc814bb1c)

---
+++ mod/TPC10/resource.pm
@@ -0,0 +1,45 @@
+package TPC10::resource;
+#  W5Base Framework
+#  Copyright (C) 2024  Hartmut Vogler (it@guru.de)
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+use strict;
+use vars qw(@ISA);
+use kernel;
+use kernel::Field;
+use tpc::resource;
+@ISA=qw(tpc::resource);
+
+sub new
+{
+   my $type=shift;
+   my %param=@_;
+   my $self=bless($type->SUPER::new(%param),$type);
+
+   return($self);
+}
+
+
+sub getCredentialName
+{
+   my $self=shift;
+
+   return("TPC10");
+}
+
+
+
+1;
