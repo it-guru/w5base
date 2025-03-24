2ee8d6a2b239564654fe521e32d55f18b0bb7714
hartmut.vogler@telekom.de <hartmut.vogler@t-systems.com>
2025-03-24 09:35:09 +0100

    add TPC10 to static/DarwinIFTest/testall.sh based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/17419455690007

--- static/DarwinIFTest/testall.sh
+++ static/DarwinIFTest/testall.sh
@@ -19,6 +19,7 @@ TPC3::project         id         030db0c8-9a52-4340-be96-674052499f57
 TPC4::project         id         2962d5ca-f9b9-40d2-b519-61790ed643d5
 TPC6::project         id         034fde2d-8d77-4f09-90f9-372711b36a4a
 TPC8::project         id         03e7266a-2c13-40c4-883f-51233e194c6a
+TPC10::project        id         1080dbe0-baa9-4bc0-8c81-2fd37d1529c5
 azure::subscription   name       dtit_cid0067
 tsotc::project        id         772c762b9d48415f8f21f560010425ad
 caas::project         id         1BFZmaTCMhwCZBtNEpKm3qdHs1CZGNqYh2
@@ -28,7 +29,6 @@ base::user            posix         hvogler
 tscape::archappl      archapplid    ICTO-4175
 tsAuditSrv::system    systemname    EDE15Q
 FLEXERAatW5W::system  systemname    ede188
-tssm::dev             configitemid  S33162327
 tsacinv::system       systemname    EDE188
 tsadopt::vfarm        name          dcppt-fra01-s01-cl01
 tsciam::user          email         Peter.Testuser@external.telekom.de
