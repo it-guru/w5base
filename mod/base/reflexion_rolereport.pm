a4c7296fc7004c7c5973f7ed0efdfb6c410a6f1d
Hartmut Vogler <hartmut.vogler@t-systems.com>
2025-10-28 09:30:53 +0100

    finalize databoss report based on https://darwin.telekom.de/darwin/auth/base/workflow/ById/17603440350001
    (cherry picked from commit a32f1799761a280c567cc95e9d1c60d643ef3745)

--- mod/base/reflexion_rolereport.pm
+++ mod/base/reflexion_rolereport.pm
@@ -60,6 +60,16 @@ sub new
                 dataobjattr   =>'cntdataboss'),
 
       new kernel::Field::Boolean(
+                name          =>'isapplmgr',
+                label         =>'is databoos',
+                dataobjattr   =>'isapplmgr'),
+
+      new kernel::Field::Number(
+                name          =>'cntapplmgr',
+                label         =>'count of applmgr',
+                dataobjattr   =>'cntapplmgr'),
+
+      new kernel::Field::Boolean(
                 name          =>'istsm',
                 label         =>'is tsm (or tsm2)',
                 dataobjattr   =>'istsm'),
@@ -79,9 +89,39 @@ sub new
                 label         =>'count of opm (or opm2)',
                 dataobjattr   =>'cntopm'),
 
+      new kernel::Field::Boolean(
+                name          =>'isadmin',
+                label         =>'is admin (or admin2)',
+                dataobjattr   =>'isadmin'),
+
+      new kernel::Field::Number(
+                name          =>'cntadmin',
+                label         =>'count of admin (or admin2)',
+                dataobjattr   =>'cntadmin'),
+
    );
-   $self->setDefaultView(qw(mandator user isdataboss istsm isopm));
+   $self->setDefaultView(qw(mandator user 
+                            isdataboss isapplmgr istsm isopm isadmin));
    return($self);
+}
+
+
+sub getFlagLine
+{
+   my %mod=@_;
+
+   my @str;
+
+   foreach my $fld (qw(isdataboss istsm isopm isapplmgr isadmin)){
+     if ($mod{$fld}){
+        push(@str,"''1'' $fld");
+     }
+     else{
+        push(@str,"''0'' $fld");
+     }
+   }
+   return(join(",",@str));
+
 }
 
 
@@ -95,85 +135,168 @@ sub getSqlFrom
 
    my @view=$self->getCurrentView();
 
-print STDERR ("fifi fields=%s\n",Dumper(\@view));
-
-
    my @sqlgen=(
+   #######################################################################
 "
 SELECT
    CONCAT('select ',
-        IF(m.COLUMN_NAME IS NOT NULL, 'mandator AS mandatorid, ', 'NULL AS mandatorid, '),
-        'databoss userid,''1'' isdataboss,''0'' istsm,''0'' isopm FROM ', c.TABLE_NAME, ' WHERE ',
+        IF(m.COLUMN_NAME IS NOT NULL,'mandator AS mandatorid,',
+           'NULL AS mandatorid,'),
+        'databoss userid,".getFlagLine('isdataboss'=>1)." FROM ',c.TABLE_NAME, 
+        ' WHERE ',
         IF(cs.COLUMN_NAME IS NOT NULL, 'cistatus < 5 AND ', ''),
         'databoss IS NOT NULL and databoss<>0'
     ) AS sqlcmd
 FROM INFORMATION_SCHEMA.COLUMNS c
 LEFT JOIN INFORMATION_SCHEMA.COLUMNS cs
-    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND cs.COLUMN_NAME = 'cistatus'
+    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND 
+       cs.COLUMN_NAME = 'cistatus'
 LEFT JOIN INFORMATION_SCHEMA.COLUMNS m
-    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND m.COLUMN_NAME = 'mandator'
+    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND 
+       m.COLUMN_NAME = 'mandator'
 WHERE c.TABLE_SCHEMA = database() AND c.COLUMN_NAME = 'databoss';
-",
+"
+,   #######################################################################
+"
+SELECT
+   CONCAT('select ',
+        IF(m.COLUMN_NAME IS NOT NULL,'mandator AS mandatorid,',
+           'NULL AS mandatorid,'),
+        'applmgr userid,".getFlagLine('isapplmgr'=>1)." FROM ',c.TABLE_NAME, 
+        ' WHERE ',
+        IF(cs.COLUMN_NAME IS NOT NULL, 'cistatus < 5 AND ', ''),
+        'applmgr IS NOT NULL and applmgr<>0'
+    ) AS sqlcmd
+FROM INFORMATION_SCHEMA.COLUMNS c
+LEFT JOIN INFORMATION_SCHEMA.COLUMNS cs
+    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND 
+       cs.COLUMN_NAME = 'cistatus'
+LEFT JOIN INFORMATION_SCHEMA.COLUMNS m
+    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND 
+       m.COLUMN_NAME = 'mandator'
+WHERE c.TABLE_SCHEMA = database() AND c.COLUMN_NAME = 'applmgr';
+"
+,  #######################################################################
 "
 SELECT
    CONCAT('SELECT ',
-        IF(m.COLUMN_NAME IS NOT NULL, 'mandator AS mandatorid, ', 'NULL AS mandatorid, '),
-        'tsm userid,''0'' isdataboss,''1'' istsm,''0'' isopm  FROM ', c.TABLE_NAME, ' WHERE ',
+        IF(m.COLUMN_NAME IS NOT NULL, 'mandator AS mandatorid, ',
+           'NULL AS mandatorid, '),
+        'tsm userid,".getFlagLine('istsm'=>1)." FROM ', c.TABLE_NAME, 
+        ' WHERE ',
         IF(cs.COLUMN_NAME IS NOT NULL, 'cistatus < 5 AND ', ''),
         'tsm IS NOT NULL and tsm<>0'
     ) AS sqlcmd
 FROM INFORMATION_SCHEMA.COLUMNS c
 LEFT JOIN INFORMATION_SCHEMA.COLUMNS cs
-    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND cs.COLUMN_NAME = 'cistatus'
+    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND 
+       cs.COLUMN_NAME = 'cistatus'
 LEFT JOIN INFORMATION_SCHEMA.COLUMNS m
-    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND m.COLUMN_NAME = 'mandator'
+    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND 
+       m.COLUMN_NAME = 'mandator'
 WHERE c.TABLE_SCHEMA = database() AND c.COLUMN_NAME = 'tsm';
-",
+"
+,  #######################################################################
 "
 SELECT
    CONCAT('SELECT ',
-        IF(m.COLUMN_NAME IS NOT NULL, 'mandator AS mandatorid, ', 'NULL AS mandatorid, '),
-        'tsm2 userid,''0'' isdataboss,''1'' istsm,''0'' isopm  FROM ', c.TABLE_NAME, ' WHERE ',
+        IF(m.COLUMN_NAME IS NOT NULL, 'mandator AS mandatorid, ', 
+           'NULL AS mandatorid, '),
+        'tsm2 userid,".getFlagLine('istsm'=>1)." FROM ', c.TABLE_NAME, 
+        ' WHERE ',
         IF(cs.COLUMN_NAME IS NOT NULL, 'cistatus < 5 AND ', ''),
         'tsm2 IS NOT NULL and tsm2<>0'
     ) AS sqlcmd
 FROM INFORMATION_SCHEMA.COLUMNS c
 LEFT JOIN INFORMATION_SCHEMA.COLUMNS cs
-    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND cs.COLUMN_NAME = 'cistatus'
+    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND 
+       cs.COLUMN_NAME = 'cistatus'
 LEFT JOIN INFORMATION_SCHEMA.COLUMNS m
-    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND m.COLUMN_NAME = 'mandator'
+    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND 
+       m.COLUMN_NAME = 'mandator'
 WHERE c.TABLE_SCHEMA = database() AND c.COLUMN_NAME = 'tsm2';
-",
+"
+,  #######################################################################
 "
 SELECT
    CONCAT('SELECT ',
-        IF(m.COLUMN_NAME IS NOT NULL, 'mandator AS mandatorid, ', 'NULL AS mandatorid, '),
-        'opm userid,''0'' isdataboss,''0'' istsm,''1'' isopm  FROM ', c.TABLE_NAME, ' WHERE ',
+        IF(m.COLUMN_NAME IS NOT NULL, 'mandator AS mandatorid, ', 
+           'NULL AS mandatorid, '),
+        'opm userid,".getFlagLine('isopm'=>1)." FROM ', c.TABLE_NAME, 
+        ' WHERE ',
         IF(cs.COLUMN_NAME IS NOT NULL, 'cistatus < 5 AND ', ''),
         'opm IS NOT NULL and opm<>0'
     ) AS sqlcmd
 FROM INFORMATION_SCHEMA.COLUMNS c
 LEFT JOIN INFORMATION_SCHEMA.COLUMNS cs
-    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND cs.COLUMN_NAME = 'cistatus'
+    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND 
+       cs.COLUMN_NAME = 'cistatus'
 LEFT JOIN INFORMATION_SCHEMA.COLUMNS m
-    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND m.COLUMN_NAME = 'mandator'
+    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND 
+       m.COLUMN_NAME = 'mandator'
 WHERE c.TABLE_SCHEMA = database() AND c.COLUMN_NAME = 'opm';
-",
+"
+,  #######################################################################
 "
 SELECT
    CONCAT('SELECT ',
-        IF(m.COLUMN_NAME IS NOT NULL, 'mandator AS mandatorid, ', 'NULL AS mandatorid, '),
-        'opm2 userid,''0'' isdataboss,''0'' istsm,''1'' isopm  FROM ', c.TABLE_NAME, ' WHERE ',
+        IF(m.COLUMN_NAME IS NOT NULL, 'mandator AS mandatorid, ', 
+           'NULL AS mandatorid, '),
+        'opm2 userid,".getFlagLine('isopm'=>1)." FROM ', c.TABLE_NAME, 
+        ' WHERE ',
         IF(cs.COLUMN_NAME IS NOT NULL, 'cistatus < 5 AND ', ''),
         'opm2 IS NOT NULL and opm2<>0'
     ) AS sqlcmd
 FROM INFORMATION_SCHEMA.COLUMNS c
 LEFT JOIN INFORMATION_SCHEMA.COLUMNS cs
-    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND cs.COLUMN_NAME = 'cistatus'
+    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND 
+       cs.COLUMN_NAME = 'cistatus'
 LEFT JOIN INFORMATION_SCHEMA.COLUMNS m
-    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND m.COLUMN_NAME = 'mandator'
+    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND 
+       m.COLUMN_NAME = 'mandator'
 WHERE c.TABLE_SCHEMA = database() AND c.COLUMN_NAME = 'opm2';
 "
+,  #######################################################################
+"
+SELECT
+   CONCAT('SELECT ',
+        IF(m.COLUMN_NAME IS NOT NULL, 'mandator AS mandatorid, ', 
+           'NULL AS mandatorid, '),
+        'adm userid,".getFlagLine('isadmin'=>1)." FROM ', c.TABLE_NAME, 
+        ' WHERE ',
+        IF(cs.COLUMN_NAME IS NOT NULL, 'cistatus < 5 AND ', ''),
+        'adm IS NOT NULL and adm<>0'
+    ) AS sqlcmd
+FROM INFORMATION_SCHEMA.COLUMNS c
+LEFT JOIN INFORMATION_SCHEMA.COLUMNS cs
+    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND 
+       cs.COLUMN_NAME = 'cistatus'
+LEFT JOIN INFORMATION_SCHEMA.COLUMNS m
+    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND 
+       m.COLUMN_NAME = 'mandator'
+WHERE c.TABLE_SCHEMA = database() AND c.COLUMN_NAME = 'adm';
+"
+,  #######################################################################
+"
+SELECT
+   CONCAT('SELECT ',
+        IF(m.COLUMN_NAME IS NOT NULL, 'mandator AS mandatorid, ', 
+           'NULL AS mandatorid, '),
+        'adm2 userid,".getFlagLine('isadmin'=>1)." FROM ', c.TABLE_NAME, 
+        ' WHERE ',
+        IF(cs.COLUMN_NAME IS NOT NULL, 'cistatus < 5 AND ', ''),
+        'adm2 IS NOT NULL and adm2<>0'
+    ) AS sqlcmd
+FROM INFORMATION_SCHEMA.COLUMNS c
+LEFT JOIN INFORMATION_SCHEMA.COLUMNS cs
+    ON c.TABLE_SCHEMA = cs.TABLE_SCHEMA AND c.TABLE_NAME = cs.TABLE_NAME AND 
+       cs.COLUMN_NAME = 'cistatus'
+LEFT JOIN INFORMATION_SCHEMA.COLUMNS m
+    ON c.TABLE_SCHEMA = m.TABLE_SCHEMA AND c.TABLE_NAME = m.TABLE_NAME AND 
+       m.COLUMN_NAME = 'mandator'
+WHERE c.TABLE_SCHEMA = database() AND c.COLUMN_NAME = 'adm2';
+"
+   #######################################################################
 );
 
    my @l;
@@ -185,29 +308,47 @@ WHERE c.TABLE_SCHEMA = database() AND c.COLUMN_NAME = 'opm2';
       }
    }
 
+   my @groupbyfields;
+
+   if (in_array([qw(mandatorid mandator)],\@view)){
+      push(@groupbyfields,"mandatorid");
+   }
+   if (in_array([qw(userid user)],\@view)){
+      push(@groupbyfields,"userid");
+   }
+   my $groupby="";
+   if ($#groupbyfields!=-1){
+      $groupby="group by ".join(",",@groupbyfields);
+   }
+
+
    my $from="(select mandatorid,userid,".
               "sum(isdataboss) cntdataboss,".
+              "sum(isapplmgr) cntapplmgr,".
               "sum(istsm) cnttsm,".
               "sum(isopm) cntopm,".
+              "sum(isadmin) cntadmin,".
               "if (sum(isdataboss)>0,'1','0') isdataboss,".
+              "if (sum(isapplmgr)>0,'1','0') isapplmgr,".
               "if (sum(istsm)>0,'1','0') istsm,".
-              "if (sum(isopm)>0,'1','0') isopm ".
+              "if (sum(isopm)>0,'1','0') isopm,".
+              "if (sum(isadmin)>0,'1','0') isadmin ".
               "from (".
-              join(" union all ",@l).") l1 group by mandatorid,userid) l2";
+              join(" union all ",@l).") l1 ".$groupby.") l2";
 
    return($from);
 }
 
 
-sub initSearchQuery
-{
-   my $self=shift;
-   my $userid=$self->getCurrentUserId();
-   if (!defined(Query->Param("search_mandator"))){
-     Query->Param("search_mandator"=>
-                  "\"TelekomIT\"");
-   }
-}
+#sub initSearchQuery
+#{
+#   my $self=shift;
+#   my $userid=$self->getCurrentUserId();
+#   if (!defined(Query->Param("search_mandator"))){
+#     Query->Param("search_mandator"=>
+#                  "\"TelekomIT\"");
+#   }
+#}
 
 
 
