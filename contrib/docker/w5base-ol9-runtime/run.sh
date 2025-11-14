4cb110f1791117a0d807fa8904ee7986b6ed0c41
Hartmut Vogler <hartmut.vogler@t-systems.com>
2025-11-14 10:24:07 +0100

    update docker set

--- contrib/docker/w5base-ol9-runtime/run.sh
+++ contrib/docker/w5base-ol9-runtime/run.sh
@@ -6,5 +6,5 @@ docker container prune -f
 docker container run \
        --mount type=bind,source=/opt/w5base,target=/opt/w5base \
        -e "W5DBHOST=$W5DBHOST" \
-       --tmpfs /etc/w5base/container:rw,size=1m \
+       --tmpfs /etc/container:rw,size=100m \
        -it --name w5base it9000/w5base-ol9-runtime:beta
