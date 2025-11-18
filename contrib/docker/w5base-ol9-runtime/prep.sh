c7ed1a424553a4a8eafd105502d6905b13ca423e
Hartmut Vogler <hartmut.vogler@t-systems.com>
2025-11-18 14:50:52 +0100

    rewrite build/run process for docker compose

--- contrib/docker/w5base-ol9-runtime/prep.sh
+++ contrib/docker/w5base-ol9-runtime/prep.sh
@@ -3,6 +3,7 @@ set -x
 rm -f certs/tls-ca-bundle.pem 2>/dev/null
 cp /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem certs/tls-ca-bundle.pem
 chmod 654 certs/tls-ca-bundle.pem
+
 rm -f certs/apache-selfsigned.key 2>/dev/null
 rm -f certs/apache-selfsigned.crt 2>/dev/null
 
