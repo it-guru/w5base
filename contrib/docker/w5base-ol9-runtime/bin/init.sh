ed43d47dd45b54aad55ba5b5c18213465e9e4675
Hartmut Vogler <hartmut.vogler@t-systems.com>
2025-11-18 15:04:04 +0100

    rewrite build/run process for docker compose

--- contrib/docker/w5base-ol9-runtime/bin/init.sh
+++ contrib/docker/w5base-ol9-runtime/bin/init.sh
@@ -45,6 +45,75 @@ if [ ! -d /etc/container ]; then  # if container is not started with
 fi
 
 cat << EOF > /etc/container/maindb.conf
+EventJobBaseUrl="https://$W5BASESITENAME/$W5BASECONFIGNAME/"
+
+DATAOBJCONNECT[w5base] ="dbi:mysql:$W5DBNAME:host=$W5DBHOST;port=$W5DBPORT"
+DATAOBJUSER[w5base]    ="$W5DBUSER"
+DATAOBJPASS[w5base]    ="$W5DBPASS"
+EOF
+
+cat << EOF > /etc/container/httpd.site.conf
+ServerName $W5BASESITENAME
+Alias /$W5BASECONFIGNAME/auth/     /opt/w5base/bin/
+Alias /$W5BASECONFIGNAME/public/   /opt/w5base/bin/
+Alias /$W5BASECONFIGNAME/static/   /opt/w5base/static/
+Alias /$W5BASECONFIGNAME/index/    /var/www/html/
+
+ErrorDocument 401 /w5base/public/base/menu/LoginFail
+
+DirectoryIndex index.html
+
+RewriteRule ^/favicon.ico$ /$W5BASECONFIGNAME/public/base/load/icon_w5base.ico
+RewriteRule ^/($W5BASECONFIGNAME)/(public|auth)/([^/]+)/([^/]+)/(.+)$ /\$1/\$2/fastapp.sh?MOD=\$3::\$4&FUNC=\$5 [QSA,PT]
+
+RewriteEngine on
+
+# enable data compress
+AddOutputFilterByType DEFLATE text/html text/plain text/xml text/css text/javascript application/javascript
+
+<Directory /var/www/html>
+        Options +Indexes -FollowSymLinks +MultiViews
+        AllowOverride None
+        Require all granted
+</Directory>
+
+
+<Location /$W5BASECONFIGNAME/public>
+         Options -Indexes -FollowSymLinks +ExecCGI -Includes
+         SetHandler fcgid-script
+         Require all granted
+</Location>
+
+<Location /$W5BASECONFIGNAME/static>
+         Options -Indexes -FollowSymLinks +ExecCGI -Includes
+         Require all granted
+</Location>
+
+#
+# The authentification if mod_auth_ae is used (BasicAuth)
+#
+<Location /$W5BASECONFIGNAME/auth>
+        Options -Indexes -FollowSymLinks +ExecCGI -Includes
+        SetHandler fcgid-script
+
+        AuthType Basic
+        AuthName "Login: emea1/a??????? or ciam/mueller.hans@telekom.de"
+        aeAccountToLower on
+
+        ### T-SSO (WebSSO and OpenID hook) Begin ######
+        aeSSOUser           TelekomTSSO
+        aeSSOPass           YSBkZjY0NWE4c2c0ZGFkcwo
+        aeSSODomain         ciam/
+        aeSSOHeaderUserAttr ssotcid
+        aeSSOBasicAuthUser  sso SSO ciam CIAM ad AD ads ADS oid OpenID GoogleOID  EntraIdOID YahooOID
+        AuthBasicProvider acache
+        <Limit GET PUT POST>
+                require valid-user
+        </Limit>
+</Location>
+
+EOF
+
 DATAOBJCONNECT[w5base] ="dbi:mysql:$W5DBNAME:host=$W5DBHOST;port=$W5DBPORT"
 DATAOBJUSER[w5base]    ="$W5DBUSER"
 DATAOBJPASS[w5base]    ="$W5DBPASS"
@@ -54,7 +123,12 @@ install -d -m 2770 -o w5base -g w5adm /etc/container/var/opt/w5base/state
 install -d -m 2770 -o w5base -g w5adm /etc/container/var/w5base
 install -d -m 2770 -o w5base -g w5adm /etc/container/var/run/w5base
 install -d -m 2770 -o nobody -g w5base /etc/container/var/spool/w5base-mail
+install -d -m 2770 -o w5base -g w5base /var/log/httpd
 install -d -m 2777 -o w5base -g w5base /var/log/httpd/mod_fcgid
+install -d -m 2770 -o w5base -g w5base /var/log/w5base
+
+# ensure stale pid is removed
+rm -f /etc/container/var/opt/w5base/state/W5Server.w5server.pid 2>/dev/null
 
 
 CURW5BRANCH=$(su w5adm -c "cd /opt/w5base && git rev-parse --abbrev-ref HEAD")
@@ -64,11 +138,12 @@ if [ "$W5BRANCH" != "$CURW5BRANCH" ]; then
    su w5adm -c "cd /opt/w5base && git checkout $W5BRANCH"
    # ensure repo is fresh
    #su w5adm -c "cd /opt/w5base && git fetch && git reset --hard origin/$W5BRANCH"
-
 fi
 
 # ensure aliases.db is fresh
 /usr/bin/newaliases
+
+/opt/w5base/sbin/W5Event -s TableVersionCheck # ensure DB is up to date
 
 
 /usr/bin/supervisord -n -u root -c   /etc/supervisord.conf
