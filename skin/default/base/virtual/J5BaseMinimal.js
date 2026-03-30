6463285e0acb70bd77af9107260e5181867dfeaf
Hartmut Vogler <it@guru.de>
2026-03-30 12:03:44 +0200

    remove eval from load.pm based on [CDR#2026032742007481]
    (cherry picked from commit d76bfa357d72b845054256367ad74b81d802d973)

--- skin/default/base/virtual/J5BaseMinimal.js
+++ skin/default/base/virtual/J5BaseMinimal.js
@@ -1,4 +1,4 @@
-"${instdir}/lib/javascript/toolbox.kernel.js"
-"${instdir}/lib/javascript/utf8.js"
-"${instdir}/lib/javascript/sprintf.js"
-"${instdir}/lib/javascript/J5Base.kernel.js"
+"%W5BASEINSTDIR%/lib/javascript/toolbox.kernel.js"
+"%W5BASEINSTDIR%/lib/javascript/utf8.js"
+"%W5BASEINSTDIR%/lib/javascript/sprintf.js"
+"%W5BASEINSTDIR%/lib/javascript/J5Base.kernel.js"
